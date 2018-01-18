d4207424d2d38c25d32a3e1534037657612306bf
Katherine Bernstein <katherine@openlattice.com>
2018-01-17 17:34:28 -0800

    add loading spinner on review psa page

--- src/containers/review/ReviewReducer.js
+++ src/containers/review/ReviewReducer.js
@@ -9,6 +9,7 @@ import * as ActionTypes from './ReviewActionTypes';
 const INITIAL_STATE :Map<*, *> = Immutable.fromJS({
   scoresAsMap: Immutable.Map(),
   psaNeighborsByDate: Immutable.Map(),
+  loadingResults: false,
   errorMesasge: ''
 });
 
@@ -16,16 +17,23 @@ export default function reviewReducer(state :Map<*, *> = INITIAL_STATE, action :
 
   switch (action.type) {
 
+    case ActionTypes.LOAD_PSAS_BY_DATE_REQUEST:
+      return state
+        .set('loadingResults', true)
+        .set('errorMesasge', '');
+
     case ActionTypes.LOAD_PSAS_BY_DATE_SUCCESS:
       return state
         .set('scoresAsMap', action.scoresAsMap)
         .set('psaNeighborsByDate', action.psaNeighborsByDate)
+        .set('loadingResults', false)
         .set('errorMesasge', '');
 
     case ActionTypes.LOAD_PSAS_BY_DATE_FAILURE:
       return state
         .set('scoresAsMap', Immutable.Map())
         .set('psaNeighborsByDate', Immutable.Map())
+        .set('loadingResults', false)
         .set('errorMesasge', action.errorMesasge);
 
     default:
