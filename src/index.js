8bbd54861c924162dd61f48eb24b29b4354831c0
Marie Reed <marie@openlattice.com>
2018-04-12 12:58:57 -0700

    Removed scroll logic because, surprise, it doesn't work.

--- src/index.js
+++ src/index.js
@@ -68,22 +68,6 @@ LatticeAuth.configure({
 const routerHistory = initializeRouterHistory();
 const reduxStore = initializeReduxStore(routerHistory);
 
-routerHistory.listen(() => {
-  // Use setTimeout to make sure this runs after React Router's own listener
-  setTimeout(() => {
-    // // Keep default behavior of restoring scroll position when user:
-    // // - clicked back button
-    // // - clicked on a link that programmatically calls `history.goBack()`
-    // // - manually changed the URL in the address bar (here we might want
-    // // to scroll to top, but we can't differentiate it from the others)
-    // if (location.action === 'POP') {
-    //   return;
-    // }
-    // In all other cases, scroll to top
-    window.scrollTo(0, 0);
-  });
-});
-
 // importEntitySets();
 ReactDOM.render(
   <Provider store={reduxStore}>
