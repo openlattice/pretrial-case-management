5d7586d559ef56bf55f1f7c53b8617e0271ec0c9
Katherine Bernstein <katherine@openlattice.com>
2018-06-04 12:38:27 -0700

    display message indicating whether person is from Odyssey or not

--- src/components/person/SelectedPersonInfo.js
+++ src/components/person/SelectedPersonInfo.js
@@ -8,14 +8,26 @@ import styled from 'styled-components';
 
 import PersonCard from './PersonCard';
 import { InfoContainer, InfoWrapper, InfoHeader, Spacer } from '../../utils/Layout';
+import { PROPERTY_TYPES } from '../../utils/consts/DataModelConsts';
 
 
 const CardContainer = styled.div`
   display: inline-block;
 `;
 
+const CaseHistoryMessage = styled.div`
+  font-size: 16px;
+  font-style: italic;
+`;
+
 type Props = {
   personDetails :Immutable.Map<*, *>
+};
+
+const CaseHistoryLoaded = ({ personId } :{ personId :string }) => {
+  const text = (Number.parseInt(personId, 10).toString() === personId)
+    ? 'Case history loaded from Odyssey' : 'No Odyssey case history';
+  return <CaseHistoryMessage>{text}</CaseHistoryMessage>;
 };
 
 const SelectedPersonInfo = ({ personDetails } :Props) => {
@@ -28,6 +40,7 @@ const SelectedPersonInfo = ({ personDetails } :Props) => {
       <InfoWrapper>
         <CardContainer>
           <PersonCard person={personDetails} />
+          <CaseHistoryLoaded personId={personDetails.getIn([PROPERTY_TYPES.PERSON_ID, 0], '')} />
         </CardContainer>
       </InfoWrapper>
     </InfoContainer>
