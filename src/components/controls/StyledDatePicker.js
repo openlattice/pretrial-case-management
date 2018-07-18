3fe47c70c4e9be3724e35e85aaa4058d94ec2b63
Todd Bergman <todd@openlattice.com>
2018-07-18 16:07:12 -0700

    Indentation fix

--- src/components/controls/StyledDatePicker.js
+++ src/components/controls/StyledDatePicker.js
@@ -5,7 +5,7 @@ import DatePicker from 'react-bootstrap-date-picker';
 import calendarIcon from '../../assets/svg/calendar-icon.svg';
 
 const DatePickerWrapper = styled.div`
-border-radius: 3px;
+  border-radius: 3px;
   background-color: #f9f9fd;
   border: 1px solid #dcdce7;
   position: relative;
