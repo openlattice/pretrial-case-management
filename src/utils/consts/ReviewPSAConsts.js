277cc361dd71d4224f90f4e8da76c8275897b2ba
Todd Bergman <todd@openlattice.com>
2018-07-19 16:51:00 -0700

    Moved consts to ReviewPSAConsts in utils/consts

---
+++ src/utils/consts/ReviewPSAConsts.js
@@ -0,0 +1,126 @@
+import { PSA_STATUSES, SORT_TYPES } from '../../utils/consts/Consts';
+import { DOMAIN } from '../../utils/consts/ReportDownloadTypes';
+import * as Routes from '../../core/router/Routes';
+
+export const DATE_FORMAT = 'MM/DD/YYYY';
+
+export const STATUS_OPTIONS = {
+  OPEN: {
+    value: PSA_STATUSES.OPEN,
+    label: 'All Open'
+  },
+  SUCCESS: {
+    value: PSA_STATUSES.SUCCESS,
+    label: 'Successful'
+  },
+  FAILURE: {
+    value: PSA_STATUSES.FAILURE,
+    label: 'Failed'
+  },
+  CANCELLED: {
+    value: PSA_STATUSES.CANCELLED,
+    label: 'Cancelled'
+  },
+  DECLINED: {
+    value: PSA_STATUSES.DECLINED,
+    label: 'Declined'
+  },
+  DISMISSED: {
+    value: PSA_STATUSES.DISMISSED,
+    label: 'Dismissed'
+  },
+  ALL: {
+    value: '*',
+    label: 'All'
+  },
+  REQUIRES_ACTION: {
+    value: PSA_STATUSES.OPEN,
+    label: 'Requires Action'
+  }
+};
+
+export const openOptions = [
+  {
+    value: 'OPEN',
+    label: 'All Open'
+  },
+  {
+    value: 'REQUIRES_ACTION',
+    label: 'Requires Action'
+  }
+];
+
+export const closedOptions = [
+  {
+    value: 'SUCCESS',
+    label: 'Successful'
+  },
+  {
+    value: 'FAILURE',
+    label: 'Failed'
+  },
+  {
+    value: 'CANCELLED',
+    label: 'Cancelled'
+  },
+  {
+    value: 'DECLINED',
+    label: 'Declined'
+  },
+  {
+    value: 'DISMISSED',
+    label: 'Dismissed'
+  }
+];
+
+export const STATUS_OPTIONS_ARR = [
+  {
+    value: 'ALL',
+    label: 'All'
+  },
+  {
+    label: 'Open',
+    options: openOptions
+  },
+  {
+    label: 'Closed',
+    options: closedOptions
+  }
+];
+
+export const DOMAIN_OPTIONS_ARR = [
+  {
+    value: '',
+    label: 'All'
+  },
+  {
+    value: DOMAIN.PENNINGTON,
+    label: 'Pennington'
+  },
+  {
+    value: DOMAIN.MINNEHAHA,
+    label: 'Minnehaha'
+  }
+];
+
+export const SORT_OPTIONS_ARR = [
+  {
+    value: SORT_TYPES.NAME,
+    label: 'Name'
+  },
+  {
+    value: SORT_TYPES.DATE,
+    label: 'Date'
+  }
+];
+
+export const NAV_OPTIONS = [
+  {
+    path: Routes.REVIEW_REPORTS,
+    label: 'View All'
+  },
+  {
+    path: Routes.SEARCH_FORMS,
+    label: 'Search'
+  }
+];
