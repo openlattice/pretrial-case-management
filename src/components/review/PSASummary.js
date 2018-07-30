0e0fb04e41952f25c4e91d278550feb97c61e245
Katherine Bernstein <katherine@openlattice.com>
2018-07-30 15:53:42 -0700

    Pass empty map to ArrestCard component if arrest hasn't loaded yet

--- src/components/review/PSASummary.js
+++ src/components/review/PSASummary.js
@@ -132,7 +132,7 @@ const renderArrestInfo = ({ neighbors, manualCaseHistory } :Props) => {
   );
   const pretrialCase = manualCaseHistory
     .filter(caseObj => caseObj.getIn([PROPERTY_TYPES.CASE_ID, 0], '') === caseNum)
-    .get(0);
+    .get(0, Immutable.Map());
   return (
     <ArrestCard arrest={pretrialCase} component="summary" />
   );
