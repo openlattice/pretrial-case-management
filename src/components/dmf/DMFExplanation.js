37afefec8d7b097219690c4493ed7cf1eff4e451
Todd Bergman <todd@openlattice.com>
2018-08-01 20:50:56 -0700

    Removed Import of ScoreScale

--- src/components/dmf/DMFExplanation.js
+++ src/components/dmf/DMFExplanation.js
@@ -11,7 +11,6 @@ import DMFTable from './DMFTable';
 import StepOne from './DMFStepOne';
 import ContentBlock from '../ContentBlock';
 import ContentSection from '../ContentSection';
-import ScoreScale from '../ScoreScale';
 import BooleanFlag from '../BooleanFlag';
 import { CONTEXT, DMF, NOTES, PSA } from '../../utils/consts/Consts';
 import {
@@ -67,50 +66,6 @@ const Arrow = styled(FontAwesome).attrs({
 })`
   margin: 0 10px;
 `;
-
-// const StepOne = ({
-//   nca,
-//   fta,
-//   nvca,
-//   context
-// } :Props) => {
-//
-//   const STATS = [
-//     {
-//       label: 'NVCA',
-//       content: [<ContentsWrapper><BooleanFlag dims={FLAG_DIMS} value={nvca} /></ContentsWrapper>]
-//     },
-//     {
-//       label: 'NCA',
-//       content: [<ContentsWrapper>{nca}<ScoreScale dims={SCALE_DIMS} score={nca} /></ContentsWrapper>]
-//     },
-//     {
-//       label: 'FTA',
-//       content: [<ContentsWrapper>{fta}<ScoreScale dims={SCALE_DIMS} score={fta} /></ContentsWrapper>]
-//     },
-//     {
-//       label: 'Time of PSA',
-//       content: [<ContentsWrapper>{context}</ContentsWrapper>]
-//     }
-//   ];
-//
-//   const content = STATS.map(item => (
-//     <ContentBlock
-//         component="DMF"
-//         contentBlock={item}
-//         key={item.label} />
-//   ));
-//
-//   return (
-//     <div>
-//       <ContentSection
-//           component="DMF"
-//           header="Step One" >
-//         {content}
-//       </ContentSection>
-//     </div>
-//   );
-// };
 
 const formatTextArr = (textArr) => {
   let text = textArr[0];
