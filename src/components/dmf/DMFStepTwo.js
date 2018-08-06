e1b1b82593c3b451eb1e4baa3016b6bcf3ef438e
Todd Bergman <todd@openlattice.com>
2018-08-06 11:30:08 -0700

    Removed pretrail fta flag and included DMFCell if applicable

--- src/components/dmf/DMFStepTwo.js
+++ src/components/dmf/DMFStepTwo.js
@@ -5,9 +5,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import DMFCell from './DMFCell';
 import ContentBlock from '../ContentBlock';
 import ContentSection from '../ContentSection';
 import BooleanFlag from '../BooleanFlag';
+import { getDMFDecision } from '../../utils/consts/DMFResultConsts';
 
 const ContentsWrapper = styled.div`
   display: flex;
@@ -17,18 +19,24 @@ const ContentsWrapper = styled.div`
   margin-top: 7px;
 `;
 
+const StepWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 0px 30px 30px;
+`;
+
 const StepTwo = ({
   extradited,
-  extraditedNotes,
   stepTwoVal,
-  stepTwoNotes,
   currentViolentOffense,
   nvca,
-  fta,
   context,
-  flagDims,
-  scaleDims
+  flagDims
 } :Props) => {
+
+  // TODO: Show Pretrial FTA Flag
 
   const STEP2_VALS = [
     {
@@ -39,10 +47,10 @@ const StepTwo = ({
       label: 'Listed Charges',
       content: [<ContentsWrapper><BooleanFlag dims={flagDims} value={stepTwoVal} /></ContentsWrapper>]
     },
-    {
-      label: 'Pretrial FTA',
-      content: [<ContentsWrapper><BooleanFlag dims={flagDims} value={fta} /></ContentsWrapper>]
-    },
+    // {
+    //   label: 'Pretrial FTA',
+    //   content: [<ContentsWrapper><BooleanFlag dims={flagDims} value={fta} /></ContentsWrapper>]
+    // },
     {
       label: 'NVCA',
       content: [<ContentsWrapper><BooleanFlag dims={flagDims} value={nvca} /></ContentsWrapper>]
@@ -61,7 +69,7 @@ const StepTwo = ({
         key={item.label} />
   ));
 
-  return (
+  const flags = () => (
     <div>
       <hr />
       <ContentSection
@@ -71,45 +79,21 @@ const StepTwo = ({
       </ContentSection>
     </div>
   );
-  //
-  // const violent = currentViolentOffense && nvca;
-  // if (!extradited && !stepTwoVal && !violent) {
-  //   return (
-  //     <div>
-  //       <StepHeader>Step Two</StepHeader>
-  //       <StepWrapper>
-  //         <div>
-  //           Defendant was not extradited, no NVCA flag and current violent offense, and
-  //           no charges meet the requirements to skip to maximum requirements.
-  //         </div>
-  //       </StepWrapper>
-  //     </div>
-  //   );
-  // }
-  // const textArr = [];
-  // if (extradited) {
-  //   textArr.push('defendant was extradited');
-  // }
-  // if (violent) {
-  //   textArr.push('PSA resulted in NVCA flag with current violent offense');
-  // }
-  // if (stepTwoVal) {
-  //   textArr.push('current charge severity meets the requirements to skip to maximum requirements');
-  // }
-  //
-  // const text = formatTextArr(textArr);
-  //
-  // return (
-  //   <div>
-  //     <StepHeader>Step Two</StepHeader>
-  //     <StepWrapper>
-  //       <span>{text}</span>
-  //     </StepWrapper>
-  //     <StepWrapper>
-  //       <DMFCell dmf={getDMFDecision(6, 6, context)} selected />
-  //     </StepWrapper>
-  //   </div>
-  // );
+
+  const StepTwoDecision = extradited || stepTwoVal || (nvca && currentViolentOffense);
+
+  const displayDMF = StepTwoDecision ? (
+    <StepWrapper>
+      <DMFCell dmf={getDMFDecision(6, 6, context)} selected large />
+    </StepWrapper>
+  ) : null;
+
+  return (
+    <div>
+      {flags()}
+      {displayDMF}
+    </div>
+  );
 };
 
 export default StepTwo;
