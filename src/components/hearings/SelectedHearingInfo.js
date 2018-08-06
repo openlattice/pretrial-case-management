89b3833513328ab4dfcaad803b5b015d3d59e9c2
Katherine Bernstein <katherine@openlattice.com>
2018-08-06 12:48:36 -0700

    schedule hearing as part of psa workflow

---
+++ src/components/hearings/SelectedHearingInfo.js
@@ -0,0 +1,108 @@
+/*
+ * @flow
+ */
+
+import React from 'react';
+import styled from 'styled-components';
+
+import BasicButton from '../buttons/BasicButton';
+import InfoButton from '../buttons/InfoButton';
+import closeX from '../../assets/svg/close-x-gray.svg';
+import { HEARING } from '../../utils/consts/Consts';
+import { formatDate, formatDateTime } from '../../utils/Utils';
+
+type Props = {
+  hearing :Object,
+  onClose :() => void
+};
+
+const Container = styled.div`
+  text-align: center;
+
+  div {
+    max-width: 520px;
+    margin: 0 auto;
+  }
+`;
+
+const CloseButtonWrapper = styled.div`
+  max-width: 100% !important;
+  margin: 30px 10px -55px 0 !important;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+
+  button {
+    background: none;
+    border: none;
+  }
+`;
+
+const InfoRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: ${props => (props.center ? 'center' : 'space-between')};
+  align-items: center;
+  padding: 25px 30px;
+
+  h1 {
+    font-family: 'Open Sans', sans-serif;
+    font-size: 18px;
+    font-weight: 600;
+    color: #555e6f;
+  }
+`;
+
+const InfoItem = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  span {
+    font-family: 'Open Sans', sans-serif;
+    font-size: 12px;
+    font-weight: 600;
+    color: #8e929b;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+
+  div {
+    font-family: 'Open Sans';
+    font-size: 18px;
+    color: #2e2e34;
+  }
+`;
+
+const SelectedHearingInfo = ({ hearing, onClose } :Props) => (
+  <Container>
+    <CloseButtonWrapper>
+      <button onClick={onClose}><img src={closeX} alt="" /></button>
+    </CloseButtonWrapper>
+    <div>
+      <InfoRow center>
+        <h1>Hearing schedule has been set.</h1>
+      </InfoRow>
+
+      <InfoRow>
+        <InfoItem>
+          <span>Date</span>
+          <div>{formatDate(hearing[HEARING.DATE_TIME])}</div>
+        </InfoItem>
+        <InfoItem>
+          <span>Time</span>
+          <div>{formatDateTime(hearing[HEARING.DATE_TIME], 'hh:mm a')}</div>
+        </InfoItem>
+        <InfoItem>
+          <span>Courtroom</span>
+          <div>{hearing[HEARING.COURTROOM]}</div>
+        </InfoItem>
+      </InfoRow>
+
+      <InfoRow center>
+        <InfoButton onClick={onClose}>Close</InfoButton>
+      </InfoRow>
+    </div>
+  </Container>
+);
+
+export default SelectedHearingInfo;
