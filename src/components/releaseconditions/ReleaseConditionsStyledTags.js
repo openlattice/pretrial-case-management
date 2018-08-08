cd2c8392687947b8b3e234343f4d4d27e25836b6
Todd Bergman <todd@openlattice.com>
2018-08-08 16:04:05 -0700

    Pulled out shared styling into one file

---
+++ src/components/releaseconditions/ReleaseConditionsStyledTags.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const RowWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 30px 30px;
+  border-bottom: 1px solid #e1e1eb !important;
+`;
+
+export const OptionsGrid = styled.div`
+  margin-top: 20px;
+  display: grid;
+  grid-template-columns: ${props => (`repeat(${props.numColumns}, 1fr)`)};
+  grid-gap: 20px;
+`;
+
+export const Row = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+`;
