7c2ce5589f1d9bf223589b5823ece415eee71fa9
Katherine Bernstein <katherine@openlattice.com>
2018-08-09 09:34:05 -0700

    show real PII

--- src/containers/review/ReviewSagas.js
+++ src/containers/review/ReviewSagas.js
@@ -219,7 +219,6 @@ function* loadPSADataWorker(action :SequenceAction) :Generator<*, *, *> {
     if (action.value.length) {
       const entitySetId = yield call(EntityDataModelApi.getEntitySetId, ENTITY_SETS.PSA_SCORES);
       let neighborsById = yield call(SearchApi.searchEntityNeighborsBulk, entitySetId, action.value);
-      neighborsById = obfuscateBulkEntityNeighbors(neighborsById); // TODO just for demo
       neighborsById = Immutable.fromJS(neighborsById);
 
       neighborsById.keySeq().forEach((id) => {
@@ -424,8 +423,7 @@ function* bulkDownloadPSAReviewPDFWorker(action :SequenceAction) :Generator<*, *
       call(EntityDataModelApi.getEntitySetId, ENTITY_SETS.PEOPLE),
       call(EntityDataModelApi.getEntitySetId, ENTITY_SETS.PSA_SCORES)
     ]);
-    let peopleNeighbors = yield call(SearchApi.searchEntityNeighborsBulk, personEntitySetId, peopleEntityKeyIds);
-    peopleNeighbors = obfuscateBulkEntityNeighbors(peopleNeighbors);
+    const peopleNeighbors = yield call(SearchApi.searchEntityNeighborsBulk, personEntitySetId, peopleEntityKeyIds);
 
     let manualChargesByPersonId = Immutable.Map();
     let psasById = Immutable.Map();
@@ -475,7 +473,6 @@ function* bulkDownloadPSAReviewPDFWorker(action :SequenceAction) :Generator<*, *
     });
 
     let psaNeighborsById = yield call(SearchApi.searchEntityNeighborsBulk, psaEntitySetId, psasById.keySeq().toJS());
-    psaNeighborsById = obfuscateBulkEntityNeighbors(psaNeighborsById); // TODO just for demo
     psaNeighborsById = Immutable.fromJS(psaNeighborsById);
 
     const pageDetailsList = [];
@@ -633,8 +630,7 @@ function* refreshPSANeighborsWorker(action :SequenceAction) :Generator<*, *, *> 
   try {
     yield put(refreshPSANeighbors.request(action.id, { id }));
     const entitySetId = yield call(EntityDataModelApi.getEntitySetId, ENTITY_SETS.PSA_SCORES);
-    let neighborsList = yield call(SearchApi.searchEntityNeighbors, entitySetId, id);
-    neighborsList = obfuscateEntityNeighbors(neighborsList); // TODO just for demo
+    const neighborsList = yield call(SearchApi.searchEntityNeighbors, entitySetId, id);
     let neighbors = Immutable.Map();
     neighborsList.forEach((neighbor) => {
       const { neighborEntitySet, neighborDetails } = neighbor;
