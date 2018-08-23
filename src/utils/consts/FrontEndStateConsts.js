af8d34afdd94cd9e2cc6b3031060956a4c5858a1
Todd Bergman <todd@openlattice.com>
2018-08-23 15:07:05 -0700

    Create frontend state consts for general PSA and REVIEW

---
+++ src/utils/consts/FrontEndStateConsts.js
@@ -0,0 +1,25 @@
+// ReviewPSA
+
+export const REVIEW_PSA = {
+  ENTITY_SET_ID: 'scoresEntitySetId',
+  SCORES: 'scoresAsMap',
+  NEIGHBORS_BY_ID: 'psaNeighborsById',
+  NEIGHBORS_BY_DATE: 'psaNeighborsByDate',
+  LOADING_DATA: 'loadingPSAData',
+  LOADING_RESULTS: 'loadingResults',
+  ERROR: 'errorMessage',
+  ALL_FILERS: 'allFilers',
+  CASE_HISTORY: 'caseHistory',
+  MANUAL_CASE_HISTORY: 'manualCaseHistory',
+  CHARGE_HISTORY: 'chargeHistory',
+  MANUAL_CHARGE_HISTORY: 'manualChargeHistory',
+  SENTENCE_HISTORY: 'sentenceHistory',
+  FTA_HISTORY: 'ftaHistory',
+  HEARINGS: 'hearings',
+  READ_ONLY: 'readOnly',
+  PSA_IDS_REFRESHING: 'psaIdsRefreshing'
+};
+
+export const PSA_FORM = {
+
+};
