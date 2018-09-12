d46f7d56eed86d5fc5ee7707cefe82fc39989c63
Todd Bergman <todd@openlattice.com>
2018-09-12 13:21:12 -0700

    Added getOpenPsaNeighbors to court action factory

--- src/containers/court/CourtActionFactory.js
+++ src/containers/court/CourtActionFactory.js
@@ -13,11 +13,16 @@ const filterPeopleIdsWithOpenPSAs :RequestSequence = newRequestSequence(FILTER_P
 const LOAD_HEARINGS_FOR_DATE :string = 'LOAD_HEARINGS_FOR_DATE';
 const loadHearingsForDate :RequestSequence = newRequestSequence(LOAD_HEARINGS_FOR_DATE);
 
+const GET_OPEN_PSA_NEIGHBORS :string = 'GET_OPEN_PSA_NEIGHBORS';
+const getOpenPsaNeighbors :RequestSequence = newRequestSequence(GET_OPEN_PSA_NEIGHBORS);
+
 export {
   CHANGE_HEARING_FILTERS,
   FILTER_PEOPLE_IDS_WITH_OPEN_PSAS,
   LOAD_HEARINGS_FOR_DATE,
+  GET_OPEN_PSA_NEIGHBORS,
   changeHearingFilters,
   filterPeopleIdsWithOpenPSAs,
-  loadHearingsForDate
+  loadHearingsForDate,
+  getOpenPsaNeighbors
 };
