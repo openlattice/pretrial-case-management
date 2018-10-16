66b9afdb2e2e58c38e1b4e346856bc755ea76bca
Todd Bergman <todd@openlattice.com>
2018-10-16 16:51:25 -0700

    Refactored create new hearing for available hearings section

--- src/components/hearings/NewHearingSection.js
+++ src/components/hearings/NewHearingSection.js
@@ -5,54 +5,31 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import ContentBlock from '../ContentBlock';
+import ContentSection from '../ContentSection';
+import CONTENT_CONSTS from '../../utils/consts/ContentConsts';
 import InfoButton from '../buttons/InfoButton';
 import DatePicker from '../controls/StyledDatePicker';
 import SearchableSelect from '../controls/SearchableSelect';
+
 import { getCourtroomOptions, getJudgeOptions, HEARING_CONSTS } from '../../utils/consts/HearingConsts';
 import { getTimeOptions } from '../../utils/consts/DateTimeConsts';
 
-
-const CenteredContainer = styled.div`
-  width: 100%;
-  text-align: center;
-`;
-
 const StyledSearchableSelect = styled(SearchableSelect)`
+  width: 200px;
   input {
     width: 100%;
+    font-size: 14px;
   }
 `;
 
 const CreateButton = styled(InfoButton)`
   width: 210px;
   height: 40px;
+  margin-top: 50px;
   padding-left: 0;
   padding-right: 0;
 `;
-
-const InputRow = styled.div`
-  display: inline-flex;
-  flex-direction: row;
-  margin-bottom: 20px;
-  width: 100%;
-
-  section {
-    width: 25%;
-    padding: 0 2.5% 0 2.5%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  }
-`;
-
-const InputLabel = styled.span`
-  font-family: 'Open Sans', sans-serif;
-  font-size: 14px;
-  text-align: left;
-  color: #555e6f;
-  margin-bottom: 10px;
-`;
-
 
 const NameInput = styled.input.attrs({
   type: 'otherJudgeText'
@@ -68,8 +45,24 @@ const NameInput = styled.input.attrs({
   background-color: #ffffff;
 `;
 
+const HearingSectionAside = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const HearingSectionWrapper = styled.div`
+  min-height: 160px;
+  display: grid;
+  grid-template-columns: 75% 25%;
+  padding-bottom: 20px;
+  margin: 0 -15px;
+`;
+
 const NewHearingSection = ({
   allJudges,
+  manuallyCreatingHearing,
   newHearingDate,
   newHearingTime,
   newHearingCourtroom,
@@ -81,69 +74,116 @@ const NewHearingSection = ({
   onInputChange,
   isReadyToSubmit,
   selectCurrentHearing
-} :Props) => (
-  <CenteredContainer>
-    <InputRow>
-      <section>
-        <InputLabel>Date</InputLabel>
-        <DatePicker
-            value={newHearingDate}
-            onChange={hearingDate => onDateChange(hearingDate)}
-            clearButton={false} />
-      </section>
-      <section>
-        <InputLabel>Time</InputLabel>
-        <StyledSearchableSelect
-            options={getTimeOptions()}
-            value={newHearingTime}
-            onSelect={hearingTime => onSelectChange({
-              [HEARING_CONSTS.FIELD]: HEARING_CONSTS.NEW_HEARING_TIME,
-              [HEARING_CONSTS.NEW_HEARING_TIME]: hearingTime
-            })}
-            short />
-      </section>
-      <section>
-        <InputLabel>Courtroom</InputLabel>
-        <StyledSearchableSelect
-            options={getCourtroomOptions()}
-            value={newHearingCourtroom}
-            onSelect={hearingCourtroom => onSelectChange({
-              [HEARING_CONSTS.FIELD]: HEARING_CONSTS.NEW_HEARING_COURTROOM,
-              [HEARING_CONSTS.NEW_HEARING_COURTROOM]: hearingCourtroom
-            })}
-            short />
-      </section>
-      <section>
-        <InputLabel />
-        <CreateButton disabled={!isReadyToSubmit()} onClick={selectCurrentHearing}>
-          Create New
-        </CreateButton>
-      </section>
-    </InputRow>
-    <InputRow>
-      <section>
-        <InputLabel>Judge</InputLabel>
-        <StyledSearchableSelect
-            options={getJudgeOptions(allJudges, jurisdiction)}
-            value={judge}
-            onSelect={judgeOption => onSelectChange(judgeOption)}
-            short />
-      </section>
+} :Props) => {
+  let date;
+  let time;
+  let courtroom;
+  let judgeSelect;
+  let otherJudge;
+  let createHearingButton;
+
+  if (manuallyCreatingHearing) {
+    date = (
+      <DatePicker
+          value={newHearingDate}
+          onChange={hearingDate => onDateChange(hearingDate)}
+          clearButton={false} />
+    );
+
+    time = (
+      <StyledSearchableSelect
+          options={getTimeOptions()}
+          value={newHearingTime}
+          onSelect={hearingTime => onSelectChange({
+            [HEARING_CONSTS.FIELD]: HEARING_CONSTS.NEW_HEARING_TIME,
+            [HEARING_CONSTS.NEW_HEARING_TIME]: hearingTime
+          })}
+          short />
+    );
+
+    courtroom = (
+      <StyledSearchableSelect
+          options={getCourtroomOptions()}
+          value={newHearingCourtroom}
+          onSelect={hearingCourtroom => onSelectChange({
+            [HEARING_CONSTS.FIELD]: HEARING_CONSTS.NEW_HEARING_COURTROOM,
+            [HEARING_CONSTS.NEW_HEARING_COURTROOM]: hearingCourtroom
+          })}
+          short />
+    );
+
+    judgeSelect = (
+      <StyledSearchableSelect
+          options={getJudgeOptions(allJudges, jurisdiction)}
+          value={judge}
+          onSelect={judgeOption => onSelectChange(judgeOption)}
+          short />
+    );
+
+    createHearingButton = (
+      <CreateButton disabled={!isReadyToSubmit()} onClick={selectCurrentHearing}>
+        Create New
+      </CreateButton>
+    );
+
+    otherJudge = (
+      <NameInput
+          onChange={e => (onInputChange(e))}
+          name="otherJudgeText"
+          value={otherJudgeText} />
+    );
+  }
+
+  const HEARING_ARR = [
+    {
+      label: 'Date',
+      content: [date]
+    },
+    {
+      label: 'Time',
+      content: [time]
+    },
+    {
+      label: 'Courtroom',
+      content: [courtroom]
+    },
+    {
+      label: 'Judge',
+      content: [judgeSelect]
+    }
+  ];
+  if (judge === 'Other') {
+    HEARING_ARR.push(
       {
-        judge === 'Other'
-          ? (
-            <section>
-              <InputLabel>Other Judge's Name</InputLabel>
-              <NameInput
-                  onChange={e => (onInputChange(e))}
-                  name="otherJudgeText"
-                  value={otherJudgeText} />
-            </section>
-          )
-          : null
+        label: "Other Judge's Name",
+        content: [otherJudge]
       }
-    </InputRow>
-  </CenteredContainer>
-);
+    );
+  }
+  const hearingInfoContent = HEARING_ARR.map(hearingItem => (
+    <ContentBlock
+        component={CONTENT_CONSTS.CREATING_HEARING}
+        contentBlock={hearingItem}
+        key={hearingItem.label} />
+  ));
+
+  const hearingInfoSection = (
+    <ContentSection
+        header="Create New Hearing"
+        modifyingHearing={manuallyCreatingHearing}
+        component={CONTENT_CONSTS.CREATING_HEARING}>
+      {hearingInfoContent}
+    </ContentSection>
+  );
+
+  return (
+    <HearingSectionWrapper>
+      {hearingInfoSection}
+      <HearingSectionAside>
+        {createHearingButton}
+      </HearingSectionAside>
+    </HearingSectionWrapper>
+  );
+};
 
 export default NewHearingSection;
