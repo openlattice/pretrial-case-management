e11c5d31cbcb7e589b6491da3ba7bf7bed158e5d
Todd Bergman <todd@openlattice.com>
2018-10-19 18:59:59 -0600

    Added download psa by hearing date to download action factory

--- src/containers/download/DownloadActionFactory.js
+++ src/containers/download/DownloadActionFactory.js
@@ -4,10 +4,15 @@
 
 import { newRequestSequence } from 'redux-reqseq';
 
+const DOWNLOAD_PSA_BY_HEARING_DATE :string = 'DOWNLOAD_PSA_BY_HEARING_DATE';
+const downloadPSAsByHearingDate :RequestSequence = newRequestSequence(DOWNLOAD_PSA_BY_HEARING_DATE);
+
 const DOWNLOAD_PSA_FORMS :string = 'DOWNLOAD_PSA_FORMS';
 const downloadPsaForms :RequestSequence = newRequestSequence(DOWNLOAD_PSA_FORMS);
 
 export {
+  DOWNLOAD_PSA_BY_HEARING_DATE,
   DOWNLOAD_PSA_FORMS,
+  downloadPSAsByHearingDate,
   downloadPsaForms
 };
