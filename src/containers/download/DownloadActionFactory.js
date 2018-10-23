231e7963f2eaf7e6016f1347c85f2863819cee24
Todd Bergman <todd@openlattice.com>
2018-10-22 18:30:41 -0700

    Alphabetized order of actions

--- src/containers/download/DownloadActionFactory.js
+++ src/containers/download/DownloadActionFactory.js
@@ -4,15 +4,15 @@
 
 import { newRequestSequence } from 'redux-reqseq';
 
-const DOWNLOAD_PSA_FORMS :string = 'DOWNLOAD_PSA_FORMS';
-const downloadPsaForms :RequestSequence = newRequestSequence(DOWNLOAD_PSA_FORMS);
-
 const DOWNLOAD_CHARGE_LISTS :string = 'DOWNLOAD_CHARGE_LISTS';
 const downloadChargeLists :RequestSequence = newRequestSequence(DOWNLOAD_CHARGE_LISTS);
 
+const DOWNLOAD_PSA_FORMS :string = 'DOWNLOAD_PSA_FORMS';
+const downloadPsaForms :RequestSequence = newRequestSequence(DOWNLOAD_PSA_FORMS);
+
 export {
-  DOWNLOAD_PSA_FORMS,
   DOWNLOAD_CHARGE_LISTS,
-  downloadPsaForms,
-  downloadChargeLists
+  DOWNLOAD_PSA_FORMS,
+  downloadChargeLists,
+  downloadPsaForms
 };
