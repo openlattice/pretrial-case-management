a1bbdfa90e347e00dc837d4ba9082bce98e75246
Todd Bergman <todd@openlattice.com>
2018-10-27 16:31:22 -0700

    Created color const file

---
+++ src/utils/consts/Colors.js
@@ -0,0 +1,58 @@
+export const OL = {
+  PURPLE01: '#361876',
+  PURPLE02: '#6124e2',
+  PURPLE03: '#8045ff',
+  PURPLE04: '#b898ff',
+  PURPLE05: '#d0bbff',
+  PURPLE06: '#e4d8ff',
+  PURPLE07: '#8b66db',
+  PURPLE08: '#a069d7',
+  PURPLE09: '#b36cd2',
+  PURPLE10: '#c86fce',
+  PURPLE11: '#dd72ca',
+  PINK01: '#ff77c2',
+  GREY01: '#555e6f',
+  GREY02: '#8e929b',
+  GREY03: '#b6bbc7',
+  GREY04: '#cdd1db',
+  GREY05: '#dcdce7',
+  GREY06: '#e6e6f7',
+  GREY07: '#eaeaf0',
+  GREY08: '#f0f0f7',
+  GREY09: '#f5f5f8',
+  GREY10: '#f9f9fd',
+  GREY11: '#e1e1eb', // Not included in StyleSheet, but used as border color for most components in PSA
+  GREY12: '#f7f8f9', // Not included in StyleSheet, but used as hover color for PSA Row List
+  GREY13: '#ceced9', // Not included in StyleSheet, but used as border color for NavButtonToolbar
+  GREY14: '#f8f8fc', // Not included in StyleSheet, but used as background color for ChargeRow
+  GREY15: '#2e2e34', // Not included in StyleSheet, but used as background color for ChargeRow
+  BLUE01: '#455aff',
+  BLUE02: '#a3adff',
+  GREEN01: '#008f63',
+  GREEN02: '#00be84',
+  GREEN03: '#80dfc2',
+  RED01: '#ff3c5d',
+  YELLOW01: '#dd9e00',
+  YELLOW02: '#ffe671',
+  ORANGE01: '#ff9a58',
+  PEACH: '#ffb7a2',
+  WHITE: '#ffffff'
+};
+
+export const SCALE = {
+  ONE: OL.PURPLE07,
+  TWO: OL.PURPLE08,
+  THREE: OL.PURPLE09,
+  FOUR: OL.PURPLE10,
+  FIVE: OL.PURPLE11,
+  SIX: OL.PINK01
+};
+
+export const STATUS = {
+  OPEN: OL.PURPLE07,
+  SUCCESS: OL.GREEN02,
+  FAILURE: OL.RED01,
+  CANCELLED: OL.GREY03,
+  DECLINED: OL.GREY01,
+  DISMISSED: OL.GREY01
+};
