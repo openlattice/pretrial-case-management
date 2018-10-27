a36865d614d5950bffedc4e40ab6128d080833d8
Todd Bergman <todd@openlattice.com>
2018-10-26 17:55:14 -0700

    REfactored for pending charges and pending charges flag

--- src/containers/people/PersonDetailsContainer.js
+++ src/containers/people/PersonDetailsContainer.js
@@ -10,8 +10,15 @@ import { Constants } from 'lattice';
 
 import AboutPerson from '../../components/people/AboutPerson';
 import LoadingSpinner from '../../components/LoadingSpinner';
-import { ENTITY_SETS } from '../../utils/consts/DataModelConsts';
-import { STATE, PEOPLE, PSA_NEIGHBOR } from '../../utils/consts/FrontEndStateConsts';
+import { ENTITY_SETS, PROPERTY_TYPES } from '../../utils/consts/DataModelConsts';
+import {
+  STATE,
+  PEOPLE,
+  REVIEW,
+  PSA_NEIGHBOR,
+  PSA_ASSOCIATION
+} from '../../utils/consts/FrontEndStateConsts';
+
 import * as PeopleActionFactory from './PeopleActionFactory';
 import * as ReviewActionFactory from '../review/ReviewActionFactory';
 
@@ -21,6 +28,9 @@ type Props = {
   selectedPersonData :Immutable.Map<*, *>,
   isFetchingPersonData :boolean,
   neighbors :Immutable.Map<*, *>,
+  psaNeighborsById :Immutable.Map<*, *>,
+  mostRecentPSA :Immutable.Map<*, *>,
+  mostRecentPSAEntityKeyId :string,
   actions :{
     getPersonData :(personId :string) => void,
     getPersonNeighbors :(value :{
@@ -65,21 +75,50 @@ class PersonDetailsContainer extends React.Component<Props, State> {
 
   render() {
     const {
-      neighbors,
       isFetchingPersonData,
+      loadingPSAData,
+      loadingPSAResults,
+      mostRecentPSA,
+      mostRecentPSAEntityKeyId,
+      neighbors,
+      psaNeighborsById,
       selectedPersonData
     } = this.props;
+    const arrestDate = psaNeighborsById
+      .getIn(
+        [
+          mostRecentPSAEntityKeyId,
+          ENTITY_SETS.MANUAL_PRETRIAL_CASES,
+          PSA_NEIGHBOR.DETAILS,
+          PROPERTY_TYPES.ARREST_DATE_TIME,
+          0
+        ],
+        ''
+      );
+    const lastEditDateForPSA = psaNeighborsById.getIn(
+      [mostRecentPSAEntityKeyId, ENTITY_SETS.STAFF, 0, PSA_ASSOCIATION.DETAILS, PROPERTY_TYPES.DATE_TIME, 0],
+      ''
+    );
+
     const scoreSeq = neighbors.get(ENTITY_SETS.PSA_SCORES, Immutable.Map())
       .filter(neighbor => !!neighbor.get(PSA_NEIGHBOR.DETAILS))
       .map(neighbor => [neighbor
         .getIn([PSA_NEIGHBOR.DETAILS, OPENLATTICE_ID_FQN, 0]), neighbor.get(PSA_NEIGHBOR.DETAILS)]);
+
+    const isLoading = (loadingPSAData || loadingPSAResults || isFetchingPersonData);
     return (
       <div>
         {
           isFetchingPersonData
-            ? <LoadingSpinner /> :
-            (
+            ? <LoadingSpinner />
+            : (
               <AboutPerson
+                  loading={isLoading}
+                  loadingPSAResults={loadingPSAResults}
+                  lastEditDateForPSA={lastEditDateForPSA}
+                  arrestDate={arrestDate}
+                  mostRecentPSA={mostRecentPSA}
+                  mostRecentPSAEntityKeyId={mostRecentPSAEntityKeyId}
                   selectedPersonData={selectedPersonData}
                   isFetchingPersonData={isFetchingPersonData}
                   scoreSeq={scoreSeq}
@@ -93,14 +132,24 @@ class PersonDetailsContainer extends React.Component<Props, State> {
 
 function mapStateToProps(state, ownProps) {
   const { personId } = ownProps.match.params;
+  const psaNeighborsById = state.getIn([STATE.REVIEW, REVIEW.NEIGHBORS_BY_ID], true);
+  const loadingPSAData = state.getIn([STATE.REVIEW, REVIEW.LOADING_DATA], true);
+  const loadingPSAResults = state.getIn([STATE.REVIEW, REVIEW.LOADING_RESULTS], true);
   const isFetchingPersonData = state.getIn([STATE.PEOPLE, PEOPLE.FETCHING_PERSON_DATA], true);
   const selectedPersonData = state.getIn([STATE.PEOPLE, PEOPLE.PERSON_DATA], Immutable.List());
   const neighbors = state.getIn([STATE.PEOPLE, PEOPLE.NEIGHBORS, personId], Immutable.Map());
+  const mostRecentPSA = state.getIn([STATE.PEOPLE, PEOPLE.MOST_RECENT_PSA], Immutable.Map());
+  const mostRecentPSAEntityKeyId = state.getIn([STATE.PEOPLE, PEOPLE.MOST_RECENT_PSA_ENTITY_KEY], '');
 
   return {
+    loadingPSAData,
+    loadingPSAResults,
+    psaNeighborsById,
     selectedPersonData,
     isFetchingPersonData,
-    neighbors
+    neighbors,
+    mostRecentPSA,
+    mostRecentPSAEntityKeyId
   };
 }
 
