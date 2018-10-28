f20df3dd101cf6e3ecfc8feb1a9ed7c6a5697334
Todd Bergman <todd@openlattice.com>
2018-10-27 20:55:34 -0700

    Replaced hex colors with ol color consts

--- src/components/dashboard/CreateFormListItem.js
+++ src/components/dashboard/CreateFormListItem.js
@@ -7,29 +7,29 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 import FormListItemIcon from './FormListItemIcon';
-
+import { OL } from '../../utils/consts/Colors';
 
 const ItemWrapper = styled(Link)`
   align-items: center;
-  background: white;
+  background: ${OL.WHITE};
   color: inherit;
   display: flex;
   height: 55px;
   padding: 8px;
   margin: 10px 0;
   width: 100%;
-  border: 1px solid #e1e1eb;
+  border: 1px solid ${OL.GREY11};
   border-radius: 5px;
 
   &:hover {
-    background: #dcdcdc;
+    background: ${OL.GREY25};
     color: inherit;
     text-decoration: none;
   }
 `;
 
 const StyledName = styled.div`
-  color: #727272;
+  color: ${OL.GREY26};
   font-size: 18px;
 `;
 
