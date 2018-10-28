a30eb4bc7101e9a9cfa4d24d3f33271f24eececb
Todd Bergman <todd@openlattice.com>
2018-10-27 20:47:55 -0700

    Replaced hex colors with ol color consts in the remaining controls

--- src/components/controls/StyledTextArea.js
+++ src/components/controls/StyledTextArea.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { OL } from '../../utils/consts/Colors';
+
 const StyledTextArea = styled.textarea`
   width: 100%;
   min-height: 138px;
   border-radius: 3px;
-  background-color: #f9f9fd;
-  border: solid 1px #dcdce7;
+  background-color: ${OL.GREY10};
+  border: solid 1px ${OL.GREY05};
   padding: 10px 20px;
   font-family: 'Open Sans', sans-serif;
   font-size: 14px;
 
   &::placeholder {
-    color: #8e929b;
+    color: ${OL.GREY02};
   }
 
   &:focus {
     box-shadow: inset 0 0 0 1px rebeccapurple;
     outline: none;
-    background-color: #ffffff;
+    background-color: ${OL.WHITE};
   }
 
   &:disabled {
     cursor: default;
-    background-color: #ffffff;
+    background-color: ${OL.WHITE};
   }
 `;
 
