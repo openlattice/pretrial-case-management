10948e33543444d494158b6f99548e2a0da4cd60
Todd Bergman <todd@openlattice.com>
2018-11-01 16:59:46 -0700

    Created View More Link for new profile pages

---
+++ src/components/buttons/ViewMoreLink.js
@@ -0,0 +1,26 @@
+/*
+ * @flow
+ */
+
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import { OL } from '../../utils/consts/Colors';
+
+const ViewMoreLink = styled(Link)`
+  background-color: transparent;
+  border: none;
+  color: ${OL.PURPLE02};
+  cursor: pointer;
+  font-weight: 600;
+  font-size: 14px;
+  outline: none;
+  padding: 10px 20px;
+  text-align: right;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export default ViewMoreLink;
