f16111e236f01966561d7cb7cea961835d39e8ff
Todd Bergman <todd@openlattice.com>
2018-11-02 11:40:40 -0700

    created new component for hearings with title

---
+++ src/components/hearings/HearingCardsWithTitle.js
@@ -0,0 +1,46 @@
+/*
+ * @flow
+ */
+
+import React from 'react';
+import Immutable from 'immutable';
+import styled from 'styled-components';
+
+import HearingCardsHolder from './HearingCardsHolder';
+import { Title } from '../../utils/Layout';
+
+const Wrapper = styled.div`
+  width: 100%;
+`;
+
+type Props = {
+  hearings :Immutable.List<*>,
+  hearingsWithOutcomes :Immutable.List<*>,
+  selectedHearing :Object,
+  handleSelect :(row :Immutable.Map<*, *>, hearingId :string, entityKeyId :string) => void,
+  title :string,
+  subtitle :string
+}
+
+const HearingCardsWithTitle = ({
+  hearings,
+  hearingsWithOutcomes,
+  handleSelect,
+  selectedHearing,
+  title,
+  subtitle
+} :Props) => (
+  <Wrapper>
+    <Title withSubtitle>
+      <span>{title}</span>
+      {subtitle || null}
+    </Title>
+    <HearingCardsHolder
+        hearings={hearings}
+        hearingsWithOutcomes={hearingsWithOutcomes}
+        selectedHearing={selectedHearing}
+        handleSelect={handleSelect} />
+  </Wrapper>
+);
+
+export default HearingCardsWithTitle;
