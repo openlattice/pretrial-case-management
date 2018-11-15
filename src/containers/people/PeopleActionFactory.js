a7790a6830bea42f519e6422466e998cd24e482d
Todd Bergman <todd@openlattice.com>
2018-11-15 11:13:36 -0800

    Added refreshPersonNeighbors to PeopleActionFactory

--- src/containers/people/PeopleActionFactory.js
+++ src/containers/people/PeopleActionFactory.js
@@ -10,6 +10,9 @@ const getPeople = newRequestSequence(GET_PEOPLE);
 const GET_PERSON_DATA = 'GET_PERSON_DATA';
 const getPersonData = newRequestSequence(GET_PERSON_DATA);
 
+const REFRESH_PERSON_NEIGHBORS = 'REFRESH_PERSON_NEIGHBORS';
+const refreshPersonNeighbors = newRequestSequence(REFRESH_PERSON_NEIGHBORS);
+
 const GET_PERSON_NEIGHBORS = 'GET_PERSON_NEIGHBORS';
 const getPersonNeighbors = newRequestSequence(GET_PERSON_NEIGHBORS);
 
@@ -17,7 +20,9 @@ export {
   GET_PEOPLE,
   GET_PERSON_DATA,
   GET_PERSON_NEIGHBORS,
+  REFRESH_PERSON_NEIGHBORS,
   getPeople,
   getPersonData,
-  getPersonNeighbors
+  getPersonNeighbors,
+  refreshPersonNeighbors
 };
