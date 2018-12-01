b40b751d44f566bfcb2c325054c1db7eea7f2de0
Todd Bergman <todd@openlattice.com>
2018-11-30 17:03:13 -0800

    collected charge list for violent, dmf, bhe, and bre lists

--- src/containers/charges/ChargesSagas.js
+++ src/containers/charges/ChargesSagas.js
@@ -2,8 +2,10 @@
  * @flow
  */
 import { DataApiActions, DataApiSagas } from 'lattice-sagas';
+import { Map, Set, fromJS } from 'immutable';
 import { call, put, takeEvery } from 'redux-saga/effects';
 
+import { PROPERTY_TYPES } from '../../utils/consts/DataModelConsts';
 import { LOAD_CHARGES, loadCharges } from './ChargesActionFactory';
 
 const { getEntitySetData } = DataApiActions;
@@ -14,7 +16,12 @@ const { getEntitySetDataWorker } = DataApiSagas;
  */
 
 function* loadChargesWorker(action :SequenceAction) :Generator<*, *, *> {
-
+  let violentArrestCharges = Map();
+  let violentCourtCharges = Map();
+  let dmfStep2Charges = Map();
+  let dmfStep4Charges = Map();
+  let bookingReleaseExceptionCharges = Map();
+  let bookingHoldExceptionCharges = Map();
   const { id, value } = action;
   const { arrestChargesEntitySetId, courtChargesEntitySetId, selectedOrgId } = value;
   if (value === null || value === undefined) {
@@ -37,9 +44,68 @@ function* loadChargesWorker(action :SequenceAction) :Generator<*, *, *> {
     if (chargeError) throw chargeError;
 
     // reset values to data
-    arrestCharges = arrestCharges.data;
-    courtCharges = courtCharges.data;
-    yield put(loadCharges.success(action.id, { selectedOrgId, arrestCharges, courtCharges }));
+    arrestCharges = fromJS(arrestCharges.data);
+    courtCharges = fromJS(courtCharges.data);
+
+    // Collect violent, dmf, bhe and bre lists for arrest charges
+    arrestCharges.forEach((charge) => {
+      const statute = charge.getIn([PROPERTY_TYPES.REFERENCE_CHARGE_STATUTE, 0], '');
+      const description = charge.getIn([PROPERTY_TYPES.REFERENCE_CHARGE_DESCRIPTION, 0], '');
+      if (charge.getIn([PROPERTY_TYPES.CHARGE_IS_VIOLENT, 0], false)) {
+        violentArrestCharges = violentArrestCharges.set(
+          statute,
+          violentArrestCharges.get(statute, Set()).add(description)
+        );
+      }
+      if (charge.getIn([PROPERTY_TYPES.CHARGE_DMF_STEP_2, 0], false)) {
+        dmfStep2Charges = dmfStep2Charges.set(
+          statute,
+          dmfStep2Charges.get(statute, Set()).add(description)
+        );
+      }
+      if (charge.getIn([PROPERTY_TYPES.CHARGE_DMF_STEP_4, 0], false)) {
+        dmfStep4Charges = dmfStep4Charges.set(
+          statute,
+          dmfStep4Charges.get(statute, Set()).add(description)
+        );
+      }
+      if (charge.getIn([PROPERTY_TYPES.BRE, 0], false)) {
+        bookingReleaseExceptionCharges = bookingReleaseExceptionCharges.set(
+          statute,
+          bookingReleaseExceptionCharges.get(statute, Set()).add(description)
+        );
+      }
+      if (charge.getIn([PROPERTY_TYPES.BHE, 0], false)) {
+        bookingHoldExceptionCharges = bookingHoldExceptionCharges.set(
+          statute,
+          bookingHoldExceptionCharges.get(statute, Set()).add(description)
+        );
+      }
+    });
+
+    // Collect violent list for court charges
+    courtCharges.forEach((charge) => {
+      const statute = charge.getIn([PROPERTY_TYPES.REFERENCE_CHARGE_STATUTE, 0], '');
+      const description = charge.getIn([PROPERTY_TYPES.REFERENCE_CHARGE_DESCRIPTION, 0], '');
+      if (charge.getIn([PROPERTY_TYPES.CHARGE_IS_VIOLENT, 0], false)) {
+        violentCourtCharges = violentCourtCharges.set(
+          statute,
+          violentCourtCharges.get(statute, Set()).add(description)
+        );
+      }
+    });
+
+    yield put(loadCharges.success(action.id, {
+      arrestCharges,
+      bookingHoldExceptionCharges,
+      bookingReleaseExceptionCharges,
+      courtCharges,
+      dmfStep2Charges,
+      dmfStep4Charges,
+      selectedOrgId,
+      violentArrestCharges,
+      violentCourtCharges
+    }));
   }
   catch (error) {
     yield put(loadCharges.failure(action.id, error));
