d839db962271ce0de3704ea89e5c461884c35ffe
Todd Bergman <todd@openlattice.com>
2018-12-03 18:38:46 -0800

    Fixed positioning of second column of names.

--- src/utils/PDFUtils.js
+++ src/utils/PDFUtils.js
@@ -1013,16 +1013,15 @@ const coverPage = (doc :Object, selectedPeople :Immutable.Map<*, *>[]) => {
   y += Y_INC_SMALL;
   thickLine(doc, y);
   y += Y_INC;
-
   selectedPeople.slice(0, 50).forEach((selectedPerson) => {
     doc.text(X_COL_1, y, getListName(selectedPerson));
     y += Y_INC;
   });
+  y = 15 + Y_INC_SMALL + Y_INC;
   selectedPeople.slice(50).forEach((selectedPerson) => {
-    doc.text(X_COL_2, y, getListName(selectedPerson));
+    doc.text(X_COL_3, y, getListName(selectedPerson));
     y += Y_INC;
   });
-
 };
 
 export const exportPDFList = (fileName :string, pages :{
