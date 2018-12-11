2756371702f516f8ef2f4409bf1bd3cad050d90e
Todd Bergman <todd@openlattice.com>
2018-12-10 19:44:41 -0800

    Added update and delete charge to ChargesActionFactory

--- src/containers/charges/ChargesActionFactory.js
+++ src/containers/charges/ChargesActionFactory.js
@@ -4,10 +4,21 @@
 
 import { newRequestSequence } from 'redux-reqseq';
 
+const DELETE_CHARGE :string = 'DELETE_CHARGE';
+const deleteCharge :RequestSequence = newRequestSequence(DELETE_CHARGE);
+
+const UPDATE_CHARGE :string = 'UPDATE_CHARGE';
+const updateCharge :RequestSequence = newRequestSequence(UPDATE_CHARGE);
+
 const LOAD_CHARGES :string = 'LOAD_CHARGES';
 const loadCharges :RequestSequence = newRequestSequence(LOAD_CHARGES);
 
+
 export {
+  DELETE_CHARGE,
   LOAD_CHARGES,
-  loadCharges
+  UPDATE_CHARGE,
+  deleteCharge,
+  loadCharges,
+  updateCharge
 };
