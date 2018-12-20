71b1f423b509440fa8af9a7597a92ed4d3f17295
Todd Bergman <todd@openlattice.com>
2018-12-20 09:43:44 -0800

    Created EDM reducer

---
+++ src/edm/EDMReducer.js
@@ -0,0 +1,51 @@
+/*
+ * @flow
+ */
+
+import { List, Map, fromJS } from 'immutable';
+import { Models } from 'lattice';
+import { EntityDataModelApiActions } from 'lattice-sagas';
+
+import { EDM } from '../utils/consts/FrontEndStateConsts';
+
+const { getAllPropertyTypes } = EntityDataModelApiActions;
+const { FullyQualifiedName } = Models;
+
+
+const INITIAL_STATE :Map<*, *> = fromJS({
+  [EDM.FQN_TO_ID]: Map(),
+  [EDM.IS_FETCHING_PROPERTY_TYPES]: false,
+  [EDM.PROPERTY_TYPES_BY_ID]: Map(),
+});
+
+export default function edmReducer(state :Map<*, *> = INITIAL_STATE, action :Object) {
+
+  switch (action.type) {
+
+    case getAllPropertyTypes.case(action.type): {
+      return getAllPropertyTypes.reducer(state, action, {
+        REQUEST: () => state.set(EDM.IS_FETCHING_PROPERTY_TYPES, true),
+        SUCCESS: () => {
+          const seqAction :SequenceAction = (action :any);
+          const propertyTypes :List<Map<*, *>> = fromJS(seqAction.value);
+          const propertyTypesById :Map<string, number> = Map().asMutable();
+          const fqnToIdMap :Map<FullyQualifiedName, string> = Map().asMutable();
+          propertyTypes.forEach((propertyType :Map<*, *>) => {
+            propertyTypesById.set(propertyType.get('id'), propertyType);
+            fqnToIdMap.set(new FullyQualifiedName(propertyType.get('type')), propertyType.get('id'));
+          });
+          return state
+            .set(EDM.FQN_TO_ID, fqnToIdMap.asImmutable())
+            .set(EDM.PROPERTY_TYPES_BY_ID, propertyTypesById.asImmutable());
+        },
+        FAILURE: () => state
+          .set(EDM.FQN_TO_ID, Map())
+          .set(EDM.PROPERTY_TYPES_BY_ID, Map()),
+        FINALLY: () => state.set(EDM.IS_FETCHING_PROPERTY_TYPES, false)
+      });
+    }
+
+    default:
+      return state;
+  }
+}
