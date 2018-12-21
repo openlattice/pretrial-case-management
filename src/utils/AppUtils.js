194c5b2130fba670a2a31926831f2403b406527e
Todd Bergman <todd@openlattice.com>
2018-12-21 11:28:01 -0800

    Refactored App utils to not be a default export

--- src/utils/AppUtils.js
+++ src/utils/AppUtils.js
@@ -3,10 +3,8 @@
  */
 import { APP } from './consts/FrontEndStateConsts';
 
-const getEntitySetId :string = (app, FQN :string, orgId :string) => app.getIn([
+export const getEntitySetId :string = (app, FQN :string, orgId :string) => app.getIn([
   FQN,
   APP.ENTITY_SETS_BY_ORG,
   orgId
 ]);
-
-export default getEntitySetId;
