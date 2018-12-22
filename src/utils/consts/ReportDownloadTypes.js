c4873de66953d30e0b3b2dbe0bf6fb6d2f939155
Todd Bergman <todd@openlattice.com>
2018-12-21 16:44:13 -0800

    Refactored for app type fqns rather than entity sets

--- src/utils/consts/ReportDownloadTypes.js
+++ src/utils/consts/ReportDownloadTypes.js
@@ -1,23 +1,46 @@
-import { ENTITY_SETS, PROPERTY_TYPES } from './DataModelConsts';
+/*
+ * @flow
+ */
+
+import { APP_TYPES_FQNS, PROPERTY_TYPES } from './DataModelConsts';
+
+const {
+  ASSESSED_BY,
+  DMF_RESULTS,
+  PEOPLE,
+  PSA_RISK_FACTORS,
+  PSA_SCORES,
+  RELEASE_RECOMMENDATIONS,
+  STAFF
+} = APP_TYPES_FQNS;
+
+
+const assessedByFqn :string = ASSESSED_BY.toString();
+const dmfResultsFqn :string = DMF_RESULTS.toString();
+const releastRecommendationsFqn :string = RELEASE_RECOMMENDATIONS.toString();
+const peopleFqn :string = PEOPLE.toString();
+const psaRiskFactorsFqn :string = PSA_RISK_FACTORS.toString();
+const psaScoresFqn :string = PSA_SCORES.toString();
+const staffFqn :string = STAFF.toString();
 
 export const SUMMARY_REPORT = {
-  [ENTITY_SETS.PEOPLE]: {
+  [peopleFqn]: {
     [PROPERTY_TYPES.LAST_NAME]: 'Last Name',
     [PROPERTY_TYPES.FIRST_NAME]: 'First Name',
     [PROPERTY_TYPES.DOB]: 'Date of Birth'
   },
-  [ENTITY_SETS.STAFF]: {
+  [staffFqn]: {
     [PROPERTY_TYPES.PERSON_ID]: 'Completed By'
   },
-  [ENTITY_SETS.ASSESSED_BY]: {
+  [assessedByFqn]: {
     [PROPERTY_TYPES.COMPLETED_DATE_TIME]: 'Creation Date'
   },
-  [ENTITY_SETS.PSA_SCORES]: {
+  [psaScoresFqn]: {
     [PROPERTY_TYPES.NVCA_FLAG]: 'NVCA',
     [PROPERTY_TYPES.NCA_SCALE]: 'NCA',
     [PROPERTY_TYPES.FTA_SCALE]: 'FTA'
   },
-  [ENTITY_SETS.DMF_RESULTS]: {
+  [dmfResultsFqn]: {
     S2: 'S2',
     S4: 'S4',
     [PROPERTY_TYPES.COLOR]: 'RCM Color',
@@ -28,12 +51,12 @@ export const SUMMARY_REPORT = {
 };
 
 export const PSA_RESPONSE_TABLE = {
-  [ENTITY_SETS.PEOPLE]: {
+  [peopleFqn]: {
     [PROPERTY_TYPES.LAST_NAME]: 'Last Name',
     [PROPERTY_TYPES.FIRST_NAME]: 'First Name',
     [PROPERTY_TYPES.DOB]: 'Date of Birth'
   },
-  [ENTITY_SETS.PSA_RISK_FACTORS]: {
+  [psaRiskFactorsFqn]: {
     [PROPERTY_TYPES.AGE_AT_CURRENT_ARREST]: 'Q1',
     [PROPERTY_TYPES.CURRENT_VIOLENT_OFFENSE]: 'Q2',
     [PROPERTY_TYPES.PENDING_CHARGE]: 'Q3',
@@ -44,7 +67,7 @@ export const PSA_RESPONSE_TABLE = {
     [PROPERTY_TYPES.PRIOR_FAILURE_TO_APPEAR_OLD]: 'Q8',
     [PROPERTY_TYPES.PRIOR_SENTENCE_TO_INCARCERATION]: 'Q9'
   },
-  [ENTITY_SETS.RELEASE_RECOMMENDATIONS]: {
+  [releastRecommendationsFqn]: {
     [PROPERTY_TYPES.RELEASE_RECOMMENDATION]: 'Additional Notes'
   }
 };
