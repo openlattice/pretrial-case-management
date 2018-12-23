a75490d6b4bbd6ce78b08f30c6c78f49fe4ad740
Todd Bergman <todd@openlattice.com>
2018-12-23 13:12:30 -0500

    Replace entitySet consts with AppTypeFqns

--- src/utils/PSAUtils.js
+++ src/utils/PSAUtils.js
@@ -1,9 +1,21 @@
 import Immutable from 'immutable';
 import moment from 'moment';
 
-import { ENTITY_SETS, PROPERTY_TYPES } from './consts/DataModelConsts';
+import { APP_TYPES_FQNS, PROPERTY_TYPES } from './consts/DataModelConsts';
 import { PSA_STATUSES } from './consts/Consts';
 import { PSA_NEIGHBOR, PSA_ASSOCIATION } from './consts/FrontEndStateConsts';
+
+let {
+  ASSESSED_BY,
+  EDITED_BY,
+  PEOPLE,
+  STAFF
+} = APP_TYPES_FQNS;
+
+ASSESSED_BY = ASSESSED_BY.toString();
+EDITED_BY = EDITED_BY.toString();
+PEOPLE = PEOPLE.toString();
+STAFF = STAFF.toString();
 
 
 export const sortPeopleByName = (p1, p2) => {
@@ -23,8 +35,8 @@ export const sortPeopleByName = (p1, p2) => {
 };
 
 export const sortByName = ([id1, neighbor1], [id2, neighbor2]) => {
-  const p1 = neighbor1.getIn([ENTITY_SETS.PEOPLE, PSA_NEIGHBOR.DETAILS], Immutable.Map());
-  const p2 = neighbor2.getIn([ENTITY_SETS.PEOPLE, PSA_NEIGHBOR.DETAILS], Immutable.Map());
+  const p1 = neighbor1.getIn([PEOPLE, PSA_NEIGHBOR.DETAILS], Immutable.Map());
+  const p2 = neighbor2.getIn([PEOPLE, PSA_NEIGHBOR.DETAILS], Immutable.Map());
 
   return sortPeopleByName(p1, p2);
 };
@@ -35,7 +47,7 @@ export const sortByDate = ([id1, neighbor1], [id2, neighbor2]) => {
 
   const getDate = (neighborObj, latest) => {
     const associationName = neighborObj.getIn([PSA_ASSOCIATION.ENTITY_SET, 'name']);
-    const ptFqn = associationName === ENTITY_SETS.ASSESSED_BY
+    const ptFqn = associationName === ASSESSED_BY
       ? PROPERTY_TYPES.COMPLETED_DATE_TIME : PROPERTY_TYPES.DATE_TIME;
     const date = moment(neighborObj.getIn([PSA_ASSOCIATION.DETAILS, ptFqn, 0], ''));
     if (date.isValid()) {
@@ -46,12 +58,12 @@ export const sortByDate = ([id1, neighbor1], [id2, neighbor2]) => {
     return null;
   };
 
-  neighbor1.get(ENTITY_SETS.STAFF, Immutable.List()).forEach((neighborObj) => {
+  neighbor1.get(STAFF, Immutable.List()).forEach((neighborObj) => {
     const date = getDate(neighborObj, latest1);
     if (date) latest1 = date;
   });
 
-  neighbor2.get(ENTITY_SETS.STAFF, Immutable.List()).forEach((neighborObj) => {
+  neighbor2.get(STAFF, Immutable.List()).forEach((neighborObj) => {
     const date = getDate(neighborObj, latest2);
     if (date) latest2 = date;
   });
@@ -86,8 +98,8 @@ export const psaIsClosed = (psa) => {
 export const getLastEditDetails = (neighbors) => {
   let date;
   let user;
-  neighbors.get(ENTITY_SETS.STAFF, Immutable.List()).forEach((neighbor) => {
-    if (neighbor.getIn([PSA_ASSOCIATION.ENTITY_SET, 'name']) === ENTITY_SETS.EDITED_BY) {
+  neighbors.get(STAFF, Immutable.List()).forEach((neighbor) => {
+    if (neighbor.getIn([PSA_ASSOCIATION.ENTITY_SET, 'name']) === EDITED_BY) {
       const editUser = neighbor.getIn([PSA_NEIGHBOR.DETAILS, PROPERTY_TYPES.PERSON_ID, 0]);
       const editDate = moment(neighbor.getIn([PSA_ASSOCIATION.DETAILS, PROPERTY_TYPES.DATE_TIME, 0], ''));
       if (editUser && editDate.isValid() && (!date || editDate.isAfter(date))) {
