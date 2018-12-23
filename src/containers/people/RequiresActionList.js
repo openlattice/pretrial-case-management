396f4e91b8a6e85adfa40981f642a07ff9d40e8e
Todd Bergman <todd@openlattice.com>
2018-12-23 17:11:25 -0500

    replaced entity set names with app type fqns

--- src/containers/people/RequiresActionList.js
+++ src/containers/people/RequiresActionList.js
@@ -10,7 +10,7 @@ import { connect } from 'react-redux';
 import LoadingSpinner from '../../components/LoadingSpinner';
 import PeopleList from '../../components/people/PeopleList';
 import { getFormattedPeople } from '../../utils/PeopleUtils';
-import { ENTITY_SETS, PROPERTY_TYPES } from '../../utils/consts/DataModelConsts';
+import { APP_TYPES_FQNS, PROPERTY_TYPES } from '../../utils/consts/DataModelConsts';
 import { PSA_STATUSES } from '../../utils/consts/Consts';
 import {
   PSA_NEIGHBOR,
@@ -20,6 +20,11 @@ import {
 } from '../../utils/consts/FrontEndStateConsts';
 
 import * as ReviewActionFactory from '../review/ReviewActionFactory';
+
+let { PEOPLE, STAFF } = APP_TYPES_FQNS;
+
+PEOPLE = PEOPLE.toString();
+STAFF = STAFF.toString();
 
 type Props = {
   countyFilter :string,
@@ -46,11 +51,11 @@ class RequiresActionList extends React.Component<Props, State> {
 
     psaNeighborsById.valueSeq().forEach((neighbors) => {
       const staffMatchingFilter = neighbors
-        .get(ENTITY_SETS.STAFF, Immutable.List()).filter(neighbor => neighbor
+        .get(STAFF, Immutable.List()).filter(neighbor => neighbor
           .getIn([PSA_NEIGHBOR.DETAILS, PROPERTY_TYPES.PERSON_ID, 0], '').includes(countyFilter));
 
       if (!countyFilter.length || staffMatchingFilter.size) {
-        const neighbor = neighbors.getIn([ENTITY_SETS.PEOPLE, PSA_NEIGHBOR.DETAILS], Immutable.Map());
+        const neighbor = neighbors.getIn([PEOPLE, PSA_NEIGHBOR.DETAILS], Immutable.Map());
         const personId = neighbor.get(PROPERTY_TYPES.PERSON_ID);
         if (personId) {
           peopleById = peopleById.set(personId, peopleById.get(personId, Immutable.List()).push(neighbor));
