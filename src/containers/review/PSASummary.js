8c728fe46622320f7f7136d18623d8fb461144ed
Todd Bergman <todd@openlattice.com>
2018-12-23 13:11:07 -0500

    Replace entitySet consts with AppTypeFqns

--- src/containers/review/PSASummary.js
+++ src/containers/review/PSASummary.js
@@ -16,7 +16,7 @@ import PersonCardSummary from '../../components/person/PersonCardSummary';
 import PSAReportDownloadButton from '../../components/review/PSAReportDownloadButton';
 import PSAStats from '../../components/review/PSAStats';
 import SummaryDMFDetails from '../../components/dmf/SummaryDMFDetails';
-import { ENTITY_SETS, PROPERTY_TYPES } from '../../utils/consts/DataModelConsts';
+import { APP_TYPES_FQNS, PROPERTY_TYPES } from '../../utils/consts/DataModelConsts';
 import { formatDateTimeList } from '../../utils/FormattingUtils';
 import { getTimeStamp, getNeighborDetailsForEntitySet } from '../../utils/DataUtils';
 import { OL } from '../../utils/consts/Colors';
@@ -31,6 +31,19 @@ import {
 
 import * as Routes from '../../core/router/Routes';
 import * as ReviewActionFactory from './ReviewActionFactory';
+
+const {
+  ASSESSED_BY,
+  MANUAL_PRETRIAL_CASES,
+  PSA_RISK_FACTORS,
+  STAFF
+} = APP_TYPES_FQNS;
+
+const assessedByFqn :string = ASSESSED_BY.toString();
+const manualPretrialCasesFqn :string = MANUAL_PRETRIAL_CASES.toString();
+const peopleFqn :string = APP_TYPES_FQNS.PEOPLE.toString();
+const psaRiskFactorsFqn :string = PSA_RISK_FACTORS.toString();
+const staffFqn :string = STAFF.toString();
 
 const ButtonWrapper = styled.div`
   display: flex;
@@ -139,7 +152,7 @@ class PSASummary extends React.Component<Props, *> {
   renderArrestInfo = () => {
     const { neighbors, profile } = this.props;
     const component = profile ? `${CONTENT_CONSTS.PROFILE}|${CONTENT_CONSTS.ARREST}` : CONTENT_CONSTS.ARREST;
-    const pretrialCase = getNeighborDetailsForEntitySet(neighbors, ENTITY_SETS.MANUAL_PRETRIAL_CASES);
+    const pretrialCase = getNeighborDetailsForEntitySet(neighbors, manualPretrialCasesFqn);
     return (
       <ArrestCard arrest={pretrialCase} component={component} />
     );
@@ -157,7 +170,7 @@ class PSASummary extends React.Component<Props, *> {
 
   renderPersonInfo = () => {
     const { neighbors } = this.props;
-    const person = getNeighborDetailsForEntitySet(neighbors, ENTITY_SETS.PEOPLE);
+    const person = getNeighborDetailsForEntitySet(neighbors, peopleFqn);
     return (
       <PersonCardSummary person={person} />
     );
@@ -187,11 +200,11 @@ class PSASummary extends React.Component<Props, *> {
     const { neighbors, actions, scores } = this.props;
     const { downloadPSAReviewPDF } = actions;
     let filer;
-    const psaDate = formatDateTimeList(getTimeStamp(neighbors, ENTITY_SETS.PSA_RISK_FACTORS));
-    neighbors.get(ENTITY_SETS.STAFF, Immutable.List()).forEach((neighbor) => {
-      const associationEntitySetName = neighbor.getIn([PSA_ASSOCIATION.ENTITY_SET, 'name']);
+    const psaDate = formatDateTimeList(getTimeStamp(neighbors, psaRiskFactorsFqn));
+    neighbors.get(staffFqn, Immutable.List()).forEach((neighbor) => {
+      const associationEntitySetId = neighbor.getIn([PSA_ASSOCIATION.ENTITY_SET, 'id']);
       const personId = neighbor.getIn([PSA_NEIGHBOR.DETAILS, PROPERTY_TYPES.PERSON_ID, 0], '');
-      if (associationEntitySetName === ENTITY_SETS.ASSESSED_BY) {
+      if (associationEntitySetId === assessedByFqn) {
         filer = personId;
       }
     });
