ab994a1bce94c9609080b20d45d1268851297f84
Todd Bergman <todd@openlattice.com>
2018-12-26 17:13:46 -0500

    Refactored to render report types based on org settings

--- src/containers/forms/FormsContainer.js
+++ src/containers/forms/FormsContainer.js
@@ -11,7 +11,7 @@ import CreateFormListItem from '../../components/dashboard/CreateFormListItem';
 import psaIcon from '../../assets/svg/public-safety-icon.svg';
 import { APP, STATE } from '../../utils/consts/FrontEndStateConsts';
 import { CONTEXT } from '../../utils/consts/Consts';
-import { ORG_IDS } from '../../utils/consts/DataModelConsts';
+import { ORG_IDS, SETTINGS, CONTEXTS } from '../../utils/consts/DataModelConsts';
 import { OL } from '../../utils/consts/Colors';
 import { StyledFormWrapper } from '../../utils/Layout';
 import * as Routes from '../../core/router/Routes';
@@ -19,7 +19,8 @@ import * as Routes from '../../core/router/Routes';
 const {
   BOOKING,
   COURT_MINN,
-  COURT_PENN
+  COURT_PENN,
+  DEMO_ORG
 } = CONTEXT;
 
 const FormsWrapper = styled.div`
@@ -35,11 +36,42 @@ class FormsContainer extends React.Component<Props, *> {
 
   getPSAPath = context => `${Routes.PSA_FORM}?${qs.stringify({ context })}`;
 
+  renderBookingContext = () => {
+    const { selectedOrganizationSettings } = this.props;
+    const includeBooking = selectedOrganizationSettings.getIn([SETTINGS.CONTEXTS, CONTEXTS.BOOKING], '');
+    if (includeBooking) {
+      return (
+        <CreateFormListItem
+            name="Public Safety Assessment (Booking)"
+            path={this.getPSAPath(BOOKING)}
+            icon={psaIcon} />
+      );
+    }
+    return null;
+  }
+
+  renderCourtContext = (jurisdiction) => {
+    const { selectedOrganizationSettings } = this.props;
+    const includeCourt = selectedOrganizationSettings.getIn([SETTINGS.CONTEXTS, CONTEXTS.COURT], '');
+    if (includeCourt) {
+      return (
+        <CreateFormListItem
+            name="Public Safety Assessment (Court)"
+            path={this.getPSAPath(jurisdiction)}
+            icon={psaIcon} />
+      );
+    }
+    return null;
+  }
+
   render() {
     const { selectedOrganizationTitle, selectedOrganizationId } = this.props;
     // TODO: This is yucky. We will want to rework once we phase out different contexts for different orgs.
     let jurisdiction;
     switch (selectedOrganizationId) {
+      case ORG_IDS.DEMO_ORG:
+        jurisdiction = DEMO_ORG;
+        break;
       case ORG_IDS.PENNINGTON_SD:
         jurisdiction = COURT_PENN;
         break;
@@ -56,14 +88,8 @@ class FormsContainer extends React.Component<Props, *> {
             jurisdiction
               ? (
                 <FormsWrapper>
-                  <CreateFormListItem
-                      name="Public Safety Assessment (Booking)"
-                      path={this.getPSAPath(BOOKING)}
-                      icon={psaIcon} />
-                  <CreateFormListItem
-                      name="Public Safety Assessment (Court)"
-                      path={this.getPSAPath(jurisdiction)}
-                      icon={psaIcon} />
+                  { this.renderBookingContext() }
+                  { this.renderCourtContext(jurisdiction) }
                 </FormsWrapper>
               )
               : null
@@ -80,6 +106,7 @@ function mapStateToProps(state) {
   return {
     // App
     [APP.SELECTED_ORG_ID]: app.get(APP.SELECTED_ORG_ID),
+    [APP.SELECTED_ORG_SETTINGS]: app.get(APP.SELECTED_ORG_SETTINGS),
     [APP.SELECTED_ORG_TITLE]: app.get(APP.SELECTED_ORG_TITLE),
   };
 }
