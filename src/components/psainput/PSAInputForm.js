1c447ee64da68303f04bb6e7010b874a77f37b65
Todd Bergman <todd@openlattice.com>
2018-12-27 19:04:05 -0500

    Only display questions 10-14 if pretrial module is included in settings

--- src/components/psainput/PSAInputForm.js
+++ src/components/psainput/PSAInputForm.js
@@ -20,7 +20,7 @@ import { formatValue } from '../../utils/FormattingUtils';
 import { getRecentFTAs, getOldFTAs } from '../../utils/FTAUtils';
 import { getSentenceToIncarcerationCaseNums } from '../../utils/SentenceUtils';
 import { StyledSectionWrapper, ErrorMessage } from '../../utils/Layout';
-import { PROPERTY_TYPES, SETTINGS } from '../../utils/consts/DataModelConsts';
+import { PROPERTY_TYPES, SETTINGS, MODULE } from '../../utils/consts/DataModelConsts';
 import { OL } from '../../utils/consts/Colors';
 import {
   getViolentChargeLabels,
@@ -403,11 +403,12 @@ class PSAInputForm extends React.Component<Props, State> {
       noBorders,
       psaDate,
       selectedOrganizationId,
+      selectedOrganizationSettings,
       viewOnly,
       violentArrestCharges,
       violentCourtCharges,
     } = this.props;
-
+    const includesPretrialModule = selectedOrganizationSettings.getIn([SETTINGS.MODULES, MODULE.PRETRIAL], false)
     const violentChargeList = violentArrestCharges.get(selectedOrganizationId, Map());
     const violentCourtChargeList = violentCourtCharges.get(selectedOrganizationId, Map());
     const dmfStep2ChargeList = dmfStep2Charges.get(selectedOrganizationId, Map());
@@ -560,55 +561,62 @@ class PSAInputForm extends React.Component<Props, State> {
                 noPriorConvictions
               )
             }
-
-            <PaddedHeader>DMF Information</PaddedHeader>
-
             {
-              this.renderTFQuestionRow(
-                10,
-                EXTRADITED,
-                EXTRADITED_PROMPT
-              )
-            }
+              includesPretrialModule
+                ? (
+                  <>
+                    <PaddedHeader>DMF Information</PaddedHeader>
 
-            {
-              this.renderTFQuestionRow(
-                11,
-                STEP_2_CHARGES,
-                STEP_2_CHARGES_PROMPT,
-                step2Charges
-              )
-            }
+                    {
+                      this.renderTFQuestionRow(
+                        10,
+                        EXTRADITED,
+                        EXTRADITED_PROMPT
+                      )
+                    }
 
-            {
-              this.renderTFQuestionRow(
-                12,
-                STEP_4_CHARGES,
-                STEP_4_CHARGES_PROMPT,
-                step4Charges
-              )
-            }
+                    {
+                      this.renderTFQuestionRow(
+                        11,
+                        STEP_2_CHARGES,
+                        STEP_2_CHARGES_PROMPT,
+                        step2Charges
+                      )
+                    }
 
-            {
-              input.get(COURT_OR_BOOKING) === CONTEXT.BOOKING
-                ? this.renderTFQuestionRow(
-                  13,
-                  SECONDARY_RELEASE_CHARGES,
-                  SECONDARY_RELEASE_CHARGES_PROMPT,
-                  secondaryReleaseCharges,
-                  true, // requested to be disabled by client
-                  secondaryReleaseHeader
-                ) : null
-            }
-            {
-              input.get(COURT_OR_BOOKING) === CONTEXT.BOOKING
-                ? this.renderTFQuestionRow(
-                  14,
-                  SECONDARY_HOLD_CHARGES,
-                  SECONDARY_HOLD_CHARGES_PROMPT,
-                  currentBRECharges,
-                  true, // requested to be disabled by client
-                  secondaryHoldHeader
+                    {
+                      this.renderTFQuestionRow(
+                        12,
+                        STEP_4_CHARGES,
+                        STEP_4_CHARGES_PROMPT,
+                        step4Charges
+                      )
+                    }
+
+                    {
+                      ((input.get(COURT_OR_BOOKING) === CONTEXT.BOOKING) && includesPretrialModule)
+                        ? this.renderTFQuestionRow(
+                          13,
+                          SECONDARY_RELEASE_CHARGES,
+                          SECONDARY_RELEASE_CHARGES_PROMPT,
+                          secondaryReleaseCharges,
+                          true, // requested to be disabled by client
+                          secondaryReleaseHeader
+                        ) : null
+                    }
+                    {
+                      ((input.get(COURT_OR_BOOKING) === CONTEXT.BOOKING) && includesPretrialModule)
+                        ? this.renderTFQuestionRow(
+                          14,
+                          SECONDARY_HOLD_CHARGES,
+                          SECONDARY_HOLD_CHARGES_PROMPT,
+                          currentBRECharges,
+                          true, // requested to be disabled by client
+                          secondaryHoldHeader
+                        ) : null
+                    }
+
+                  </>
                 ) : null
             }
             <FooterContainer>
