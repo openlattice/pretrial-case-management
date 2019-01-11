e468d4cd2fe3886e511b54240f612ad1f69bd5ab
Todd Bergman <todd@openlattice.com>
2019-01-10 21:12:17 -0800

    Set sep success state for replaceEntity and replaceAssociation actions

--- src/utils/submit/SubmitReducer.js
+++ src/utils/submit/SubmitReducer.js
@@ -16,7 +16,9 @@ import {
 
 const INITIAL_STATE :Immutable.Map<*, *> = Immutable.Map().withMutations((map :Immutable.Map<*, *>) => {
   map.set(SUBMIT.REPLACING_ASSOCIATION, false);
+  map.set(SUBMIT.REPLACE_ASSOCIATION_SUCCESS, false);
   map.set(SUBMIT.REPLACING_ENTITY, false);
+  map.set(SUBMIT.REPLACE_ENTITY_SUCCESS, false);
   map.set(SUBMIT.SUBMITTING, false);
   map.set(SUBMIT.SUCCESS, false);
   map.set(SUBMIT.SUBMITTED, false);
@@ -30,9 +32,14 @@ function submitReducer(state :Immutable.Map<*, *> = INITIAL_STATE, action :Objec
       return replaceEntity.reducer(state, action, {
         REQUEST: () => state
           .set(SUBMIT.REPLACING_ENTITY, true)
+          .set(SUBMIT.REPLACE_ENTITY_SUCCESS, false)
           .set(SUBMIT.ERROR, ''),
-        SUCCESS: () => state.set(SUBMIT.SUCCESS, true).set(SUBMIT.ERROR, ''),
-        FAILURE: () => state.set(SUBMIT.REPLACING_ENTITY, false).set(SUBMIT.ERROR, action.value),
+        SUCCESS: () => state
+          .set(SUBMIT.REPLACE_ENTITY_SUCCESS, true)
+          .set(SUBMIT.ERROR, ''),
+        FAILURE: () => state
+          .set(SUBMIT.REPLACE_ENTITY_SUCCESS, true)
+          .set(SUBMIT.ERROR, action.value),
         FINALLY: () => state.set(SUBMIT.REPLACING_ENTITY, false)
       });
     }
@@ -54,9 +61,15 @@ function submitReducer(state :Immutable.Map<*, *> = INITIAL_STATE, action :Objec
       return replaceAssociation.reducer(state, action, {
         REQUEST: () => state
           .set(SUBMIT.REPLACING_ASSOCIATION, true)
+          .set(SUBMIT.REPLACE_ASSOCIATION_SUCCESS, false)
           .set(SUBMIT.ERROR, ''),
-        SUCCESS: () => state.set(SUBMIT.REPLACING_ASSOCIATION, true).set(SUBMIT.ERROR, ''),
-        FAILURE: () => state.set(SUBMIT.REPLACING_ASSOCIATION, false).set(SUBMIT.ERROR, action.value),
+        SUCCESS: () => state
+          .set(SUBMIT.REPLACE_ASSOCIATION_SUCCESS, true)
+          .set(SUBMIT.REPLACING_ASSOCIATION, true)
+          .set(SUBMIT.ERROR, ''),
+        FAILURE: () => state
+          .set(SUBMIT.REPLACE_ASSOCIATION_SUCCESS, false)
+          .set(SUBMIT.ERROR, action.value),
         FINALLY: () => state.set(SUBMIT.REPLACING_ASSOCIATION, false)
       });
     }
