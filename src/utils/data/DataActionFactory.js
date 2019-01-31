6ec78dca5a1d5ae43e28ad813e1a5c55332e1233
Todd Bergman <todd@openlattice.com>
2019-01-31 13:41:50 -0800

    added update entity data to data actions

--- src/utils/data/DataActionFactory.js
+++ src/utils/data/DataActionFactory.js
@@ -10,9 +10,14 @@ const deleteEntity :RequestSequence = newRequestSequence(DELETE_ENTITY);
 const REPLACE_ENTITY_DATA :string = 'REPLACE_ENTITY_DATA';
 const replaceEntity :RequestSequence = newRequestSequence(REPLACE_ENTITY_DATA);
 
+const UPDATE_ENTITY_DATA :string = 'UPDATE_ENTITY_DATA';
+const updateEntity :RequestSequence = newRequestSequence(UPDATE_ENTITY_DATA);
+
 export {
   DELETE_ENTITY,
   REPLACE_ENTITY_DATA,
+  UPDATE_ENTITY_DATA,
   deleteEntity,
-  replaceEntity
+  replaceEntity,
+  updateEntity
 };
