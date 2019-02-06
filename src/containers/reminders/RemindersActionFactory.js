366463ee2a1a71ad0e93a1f9c9d3fb6853b9b779
Todd Bergman <todd@openlattice.com>
2019-02-06 15:36:44 -0800

    added load people with hearings but no preferred contact action

--- src/containers/reminders/RemindersActionFactory.js
+++ src/containers/reminders/RemindersActionFactory.js
@@ -4,6 +4,11 @@
 
 import { newRequestSequence } from 'redux-reqseq';
 
+const LOAD_PEOPLE_WITH_HEARINGS_BUT_NO_CONTACTS :string = 'LOAD_PEOPLE_WITH_HEARINGS_BUT_NO_CONTACTS';
+const loadPeopleWithHearingsButNoContacts :RequestSequence = newRequestSequence(
+  LOAD_PEOPLE_WITH_HEARINGS_BUT_NO_CONTACTS
+);
+
 const LOAD_REMINDERS_FOR_DATE :string = 'LOAD_REMINDERS_FOR_DATE';
 const loadRemindersforDate :RequestSequence = newRequestSequence(LOAD_REMINDERS_FOR_DATE);
 
@@ -11,8 +16,10 @@ const LOAD_REMINDER_NEIGHBORS :string = 'LOAD_REMINDER_NEIGHBORS';
 const loadReminderNeighborsById :RequestSequence = newRequestSequence(LOAD_REMINDER_NEIGHBORS);
 
 export {
+  LOAD_PEOPLE_WITH_HEARINGS_BUT_NO_CONTACTS,
   LOAD_REMINDER_NEIGHBORS,
   LOAD_REMINDERS_FOR_DATE,
+  loadPeopleWithHearingsButNoContacts,
   loadReminderNeighborsById,
   loadRemindersforDate
 };
