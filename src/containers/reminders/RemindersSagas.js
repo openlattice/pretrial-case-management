a32aa2348b8aa6434c8d220db7e484e0d897fe4c
Todd Bergman <todd@openlattice.com>
2019-02-06 15:32:56 -0800

    Added a saga to collect peole with hearings but no preferred contact

--- src/containers/reminders/RemindersSagas.js
+++ src/containers/reminders/RemindersSagas.js
@@ -30,8 +30,10 @@ import { toISODate } from '../../utils/FormattingUtils';
 import { obfuscateBulkEntityNeighbors } from '../../utils/consts/DemoNames';
 import { APP_TYPES_FQNS, PROPERTY_TYPES } from '../../utils/consts/DataModelConsts';
 import {
+  LOAD_PEOPLE_WITH_HEARINGS_BUT_NO_CONTACTS,
   LOAD_REMINDER_NEIGHBORS,
   LOAD_REMINDERS_FOR_DATE,
+  loadPeopleWithHearingsButNoContacts,
   loadReminderNeighborsById,
   loadRemindersforDate
 } from './RemindersActionFactory';
@@ -57,6 +59,22 @@ const getApp = state => state.get(STATE.APP, Map());
 const getEDM = state => state.get(STATE.EDM, Map());
 const getOrgId = state => state.getIn([STATE.APP, APP.SELECTED_ORG_ID], '');
 
+function* getAllSearchResults(entitySetId :string, searchTerm :string) :Generator<*, *, *> {
+  const loadSizeRequest = {
+    searchTerm,
+    start: 0,
+    maxHits: 1
+  };
+  const response = yield call(SearchApi.searchEntitySetData, entitySetId, loadSizeRequest);
+  const { numHits } = response;
+
+  const loadResultsRequest = {
+    searchTerm,
+    start: 0,
+    maxHits: numHits
+  };
+  return yield call(SearchApi.searchEntitySetData, entitySetId, loadResultsRequest);
+}
 
 function* loadRemindersforDateWorker(action :SequenceAction) :Generator<*, *, *> {
 
@@ -227,9 +245,111 @@ function* loadReminderNeighborsByIdWorker(action :SequenceAction) :Generator<*, 
 function* loadReminderNeighborsByIdWatcher() :Generator<*, *, *> {
   yield takeEvery(LOAD_REMINDER_NEIGHBORS, loadReminderNeighborsByIdWorker);
 }
+function* loadPeopleWithHearingsButNoContactsWorker(action :SequenceAction) :Generator<*, *, *> {
+  try {
+    yield put(loadPeopleWithHearingsButNoContacts.request(action.id));
+
+    let peopleWithOpenPSAsandHearingsButNoContactById = Map();
+
+    const app = yield select(getApp);
+    const edm = yield select(getEDM);
+    const orgId = yield select(getOrgId);
+    const statusfqn = new FullyQualifiedName(PROPERTY_TYPES.STATUS);
+    const entitySetIdsToAppType = app.getIn([APP.ENTITY_SETS_BY_ORG, orgId]);
+    const psaScoresEntitySetId = getEntitySetId(app, PSA_SCORES, orgId);
+    const peopleEntitySetId = getEntitySetId(app, PEOPLE, orgId);
+    const hearingEntityKeyId = getEntitySetId(app, HEARINGS, orgId);
+    const contactInformationEntityKeyId = getEntitySetId(app, CONTACT_INFORMATION, orgId);
+
+    /* Grab Open PSAs */
+    const statusPropertyTypeId = getPropertyTypeId(edm, statusfqn);
+    const filter = action.value || PSA_STATUSES.OPEN;
+    const searchTerm = action.value === '*' ? action.value : `${statusPropertyTypeId}:"${filter}"`;
+    const allScoreData = yield call(getAllSearchResults, psaScoresEntitySetId, searchTerm);
+    const scoreIds = fromJS(allScoreData.hits).map(score => score.getIn([OPENLATTICE_ID_FQN, 0], ''));
+
+    /* Grab people for all Open PSAs */
+    let psaNeighborsById = yield call(SearchApi.searchEntityNeighborsWithFilter, psaScoresEntitySetId, {
+      entityKeyIds: scoreIds.toJS(),
+      sourceEntitySetIds: [psaScoresEntitySetId],
+      destinationEntitySetIds: [peopleEntitySetId, hearingEntityKeyId]
+    });
+    psaNeighborsById = fromJS(psaNeighborsById);
+
+    /* Filter for people with hearings */
+    psaNeighborsById.entrySeq().forEach(([_, neighbors]) => {
+      let person;
+      let hasFutureHearing = false;
+      neighbors.forEach((neighbor) => {
+        const entitySetId = neighbor.getIn([PSA_NEIGHBOR.ENTITY_SET, 'id'], '');
+        const appTypeFqn = entitySetIdsToAppType.get(entitySetId, '');
+        const isHearing = appTypeFqn === HEARINGS;
+        const isPerson = appTypeFqn === PEOPLE;
+        if (isHearing) {
+          const hearingDateTime = neighbor.getIn([PSA_NEIGHBOR.DETAILS, PROPERTY_TYPES.DATE_TIME, 0]);
+          const hearingExists = !!hearingDateTime;
+          const hearingType = neighbor.getIn([PSA_NEIGHBOR.DETAILS, PROPERTY_TYPES.HEARING_TYPE, 0]);
+          const hearingHasBeenCancelled = neighbor.getIn([PSA_NEIGHBOR.DETAILS, PROPERTY_TYPES.UPDATE_TYPE, 0], '')
+            .toLowerCase().trim() === 'cancelled';
+          const hearingInFuture = moment().isBefore(moment(hearingDateTime));
+          if (hearingType
+            && hearingExists
+            && hearingInFuture
+            && !hearingHasBeenCancelled
+          ) hasFutureHearing = true;
+        }
+        if (isPerson) person = neighbor;
+      });
+      if (hasFutureHearing && person) {
+        const entityKeyId = person.getIn([PSA_NEIGHBOR.DETAILS, OPENLATTICE_ID_FQN, 0], '');
+        peopleWithOpenPSAsandHearingsButNoContactById = peopleWithOpenPSAsandHearingsButNoContactById
+          .set(entityKeyId, person);
+      }
+    });
+
+    /* Grab people neighbors */
+    let peopleNeighborsById = yield call(SearchApi.searchEntityNeighborsWithFilter, peopleEntitySetId, {
+      entityKeyIds: peopleWithOpenPSAsandHearingsButNoContactById.keySeq().toJS(),
+      sourceEntitySetIds: [],
+      destinationEntitySetIds: [contactInformationEntityKeyId]
+    });
+    peopleNeighborsById = fromJS(peopleNeighborsById);
+
+    peopleNeighborsById.entrySeq().forEach(([id, neighbors]) => {
+      let hasPreferredContact = false;
+      neighbors.forEach((neighbor) => {
+        const entitySetId = neighbor.getIn([PSA_NEIGHBOR.ENTITY_SET, 'id'], '');
+        const appTypeFqn = entitySetIdsToAppType.get(entitySetId, '');
+        const isContactInformation = appTypeFqn === CONTACT_INFORMATION;
+        if (isContactInformation) {
+          const isPreferred = neighbor.getIn([PSA_NEIGHBOR.DETAILS, PROPERTY_TYPES.PREFERRED, 0], false);
+          if (isPreferred) hasPreferredContact = true;
+        }
+      });
+      if (hasPreferredContact) {
+        peopleWithOpenPSAsandHearingsButNoContactById = peopleWithOpenPSAsandHearingsButNoContactById.delete(id);
+      }
+    });
+
+    yield put(loadPeopleWithHearingsButNoContacts
+      .success(action.id, { peopleWithOpenPSAsandHearingsButNoContactById }));
+  }
+  catch (error) {
+    console.error(error);
+    yield put(loadPeopleWithHearingsButNoContacts.failure(action.id, error));
+  }
+  finally {
+    yield put(loadPeopleWithHearingsButNoContacts.finally(action.id));
+  }
+}
+
+function* loadPeopleWithHearingsButNoContactsWatcher() :Generator<*, *, *> {
+  yield takeEvery(LOAD_PEOPLE_WITH_HEARINGS_BUT_NO_CONTACTS, loadPeopleWithHearingsButNoContactsWorker);
+}
 
 
 export {
+  loadPeopleWithHearingsButNoContactsWatcher,
   loadRemindersforDateWatcher,
   loadReminderNeighborsByIdWatcher
 };
