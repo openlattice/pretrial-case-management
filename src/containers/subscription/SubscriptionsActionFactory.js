94322e701aec31686815014ede2b38859c5692b4
Todd Bergman <todd@openlattice.com>
2019-02-08 15:30:15 -0800

    Added a clear subscription modal to clear the modal state

--- src/containers/subscription/SubscriptionsActionFactory.js
+++ src/containers/subscription/SubscriptionsActionFactory.js
@@ -4,10 +4,16 @@
 
 import { newRequestSequence } from 'redux-reqseq';
 
+const CLEAR_SUBSCRIPTION_MODAL :string = 'CLEAR_SUBSCRIPTION_MODAL';
+const clearSubscriptionModal :RequestSequence = newRequestSequence(CLEAR_SUBSCRIPTION_MODAL);
+
+
 const LOAD_SUBSCRIPTION_MODAL :string = 'LOAD_SUBSCRIPTION_MODAL';
 const loadSubcriptionModal :RequestSequence = newRequestSequence(LOAD_SUBSCRIPTION_MODAL);
 
 export {
+  CLEAR_SUBSCRIPTION_MODAL,
   LOAD_SUBSCRIPTION_MODAL,
+  clearSubscriptionModal,
   loadSubcriptionModal
 };
