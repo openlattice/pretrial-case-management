370e3138a03723edbad9829b8de7ec67903d7223
Todd Bergman <todd@openlattice.com>
2019-02-23 01:06:29 -0700

    Refactored reminders table to handle opt outs and reminders

--- src/components/reminders/RemindersTable.js
+++ src/components/reminders/RemindersTable.js
@@ -7,20 +7,35 @@ import styled from 'styled-components';
 import { Map } from 'immutable';
 
 import RemindersRow from './RemindersRow';
+import OptOutRow from './OptOutRow';
 import { NoResults } from '../../utils/Layout';
-import { getReminderFields, sortReminders, REMINDERS_HEADERS } from '../../utils/RemindersUtils';
 import { APP_TYPES_FQNS, PROPERTY_TYPES } from '../../utils/consts/DataModelConsts';
 import { PSA_NEIGHBOR } from '../../utils/consts/FrontEndStateConsts';
 import { formatPeopleInfo } from '../../utils/PeopleUtils';
 import { getHearingFields } from '../../utils/consts/HearingConsts';
+import {
+  getReminderFields,
+  getOptOutFields,
+  sortEntities,
+  REMINDERS_HEADERS,
+  OPT_OUT_HEADERS
+} from '../../utils/RemindersUtils';
 
 import { OL } from '../../utils/consts/Colors';
 
-let { PEOPLE, CONTACT_INFORMATION, HEARINGS } = APP_TYPES_FQNS;
+let {
+  CONTACT_INFORMATION,
+  HEARINGS,
+  PEOPLE,
+  REMINDERS,
+  REMINDER_OPT_OUTS
+} = APP_TYPES_FQNS;
 
 PEOPLE = PEOPLE.toString();
 CONTACT_INFORMATION = CONTACT_INFORMATION.toString();
 HEARINGS = HEARINGS.toString();
+REMINDERS = REMINDERS.toString();
+REMINDER_OPT_OUTS = REMINDER_OPT_OUTS.toString();
 
 const Table = styled.table`
   width: 100%;
@@ -44,75 +59,146 @@ const HeaderElement = styled.th`
 `;
 
 const NoResultsForTable = styled(NoResults)`
-  padding: 100px 0;
+  padding: 50px 0;
 `;
 
 class RemindersTable extends React.Component<Props, State> {
-  renderHeaders = () => (
-    <HeaderRow>
-      <HeaderElement>{REMINDERS_HEADERS.COURT_TIME}</HeaderElement>
-      <HeaderElement>{REMINDERS_HEADERS.NAME}</HeaderElement>
-      <HeaderElement>{REMINDERS_HEADERS.CONTACT}</HeaderElement>
-      <HeaderElement>{REMINDERS_HEADERS.COURTROOM}</HeaderElement>
-      <HeaderElement>{REMINDERS_HEADERS.HEARING_TYPE}</HeaderElement>
-      <HeaderElement>{REMINDERS_HEADERS.STATUS}</HeaderElement>
-      <HeaderElement>{REMINDERS_HEADERS.PSA_STATUS}</HeaderElement>
-    </HeaderRow>
-  )
+
+  renderHeaders = () => {
+    const { appTypeFqn } = this.props;
+    let headers = null;
+
+    if (appTypeFqn === REMINDERS) {
+      headers = (
+        <HeaderRow>
+          <HeaderElement>{REMINDERS_HEADERS.COURT_TIME}</HeaderElement>
+          <HeaderElement>{REMINDERS_HEADERS.NAME}</HeaderElement>
+          <HeaderElement>{REMINDERS_HEADERS.CONTACT}</HeaderElement>
+          <HeaderElement>{REMINDERS_HEADERS.COURTROOM}</HeaderElement>
+          <HeaderElement>{REMINDERS_HEADERS.HEARING_TYPE}</HeaderElement>
+          <HeaderElement>{REMINDERS_HEADERS.STATUS}</HeaderElement>
+          <HeaderElement>{REMINDERS_HEADERS.PSA_STATUS}</HeaderElement>
+        </HeaderRow>
+      );
+    }
+    if (appTypeFqn === REMINDER_OPT_OUTS) {
+      headers = (
+        <HeaderRow>
+          <HeaderElement>{OPT_OUT_HEADERS.OPT_OUT_TIME}</HeaderElement>
+          <HeaderElement>{OPT_OUT_HEADERS.NAME}</HeaderElement>
+          <HeaderElement>{OPT_OUT_HEADERS.CONTACT}</HeaderElement>
+          <HeaderElement>{OPT_OUT_HEADERS.REASON}</HeaderElement>
+        </HeaderRow>
+      );
+    }
+
+    return headers;
+  }
+
+  getNeighborDetails = (entityKeyId, neighbors) => {
+    const person = neighbors.getIn([entityKeyId, PEOPLE, PSA_NEIGHBOR.DETAILS], Map());
+    const hearing = neighbors.getIn([entityKeyId, HEARINGS, PSA_NEIGHBOR.DETAILS], Map());
+    const contactInfo = neighbors.getIn([entityKeyId, CONTACT_INFORMATION, PSA_NEIGHBOR.DETAILS], Map());
+    const {
+      identification,
+      lastFirstMid
+    } = formatPeopleInfo(person);
+    const {
+      courtroom,
+      hearingDate,
+      hearingTime,
+      hearingType
+    } = getHearingFields(hearing);
+    const contact = contactInfo.get(PROPERTY_TYPES.PHONE);
+    const hearingDateTime = `${hearingDate} ${hearingTime}`;
+
+    return ({
+      identification,
+      lastFirstMid,
+      courtroom,
+      hearingDate,
+      hearingTime,
+      hearingType,
+      contact,
+      hearingDateTime
+    });
+  }
 
   render() {
     const {
-      reminders,
+      appTypeFqn,
+      entities,
       neighbors,
       remindersWithOpenPSA,
       noResults
     } = this.props;
     if (noResults) return <NoResultsForTable>No Results</NoResultsForTable>;
-    const reminderSeq = sortReminders(reminders, neighbors)
-      .map(((reminder) => {
-        const {
-          reminderId,
-          dateTime,
-          wasNotified,
-          entityKeyId
-        } = getReminderFields(reminder);
-        const person = neighbors.getIn([entityKeyId, PEOPLE, PSA_NEIGHBOR.DETAILS], Map());
-        const hearing = neighbors.getIn([entityKeyId, HEARINGS, PSA_NEIGHBOR.DETAILS], Map());
-        const contactInfo = neighbors.getIn([entityKeyId, CONTACT_INFORMATION, PSA_NEIGHBOR.DETAILS], Map());
-        const {
-          identification,
-          lastFirstMid
-        } = formatPeopleInfo(person);
-        const {
-          courtroom,
-          hearingDate,
-          hearingTime,
-          hearingType
-        } = getHearingFields(hearing);
-        const contact = contactInfo.get(PROPERTY_TYPES.PHONE);
-        const hearingDateTime = `${hearingDate} ${hearingTime}`;
+    console.log(entities.toJS());
+    console.log(neighbors.toJS());
+    const entitySeq = sortEntities(entities, neighbors)
+      .map(((entity) => {
+        let row = null;
+        if (appTypeFqn === REMINDERS) {
+          const {
+            reminderId,
+            entityKeyId,
+            dateTime,
+            wasNotified
+          } = getReminderFields(entity);
+          const {
+            identification,
+            lastFirstMid,
+            courtroom,
+            hearingType,
+            contact,
+            hearingDateTime
+          } = this.getNeighborDetails(entityKeyId, neighbors);
+          const hasOpenPSA = remindersWithOpenPSA.includes(reminderId);
 
-        const hasOpenPSA = remindersWithOpenPSA.includes(reminderId);
-        return (
-          <RemindersRow
-              key={reminderId}
-              contact={contact}
-              courtroom={courtroom}
-              hearingTime={hearingDateTime}
-              hearingType={hearingType}
-              reminderId={reminderId}
-              time={moment(dateTime).format('HH:mm')}
-              wasNotified={wasNotified}
-              personId={identification}
-              personName={lastFirstMid}
-              hasOpenPSA={hasOpenPSA} />
-        );
+          row = (
+            <RemindersRow
+                key={reminderId}
+                contact={contact}
+                courtroom={courtroom}
+                hearingTime={hearingDateTime}
+                hearingType={hearingType}
+                reminderId={reminderId}
+                time={moment(dateTime).format('HH:mm')}
+                wasNotified={wasNotified}
+                personId={identification}
+                personName={lastFirstMid}
+                hasOpenPSA={hasOpenPSA} />
+          );
+        }
+        if (appTypeFqn === REMINDER_OPT_OUTS) {
+          const {
+            dateTime,
+            entityKeyId,
+            reason
+          } = getOptOutFields(entity);
+          const {
+            identification,
+            lastFirstMid,
+            contact
+          } = this.getNeighborDetails(entityKeyId, neighbors);
+          row = (
+            <OptOutRow
+                key={entityKeyId}
+                contact={contact}
+                optOutId={entityKeyId}
+                reason={reason}
+                time={moment(dateTime).format('HH:mm')}
+                personId={identification}
+                personName={lastFirstMid} />
+          );
+        }
+        return row;
       }));
     return (
       <Table>
         <tbody>
           { this.renderHeaders() }
-          { reminderSeq }
+          { entitySeq }
         </tbody>
       </Table>
     );
