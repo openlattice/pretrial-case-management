8a2c6c5c5b626b1d7493e6b0f1352dc21fd820e4
Todd Bergman <todd@openlattice.com>
2019-03-11 13:44:49 -0700

    no need for remindersWithOpenPSAs

--- src/containers/reminders/RemindersReducer.js
+++ src/containers/reminders/RemindersReducer.js
@@ -103,10 +103,9 @@ export default function remindersReducer(state :Map<*, *> = INITIAL_STATE, actio
       return loadReminderNeighborsById.reducer(state, action, {
         REQUEST: () => state.set(REMINDERS.LOADING_REMINDER_NEIGHBORS, true),
         SUCCESS: () => {
-          const { reminderNeighborsById, reminderIdsWithOpenPSAs } = action.value;
+          const { reminderNeighborsById } = action.value;
           return state
             .set(REMINDERS.REMINDER_NEIGHBORS, reminderNeighborsById)
-            .set(REMINDERS.REMINDERS_WITH_OPEN_PSA_IDS, reminderIdsWithOpenPSAs);
         },
         FINALLY: () => state.set(REMINDERS.LOADING_REMINDER_NEIGHBORS, false)
       });
