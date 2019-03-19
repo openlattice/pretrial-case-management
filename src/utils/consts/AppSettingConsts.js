35011ab667694fe8d5fc00af853a51c64342e629
Todd Bergman <todd@openlattice.com>
2019-03-18 18:36:16 -0700

    created AppSettingsConsts

---
+++ src/utils/consts/AppSettingConsts.js
@@ -0,0 +1,23 @@
+
+export const CASE_CONTEXTS = {
+  ARREST: 'arrest',
+  COURT: 'court'
+};
+
+export const CONTEXTS = {
+  COURT: 'court',
+  BOOKING: 'booking'
+};
+
+export const MODULE = {
+  PSA: 'psa',
+  PRETRIAL: 'pretrial',
+};
+
+
+export const SETTINGS = {
+  CONTEXTS: 'contexts',
+  LOAD_CASES: 'loadCasesOnTheFly',
+  COURT_REMINDERS: 'courtRemindersEnabled',
+  MODULES: 'modules'
+};
