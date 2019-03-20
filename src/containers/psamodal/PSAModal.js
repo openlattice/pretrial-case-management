9347d706bdf04d617f3934ae4efd355f2883f2cf
Todd Bergman <todd@openlattice.com>
2019-03-19 17:41:01 -0700

    Not being used

--- src/containers/psamodal/PSAModal.js
+++ src/containers/psamodal/PSAModal.js
@@ -656,7 +656,6 @@ class PSAModal extends React.Component<Props, State> {
       lastEditDateForPSA
     );
 
-    const includesPretrialModule = selectedOrganizationSettings.getIn([SETTINGS.MODULES, MODULE.PRETRIAL], '');
     const psaContext = psaNeighbors.getIn([PSA_RISK_FACTORS, PSA_NEIGHBOR.DETAILS, PROPERTY_TYPES.CONTEXT, 0], '');
     const caseContext = psaContext === CONTEXT.BOOKING ? CONTEXTS.BOOKING : CONTEXTS.COURT;
     // Get Case Context from settings and pass to config
