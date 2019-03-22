74c3e1e355d73baceb661621ba5de8d7f4d11a29
Todd Bergman <todd@openlattice.com>
2019-03-22 11:13:04 -0700

    Add clearManualRemindersForm

--- src/containers/manualreminders/ManualRemindersActionFactory.js
+++ src/containers/manualreminders/ManualRemindersActionFactory.js
@@ -4,6 +4,9 @@
 
 import { newRequestSequence } from 'redux-reqseq';
 
+const CLEAR_MANUAL_REMINDERS_FORM :string = 'CLEAR_MANUAL_REMINDERS_FORM';
+const clearManualRemindersForm :RequestSequence = newRequestSequence(CLEAR_MANUAL_REMINDERS_FORM);
+
 const LOAD_MANUAL_REMINDERS_FORM :string = 'LOAD_MANUAL_REMINDERS_FORM';
 const loadManualRemindersForm :RequestSequence = newRequestSequence(LOAD_MANUAL_REMINDERS_FORM);
 
@@ -14,9 +17,11 @@ const LOAD_MANUAL_REMINDERS_NEIGHBORS :string = 'LOAD_MANUAL_REMINDERS_NEIGHBORS
 const loadManualRemindersNeighborsById :RequestSequence = newRequestSequence(LOAD_MANUAL_REMINDERS_NEIGHBORS);
 
 export {
+  CLEAR_MANUAL_REMINDERS_FORM,
   LOAD_MANUAL_REMINDERS_FORM,
   LOAD_MANUAL_REMINDERS,
   LOAD_MANUAL_REMINDERS_NEIGHBORS,
+  clearManualRemindersForm,
   loadManualRemindersForm,
   loadManualRemindersForDate,
   loadManualRemindersNeighborsById,
