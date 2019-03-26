c5725d407c2d83f7d345780628c6309e5e2d3d1a
Todd Bergman <todd@openlattice.com>
2019-03-25 21:00:33 -0700

    Refactor to use dataUtils

--- src/utils/ContactInfoUtils.js
+++ src/utils/ContactInfoUtils.js
@@ -1,20 +1,29 @@
-import { Constants } from 'lattice';
 
 import { CONTACT_METHODS } from './consts/ContactInfoConsts';
 import { PROPERTY_TYPES } from './consts/DataModelConsts';
-import { PSA_NEIGHBOR } from './consts/FrontEndStateConsts';
+import { getEntityKeyId, getFirstNeighborValue } from './DataUtils';
 
-const { OPENLATTICE_ID_FQN } = Constants;
+export const getContactInfoFields = (contact) => {
+  const phone = getFirstNeighborValue(contact, PROPERTY_TYPES.PHONE);
+  const email = getFirstNeighborValue(contact, PROPERTY_TYPES.EMAIL);
+  const contactMethod = getFirstNeighborValue(contact, PROPERTY_TYPES.CONTACT_METHOD);
+  const isMobile = getFirstNeighborValue(contact, PROPERTY_TYPES.IS_MOBILE, false);
+  const isPreferred = getFirstNeighborValue(contact, PROPERTY_TYPES.IS_PREFERRED, false);
+  const contactEntityKeyId = getEntityKeyId(contact);
+  return {
+    contactEntityKeyId,
+    phone,
+    email,
+    contactMethod,
+    isMobile,
+    isPreferred,
+  };
+};
 
 export const filterContactsByType = (contacts, method) => {
   if (method) {
     return contacts.filter((contact) => {
-      const contactMethod = contact.getIn([PROPERTY_TYPES.CONTACT_METHOD, 0],
-        contact.getIn([PSA_NEIGHBOR.DETAILS, PROPERTY_TYPES.CONTACT_METHOD, 0], ''));
-      const phone = contact.getIn([PROPERTY_TYPES.PHONE, 0],
-        contact.getIn([PSA_NEIGHBOR.DETAILS, PROPERTY_TYPES.PHONE, 0], ''));
-      const email = contact.getIn([PROPERTY_TYPES.EMAIL, 0],
-        contact.getIn([PSA_NEIGHBOR.DETAILS, PROPERTY_TYPES.EMAIL, 0], ''));
+      const { contactMethod, email, phone } = getContactInfoFields(contact);
       if (contactMethod) return (contactMethod === method);
       if (phone && method === CONTACT_METHODS.PHONE) return true;
       if (email && method === CONTACT_METHODS.EMAIL) return true;
@@ -31,24 +40,6 @@ export const formatPhoneNumber = (phone) => {
     return `(${match[1]}) ${match[2]}-${match[3]}`;
   }
   return null;
-};
-
-export const getContactInfoFields = (contact) => {
-  const contactDetails = contact.get(PSA_NEIGHBOR.DETAILS, contact);
-  const phone = contactDetails.getIn([PROPERTY_TYPES.PHONE, 0], '');
-  const email = contactDetails.getIn([PROPERTY_TYPES.EMAIL, 0], '');
-  const contactMethod = contactDetails.getIn([PROPERTY_TYPES.CONTACT_METHOD, 0], '');
-  const isMobile = contactDetails.getIn([PROPERTY_TYPES.IS_MOBILE, 0], '');
-  const isPreferred = contactDetails.getIn([PROPERTY_TYPES.IS_PREFERRED, 0], '');
-  const contactEntityKeyId = contactDetails.getIn([OPENLATTICE_ID_FQN, 0], '');
-  return {
-    contactEntityKeyId,
-    phone,
-    email,
-    contactMethod,
-    isMobile,
-    isPreferred,
-  };
 };
 
 export const phoneIsValid = phone => (
