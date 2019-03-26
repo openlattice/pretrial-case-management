33cffc61dbdae2a9d9210e30eb744f92b94d61c7
Todd Bergman <todd@openlattice.com>
2019-03-25 17:00:42 -0700

    Deteleted modals and moved to parent component

--- src/components/subscription/PersonSubscriptionRow.js
+++ src/components/subscription/PersonSubscriptionRow.js
@@ -3,19 +3,14 @@
  */
 import React from 'react';
 import styled from 'styled-components';
-import { Constants } from 'lattice';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCog, faPaperPlane } from '@fortawesome/pro-light-svg-icons';
 
-import ManageSubscriptionModal from '../../containers/subscription/ManageSubscriptionModal';
-import CreateManualReminderModal from '../manualreminders/CreateManualReminderModal';
 import StyledButton from '../buttons/StyledButton';
 import { OL } from '../../utils/consts/Colors';
 import { PROPERTY_TYPES } from '../../utils/consts/DataModelConsts';
 import { formatPeopleInfo } from '../../utils/PeopleUtils';
-
-const { OPENLATTICE_ID_FQN } = Constants;
 
 const Row = styled.div`
   width: 100%;
@@ -52,76 +47,24 @@ const OpenCreateManualReminderButton = styled(ManageSubscriptionButton)`
 `;
 
 class PersonSubscriptionRow extends React.Component<Props, State> {
-  constructor(props :Props) {
-    super(props);
-    this.state = {
-      manageSubscriptionModalOpen: false,
-      creatingManualReminder: false
-    };
-  }
 
-  onClose = () => {
-    const { onClose } = this.props;
-    this.setState({
-      manageSubscriptionModalOpen: false,
-      creatingManualReminder: false
-    });
-    onClose();
-  };
-
-  onCreateSubscriptionClose = () => {
-    this.setState({ creatingManualReminder: false });
-  }
-
-  openManageSubscriptionModal = () => {
-    const { person, loadNeighbors } = this.props;
-    const personId = person.getIn([OPENLATTICE_ID_FQN, 0], '');
-    loadNeighbors({ personId });
-    this.setState({ manageSubscriptionModalOpen: true });
-  };
-
-  renderManageSubscriptionModal = () => {
-    const { manageSubscriptionModalOpen } = this.state;
-    const { person } = this.props;
+  renderManageSubscriptionButton = () => {
+    const { person, openManageSubscriptionModal } = this.props;
     return (
-      <ManageSubscriptionModal
-          person={person}
-          open={manageSubscriptionModalOpen}
-          onClose={this.onClose} />
+      <ManageSubscriptionButton onClick={() => openManageSubscriptionModal(person)}>
+        <FontAwesomeIcon icon={faCog} height="12px" />
+        {' Settings'}
+      </ManageSubscriptionButton>
     );
   }
-
-  openCreateManualReminderModal = () => {
-    const { person, loadManualRemindersForm } = this.props;
-    const personId = person.getIn([OPENLATTICE_ID_FQN, 0], '');
-    loadManualRemindersForm({ personId });
-    this.setState({ creatingManualReminder: true });
-  };
-
-  renderCreateManualReminderModal = () => {
-    const { creatingManualReminder } = this.state;
-    const { person } = this.props;
+  renderManualReminderButton = () => {
+    const { person, openCreateManualReminderModal } = this.props;
     return (
-      <CreateManualReminderModal
-          person={person}
-          open={creatingManualReminder}
-          onClose={this.onClose} />
+      <OpenCreateManualReminderButton onClick={() => openCreateManualReminderModal(person)}>
+        <FontAwesomeIcon icon={faPaperPlane} height="12px" />
+      </OpenCreateManualReminderButton>
     );
   }
-
-  renderManageSubscriptionButton = () => (
-    <ManageSubscriptionButton onClick={this.openManageSubscriptionModal}>
-      <FontAwesomeIcon icon={faCog} height="12px" />
-      {' Settings'}
-    </ManageSubscriptionButton>
-  )
-
-  renderManualReminderButton = () => (
-    <OpenCreateManualReminderButton onClick={this.openCreateManualReminderModal}>
-      <FontAwesomeIcon icon={faPaperPlane} height="12px" />
-    </OpenCreateManualReminderButton>
-  )
-
   render() {
     const { contact, person } = this.props;
     const { lastFirstMid } = formatPeopleInfo(person);
@@ -132,8 +75,6 @@ class PersonSubscriptionRow extends React.Component<Props, State> {
       <Row includeContact={phone}>
         <BodyElement>
           {lastFirstMid}
-          { this.renderManageSubscriptionModal() }
-          { this.renderCreateManualReminderModal() }
         </BodyElement>
         { phone ? <BodyElement>{phone}</BodyElement> : null }
         <BodyElement>
