d1643891aea48e700e8946bf81f67894d70592f5
Todd Bergman <todd@openlattice.com>
2019-04-15 17:22:42 -0700

    Created checkinConsts

---
+++ src/utils/consts/CheckInConsts.js
@@ -0,0 +1,28 @@
+export const CHECKIN_HEADERS = {
+  TIME: 'Time',
+  NAME: 'Name',
+  CONTACT: 'Contact',
+  COURT_TIME: 'Hearing Time',
+  COURTROOM: 'Courtroom',
+  HEARING_TYPE: 'Type',
+  CASE_NUM: 'Case',
+  STATUS: 'Status'
+};
+
+export const FILTERS = {
+  ALL: 'All',
+  FAILED: 'Failed',
+  SUCCESSFUL: 'Successful',
+  PENDING: 'Pending'
+};
+
+export const RESULT_TYPE = {
+  ACCEPT: 'Accept',
+  REJECT: 'Reject'
+};
+
+export const CONFIDENCE_LEVEL = {
+  HIGH: 'High',
+  NORMAL: 'Normal',
+  LOW: 'Low'
+};
