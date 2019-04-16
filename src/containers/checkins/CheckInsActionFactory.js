33c6f8b686e4df96b7d8f3df13663958881469ea
Todd Bergman <todd@openlattice.com>
2019-04-15 17:19:31 -0700

    No loading of check in form -load checkIns and checkIn neighbors though

--- src/containers/checkins/CheckInsActionFactory.js
+++ src/containers/checkins/CheckInsActionFactory.js
@@ -4,15 +4,15 @@
 
 import { newRequestSequence } from 'redux-reqseq';
 
-const CLEAR_CHECK_INS_FORM :string = 'CLEAR_CHECK_INS_FORM';
-const clearCheckInsForm :RequestSequence = newRequestSequence(CLEAR_CHECK_INS_FORM);
+const LOAD_CHECKIN_APPOINTMENTS_FOR_DATE :string = 'LOAD_CHECKIN_APPOINTMENTS_FOR_DATE';
+const loadCheckInAppointmentsForDate :RequestSequence = newRequestSequence(LOAD_CHECKIN_APPOINTMENTS_FOR_DATE);
 
-const LOAD_CHECK_INS_FORM :string = 'LOAD_CHECK_INS_FORM';
-const loadCheckInsForm :RequestSequence = newRequestSequence(LOAD_CHECK_INS_FORM);
+const LOAD_CHECK_IN_NEIGHBORS :string = 'LOAD_CHECK_IN_NEIGHBORS';
+const loadCheckInNeighbors :RequestSequence = newRequestSequence(LOAD_CHECK_IN_NEIGHBORS);
 
 export {
-  CLEAR_CHECK_INS_FORM,
-  LOAD_CHECK_INS_FORM,
-  clearCheckInsForm,
-  loadCheckInsForm
+  LOAD_CHECKIN_APPOINTMENTS_FOR_DATE,
+  LOAD_CHECK_IN_NEIGHBORS,
+  loadCheckInAppointmentsForDate,
+  loadCheckInNeighbors
 };
