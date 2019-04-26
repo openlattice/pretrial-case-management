0394fd62d09e6eadbd906f60cfbca53c6af5236a
Todd Bergman <todd@openlattice.com>
2019-04-26 14:45:59 -0700

    Added loadArrestingAgencies to ChargesActionFactory

--- src/containers/charges/ChargesActionFactory.js
+++ src/containers/charges/ChargesActionFactory.js
@@ -10,15 +10,20 @@ const deleteCharge :RequestSequence = newRequestSequence(DELETE_CHARGE);
 const UPDATE_CHARGE :string = 'UPDATE_CHARGE';
 const updateCharge :RequestSequence = newRequestSequence(UPDATE_CHARGE);
 
+const LOAD_ARRESTING_AGENCIES :string = 'LOAD_ARRESTING_AGENCIES';
+const loadArrestingAgencies :RequestSequence = newRequestSequence(LOAD_ARRESTING_AGENCIES);
+
 const LOAD_CHARGES :string = 'LOAD_CHARGES';
 const loadCharges :RequestSequence = newRequestSequence(LOAD_CHARGES);
 
 
 export {
   DELETE_CHARGE,
+  LOAD_ARRESTING_AGENCIES,
   LOAD_CHARGES,
   UPDATE_CHARGE,
   deleteCharge,
+  loadArrestingAgencies,
   loadCharges,
   updateCharge
 };
