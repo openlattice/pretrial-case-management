35caec966ff08d5b492de12875f3ccfeff1f569f
Todd Bergman <todd@openlattice.com>
2019-05-12 15:14:41 -0700

    removed refreshHearingNeighbors from courtActionFactory

--- src/containers/court/CourtActionFactory.js
+++ src/containers/court/CourtActionFactory.js
@@ -16,9 +16,6 @@ const loadHearingsForDate :RequestSequence = newRequestSequence(LOAD_HEARINGS_FO
 const LOAD_HEARING_NEIGHBORS :string = 'LOAD_HEARING_NEIGHBORS';
 const loadHearingNeighbors :RequestSequence = newRequestSequence(LOAD_HEARING_NEIGHBORS);
 
-const REFRESH_HEARING_NEIGHBORS :string = 'REFRESH_HEARING_NEIGHBORS';
-const refreshHearingNeighbors :RequestSequence = newRequestSequence(REFRESH_HEARING_NEIGHBORS);
-
 const LOAD_JUDGES :string = 'LOAD_JUDGES';
 const loadJudges :RequestSequence = newRequestSequence(LOAD_JUDGES);
 
@@ -30,14 +27,12 @@ export {
   FILTER_PEOPLE_IDS_WITH_OPEN_PSAS,
   LOAD_HEARINGS_FOR_DATE,
   LOAD_HEARING_NEIGHBORS,
-  REFRESH_HEARING_NEIGHBORS,
   LOAD_JUDGES,
   SET_COURT_DATE,
   changeHearingFilters,
   filterPeopleIdsWithOpenPSAs,
   loadHearingsForDate,
   loadHearingNeighbors,
-  refreshHearingNeighbors,
   loadJudges,
   setCourtDate
 };
