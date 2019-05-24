6851489d25843ff1cb9a7f71d27b3e028a194cb0
Todd Bergman <todd@openlattice.com>
2019-05-24 14:09:22 -0600

    Added, refreshingHearingAndNeighbors, hearing, hearingbyapptypefqn to state

--- src/containers/hearings/HearingsReducer.js
+++ src/containers/hearings/HearingsReducer.js
@@ -8,6 +8,7 @@ import {
   CLEAR_HEARING_SETTINGS,
   CLOSE_HEARING_SETTINGS_MODAL,
   OPEN_HEARING_SETTINGS_MODAL,
+  refreshHearingAndNeighbors,
   SET_HEARING_SETTINGS
 } from './HearingsActionFactory';
 
@@ -15,11 +16,14 @@ import { HEARINGS } from '../../utils/consts/FrontEndStateConsts';
 
 
 const INITIAL_STATE :Map<*, *> = fromJS({
-  [HEARINGS.SETTINGS_MODAL_OPEN]: false,
   [HEARINGS.DATE]: moment().format('MM/DD/YYYY'),
-  [HEARINGS.TIME]: '',
   [HEARINGS.COURTROOM]: '',
-  [HEARINGS.JUDGE]: ''
+  [HEARINGS.JUDGE]: '',
+  [HEARINGS.REFRESHING_HEARING_AND_NEIGHBORS]: false,
+  [HEARINGS.SETTINGS_MODAL_OPEN]: false,
+  [HEARINGS.TIME]: '',
+  [HEARINGS.UPDATED_HEARING]: Map(),
+  [HEARINGS.UPDATED_HEARING_NEIGHBORS]: Map()
 });
 
 export default function hearingsReducer(state :Map<*, *> = INITIAL_STATE, action :SequenceAction) {
@@ -30,6 +34,19 @@ export default function hearingsReducer(state :Map<*, *> = INITIAL_STATE, action
     case CLOSE_HEARING_SETTINGS_MODAL: return state.set(HEARINGS.SETTINGS_MODAL_OPEN, false);
 
     case OPEN_HEARING_SETTINGS_MODAL: return state.set(HEARINGS.SETTINGS_MODAL_OPEN, true);
+
+    case refreshHearingAndNeighbors.case(action.type): {
+      return refreshHearingAndNeighbors.reducer(state, action, {
+        REQUEST: () => state.set(HEARINGS.REFRESHING_HEARING_AND_NEIGHBORS, true),
+        SUCCESS: () => {
+          const { hearing, hearingNeighborsByAppTypeFqn } = action.value;
+          return state
+            .set(HEARINGS.UPDATED_HEARING, hearing)
+            .set(HEARINGS.UPDATED_HEARING_NEIGHBORS, hearingNeighborsByAppTypeFqn);
+        },
+        FINALLY: () => state.set(HEARINGS.REFRESHING_HEARING_AND_NEIGHBORS, false),
+      });
+    }
 
     case SET_HEARING_SETTINGS: {
       const {
