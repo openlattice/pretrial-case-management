aa5ca01be15070631f8846602393c29dc478c3c1
Todd Bergman <todd@openlattice.com>
2019-07-01 17:22:29 -0700

    added clear submitted hearing and laod hearing neighbors to hearing action factory

--- src/containers/hearings/HearingsActionFactory.js
+++ src/containers/hearings/HearingsActionFactory.js
@@ -8,8 +8,14 @@ import type { RequestSequence } from 'redux-reqseq';
 const CLEAR_HEARING_SETTINGS :string = 'CLEAR_HEARING_SETTINGS';
 const clearHearingSettings :RequestSequence = newRequestSequence(CLEAR_HEARING_SETTINGS);
 
+const CLEAR_SUBMITTED_HEARING :string = 'CLEAR_SUBMITTED_HEARING';
+const clearSubmittedHearing :RequestSequence = newRequestSequence(CLEAR_SUBMITTED_HEARING);
+
 const CLOSE_HEARING_SETTINGS_MODAL :string = 'CLOSE_HEARING_SETTINGS_MODAL';
 const closeHearingSettingsModal :RequestSequence = newRequestSequence(CLOSE_HEARING_SETTINGS_MODAL);
+
+const LOAD_HEARING_NEIGHBORS :string = 'LOAD_HEARING_NEIGHBORS';
+const loadHearingNeighbors :RequestSequence = newRequestSequence(LOAD_HEARING_NEIGHBORS);
 
 const REFRESH_HEARING_AND_NEIGHBORS :string = 'REFRESH_HEARING_AND_NEIGHBORS';
 const refreshHearingAndNeighbors :RequestSequence = newRequestSequence(REFRESH_HEARING_AND_NEIGHBORS);
@@ -25,13 +31,17 @@ const submitHearing :RequestSequence = newRequestSequence(SUBMIT_HEARING);
 
 export {
   CLEAR_HEARING_SETTINGS,
+  CLEAR_SUBMITTED_HEARING,
   CLOSE_HEARING_SETTINGS_MODAL,
+  LOAD_HEARING_NEIGHBORS,
   REFRESH_HEARING_AND_NEIGHBORS,
   OPEN_HEARING_SETTINGS_MODAL,
   SET_HEARING_SETTINGS,
   SUBMIT_HEARING,
   clearHearingSettings,
+  clearSubmittedHearing,
   closeHearingSettingsModal,
+  loadHearingNeighbors,
   refreshHearingAndNeighbors,
   openHearingSettingsModal,
   setHearingSettings,
