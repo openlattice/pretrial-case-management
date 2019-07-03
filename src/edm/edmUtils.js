1324d2be86f830f7e6c1e8c8ac06e72976118dbf
Todd Bergman <todd@openlattice.com>
2019-07-02 17:23:40 -0700

    added getMapFromEntityKeysToPropertyKeys

--- src/edm/edmUtils.js
+++ src/edm/edmUtils.js
@@ -1,9 +1,22 @@
 /*
  * @flow
  */
+import { Map } from 'immutable';
+
 import { EDM } from '../utils/consts/FrontEndStateConsts';
 
 export const getPropertyTypeId :string = (edm :Map, fqn :string) => edm.getIn([
   EDM.FQN_TO_ID,
   fqn
 ]);
+
+
+export const getMapFromEntityKeysToPropertyKeys = (entity :Object, entityKeyId :string, edm :Map) => {
+  let entityObject = Map();
+  Object.keys(entity).forEach((key) => {
+    const propertyTypeKeyId = getPropertyTypeId(edm, key);
+    const property = entity[key] ? [entity[key]] : [];
+    if (property.length) entityObject = entityObject.setIn([entityKeyId, propertyTypeKeyId], property);
+  });
+  return entityObject;
+};
