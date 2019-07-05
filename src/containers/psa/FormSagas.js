08342d55954cd71247eb40c49107ecb023b61201
Todd Bergman <todd@openlattice.com>
2019-07-05 16:26:42 -0500

    Added new submit PSA action to form sagas

--- src/containers/psa/FormSagas.js
+++ src/containers/psa/FormSagas.js
@@ -1,10 +1,16 @@
 /*
  * @flow
  */
+import moment from 'moment';
 import { fromJS, List, Map } from 'immutable';
 import type { SequenceAction } from 'redux-reqseq';
-import { SearchApiActions, SearchApiSagas } from 'lattice-sagas';
 import { Constants, EntityDataModelApi } from 'lattice';
+import {
+  DataApiActions,
+  DataApiSagas,
+  SearchApiActions,
+  SearchApiSagas
+} from 'lattice-sagas';
 import {
   call,
   put,
@@ -15,19 +21,35 @@ import {
 import { loadPSAData } from '../review/ReviewActionFactory';
 import { getEntitySetIdFromApp } from '../../utils/AppUtils';
 import { getEntityProperties } from '../../utils/DataUtils';
+import { hearingIsCancelled } from '../../utils/HearingUtils';
+import { getPropertyTypeId, getPropteryIdToValueMap } from '../../edm/edmUtils';
 import { APP_TYPES, PROPERTY_TYPES } from '../../utils/consts/DataModelConsts';
 import { APP, STATE, PSA_NEIGHBOR } from '../../utils/consts/FrontEndStateConsts';
 import { PSA_STATUSES } from '../../utils/consts/Consts';
 import {
   LOAD_DATA_MODEL,
   LOAD_NEIGHBORS,
+  SUBMIT_PSA,
+  UPDATE_PSA,
   loadDataModel,
-  loadNeighbors
+  loadNeighbors,
+  submitPSA,
+  updatePSA,
 } from './FormActionFactory';
 
-const { ENTITY_KEY_ID } = PROPERTY_TYPES;
+const {
+  COMPLETED_DATE_TIME,
+  ENTITY_KEY_ID,
+  GENERAL_ID,
+  TIMESTAMP,
+  STRING_ID
+} = PROPERTY_TYPES;
 
 const {
+  CALCULATED_FOR,
+  ASSESSED_BY,
+  CHARGED_WITH,
+  APPEARS_IN,
   ARREST_CASES,
   BONDS,
   CHARGES,
@@ -53,13 +75,19 @@ const {
   STAFF
 } = APP_TYPES;
 
+const { createAssociations, createEntityAndAssociationData, getEntityData } = DataApiActions;
+const { createAssociationsWorker, createEntityAndAssociationDataWorker, getEntityDataWorker } = DataApiSagas;
 const { searchEntityNeighborsWithFilter } = SearchApiActions;
 const { searchEntityNeighborsWithFilterWorker } = SearchApiSagas;
 
 const getApp = state => state.get(STATE.APP, Map());
+const getEDM = state => state.get(STATE.EDM, Map());
 const getOrgId = state => state.getIn([STATE.APP, APP.SELECTED_ORG_ID], '');
 
 const { OPENLATTICE_ID_FQN } = Constants;
+
+const LIST_ENTITY_SETS = List.of(STAFF, RELEASE_CONDITIONS, HEARINGS, PRETRIAL_CASES, CHECKIN_APPOINTMENTS);
+
 
 function* loadDataModelWorker(action :SequenceAction) :Generator<*, *, *> {
 
@@ -94,19 +122,6 @@ const getOpenPSAIds = (neighbors, psaScoresEntitySetId) => {
     if (neighbor.neighborEntitySet && neighbor.neighborEntitySet.id === psaScoresEntitySetId) {
       const statusValues = neighbor.neighborDetails[PROPERTY_TYPES.STATUS];
       if (statusValues && statusValues.includes(PSA_STATUSES.OPEN)) {
-        return true;
-      }
-    }
-    return false;
-  }).map(neighbor => neighbor.neighborDetails[OPENLATTICE_ID_FQN][0]);
-};
-
-const getAllPSAIds = (neighbors, psaScoresEntitySetId) => {
-  if (!neighbors) return [];
-  return neighbors.filter((neighbor) => {
-    if (neighbor.neighborEntitySet && neighbor.neighborEntitySet.id === psaScoresEntitySetId) {
-      const statusValues = neighbor.neighborDetails[PROPERTY_TYPES.STATUS];
-      if (statusValues) {
         return true;
       }
     }
@@ -285,8 +300,474 @@ function* loadNeighborsWatcher() :Generator<*, *, *> {
   yield takeEvery(LOAD_NEIGHBORS, loadNeighborsWorker);
 }
 
+function* getPSAScoresAndNeighbors(psaScoresEKID :string) :Generator<*, *, *> {
+  let psaScoresEntity = Map();
+  let psaNeighborsByAppTypeFqn = Map();
+
+  if (psaScoresEKID) {
+    const app = yield select(getApp);
+    const orgId = yield select(getOrgId);
+    const entitySetIdsToAppType = app.getIn([APP.ENTITY_SETS_BY_ORG, orgId]);
+    /*
+     * Get Entity Set Ids
+     */
+    const arrestCasesESID = getEntitySetIdFromApp(app, ARREST_CASES);
+    const bondsESID = getEntitySetIdFromApp(app, BONDS);
+    const dmfResultsESID = getEntitySetIdFromApp(app, DMF_RESULTS);
+    const dmfRiskFactorsESID = getEntitySetIdFromApp(app, DMF_RISK_FACTORS);
+    const hearingsESID = getEntitySetIdFromApp(app, HEARINGS);
+    const manualPretrialCasesESID = getEntitySetIdFromApp(app, MANUAL_PRETRIAL_CASES);
+    const manualPretrialCourtCasesESID = getEntitySetIdFromApp(app, MANUAL_PRETRIAL_COURT_CASES);
+    const outcomesESID = getEntitySetIdFromApp(app, OUTCOMES);
+    const peopleESID = getEntitySetIdFromApp(app, PEOPLE);
+    const pretrialCasesESID = getEntitySetIdFromApp(app, PRETRIAL_CASES);
+    const psaRiskFactorsESID = getEntitySetIdFromApp(app, PSA_RISK_FACTORS);
+    const psaScoresESID = getEntitySetIdFromApp(app, PSA_SCORES);
+    const releaseConditionsESID = getEntitySetIdFromApp(app, RELEASE_CONDITIONS);
+    const releaseRecommendationsESID = getEntitySetIdFromApp(app, RELEASE_RECOMMENDATIONS);
+    const staffESID = getEntitySetIdFromApp(app, STAFF);
+
+    /*
+    * Get Hearing Info
+    */
+    const psaResponse = yield call(
+      getEntityDataWorker,
+      getEntityData({ entitySetId: psaScoresESID, entityKeyId: psaScoresEKID })
+    );
+    if (psaResponse.error) throw psaResponse.error;
+    psaScoresEntity = fromJS(psaResponse.data);
+
+    /*
+    * Get Neighbors
+    */
+    let psaScoresNeighborsById = yield call(
+      searchEntityNeighborsWithFilterWorker,
+      searchEntityNeighborsWithFilter({
+        entitySetId: psaScoresESID,
+        filter: {
+          entityKeyIds: [psaScoresEKID],
+          sourceEntitySetIds: [
+            dmfResultsESID,
+            releaseRecommendationsESID,
+            bondsESID,
+            outcomesESID,
+            releaseConditionsESID
+          ],
+          destinationESIDs: [
+            arrestCasesESID,
+            dmfRiskFactorsESID,
+            hearingsESID,
+            manualPretrialCasesESID,
+            manualPretrialCourtCasesESID,
+            peopleESID,
+            pretrialCasesESID,
+            psaRiskFactorsESID,
+            staffESID
+          ]
+        }
+      })
+    );
+    if (psaScoresNeighborsById.error) throw psaScoresNeighborsById.error;
+    psaScoresNeighborsById = fromJS(psaScoresNeighborsById.data);
+
+    const psaNeighbors = psaScoresNeighborsById.get(psaScoresEKID, List());
+
+    /*
+     * Format Neighbors
+     */
+
+    psaNeighbors.forEach((neighbor) => {
+      const entitySetId = neighbor.getIn([PSA_NEIGHBOR.ENTITY_SET, 'id']);
+      const appTypeFqn = entitySetIdsToAppType.get(entitySetId, '');
+      if (appTypeFqn) {
+        if (LIST_ENTITY_SETS.includes(appTypeFqn)) {
+          psaNeighborsByAppTypeFqn = psaNeighborsByAppTypeFqn.set(
+            appTypeFqn,
+            psaNeighborsByAppTypeFqn.get(appTypeFqn, List()).push(fromJS(neighbor))
+          );
+        }
+        else if (appTypeFqn === MANUAL_PRETRIAL_CASES || appTypeFqn === MANUAL_PRETRIAL_COURT_CASES) {
+          psaNeighborsByAppTypeFqn = psaNeighborsByAppTypeFqn.set(MANUAL_PRETRIAL_CASES, neighbor);
+        }
+        else {
+          psaNeighborsByAppTypeFqn = psaNeighborsByAppTypeFqn.set(appTypeFqn, fromJS(neighbor));
+        }
+      }
+    });
+
+  }
+
+  return { psaScoresEntity, psaNeighborsByAppTypeFqn };
+}
+
+
+function* submitPSAWorker(action :SequenceAction) :Generator<*, *, *> {
+  try {
+    yield put(submitPSA.request(action.id));
+    const {
+      arrestCaseEKID,
+      caseEntity,
+      chargeEntities,
+      dmfResultsEntity,
+      dmfRiskFactorsEntity,
+      includesPretrialModule,
+      manualCourtCasesAndCharges,
+      personEKID,
+      staffEKID,
+      psaEntity,
+      psaRiskFactorsEntity,
+      psaNotesEntity
+    } = action.value;
+
+    /*
+     * Get Property Type Ids
+     */
+    const app = yield select(getApp);
+    const edm = yield select(getEDM);
+
+    /*
+     * Get Entity Set Ids
+     */
+    const arrestCasesESID = getEntitySetIdFromApp(app, ARREST_CASES);
+    const appearsInESID = getEntitySetIdFromApp(app, APPEARS_IN);
+    const assessedByESID = getEntitySetIdFromApp(app, ASSESSED_BY);
+    const calculatedForESID = getEntitySetIdFromApp(app, CALCULATED_FOR);
+    const chargedWithESID = getEntitySetIdFromApp(app, CHARGED_WITH);
+    const dmfResultsESID = getEntitySetIdFromApp(app, DMF_RESULTS);
+    const dmfRiskFactorsESID = getEntitySetIdFromApp(app, DMF_RISK_FACTORS);
+    const psaRiskFactorsESID = getEntitySetIdFromApp(app, PSA_RISK_FACTORS);
+    const peopleESID = getEntitySetIdFromApp(app, PEOPLE);
+    const psaScoresESID = getEntitySetIdFromApp(app, PSA_SCORES);
+    const psaNotesESID = getEntitySetIdFromApp(app, RELEASE_RECOMMENDATIONS);
+    const staffESID = getEntitySetIdFromApp(app, STAFF);
+
+    const caseESID = manualCourtCasesAndCharges
+      ? getEntitySetIdFromApp(app, MANUAL_PRETRIAL_COURT_CASES)
+      : getEntitySetIdFromApp(app, MANUAL_PRETRIAL_CASES);
+
+    const chargeESID = manualCourtCasesAndCharges
+      ? getEntitySetIdFromApp(app, MANUAL_COURT_CHARGES)
+      : getEntitySetIdFromApp(app, MANUAL_CHARGES);
+
+    /*
+     * Get Prooperty Type Ids
+     */
+    const completedDateTimePTID = getPropertyTypeId(edm, COMPLETED_DATE_TIME);
+    const timeStampPTID = getPropertyTypeId(edm, TIMESTAMP);
+    const stringIdPTID = getPropertyTypeId(edm, STRING_ID);
+
+    /*
+     * Assemble Entities
+     */
+    const psaSubmitEntity = getPropteryIdToValueMap(psaEntity, edm);
+    const psaRiskFactorsSubmitEntity = getPropteryIdToValueMap(psaRiskFactorsEntity, edm);
+    const psaNotesSubmitEntity = getPropteryIdToValueMap(psaNotesEntity, edm);
+    const dmfResultsSubmitEntity = getPropteryIdToValueMap(dmfResultsEntity, edm);
+    const dmfRiskFactorsSubmitEntity = getPropteryIdToValueMap(dmfRiskFactorsEntity, edm);
+    const caseSubmitEntity = getPropteryIdToValueMap(caseEntity, edm);
+
+    const entities = {
+      [psaScoresESID]: [psaSubmitEntity],
+      [psaRiskFactorsESID]: [psaRiskFactorsSubmitEntity],
+      [psaNotesESID]: [psaNotesSubmitEntity],
+      [dmfResultsESID]: [dmfResultsSubmitEntity],
+      [dmfRiskFactorsESID]: [dmfRiskFactorsSubmitEntity],
+      [caseESID]: [caseSubmitEntity]
+    };
+
+    /*
+     * Assemble Associations
+     */
+
+    // Association Data
+    const caseNum = caseEntity[GENERAL_ID];
+    const calculatedForData = { [timeStampPTID]: [moment().toISOString(true)] };
+    const assessedByData = { [completedDateTimePTID]: [moment().toISOString(true)] };
+    const appearsInData = { [stringIdPTID]: [caseNum] };
+    const chargedWithData = { [stringIdPTID]: [caseNum] };
+
+    const associations = {
+      [calculatedForESID]: [
+        // PSA Scores calculated for _____
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: psaScoresESID,
+          dstEntityKeyId: personEKID,
+          dstEntitySetId: peopleESID
+        },
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: psaScoresESID,
+          dstEntityIndex: 0,
+          dstEntitySetId: psaRiskFactorsESID
+        },
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: psaScoresESID,
+          dstEntityIndex: 0,
+          dstEntitySetId: caseESID
+        },
+        // Risk Factors calculated for _____
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: psaRiskFactorsESID,
+          dstEntityKeyId: personEKID,
+          dstEntitySetId: peopleESID
+        },
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: psaRiskFactorsESID,
+          dstEntityIndex: 0,
+          dstEntitySetId: caseESID
+        },
+        // Notes calculated for _____
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: psaNotesESID,
+          dstEntityKeyId: personEKID,
+          dstEntitySetId: peopleESID
+        },
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: psaNotesESID,
+          dstEntityIndex: 0,
+          dstEntitySetId: psaRiskFactorsESID
+        },
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: psaNotesESID,
+          dstEntityIndex: 0,
+          dstEntitySetId: caseESID
+        },
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: psaNotesESID,
+          dstEntityIndex: 0,
+          dstEntitySetId: psaScoresESID
+        }
+      ],
+      [assessedByESID]: [
+        // _____ assessed by staff
+        {
+          data: assessedByData,
+          srcEntityIndex: 0,
+          srcEntitySetId: psaScoresESID,
+          dstEntityKeyId: staffEKID,
+          dstEntitySetId: staffESID
+        },
+        {
+          data: assessedByData,
+          srcEntityIndex: 0,
+          srcEntitySetId: psaRiskFactorsESID,
+          dstEntityKeyId: staffEKID,
+          dstEntitySetId: staffESID
+        },
+        {
+          data: assessedByData,
+          srcEntityIndex: 0,
+          srcEntitySetId: dmfResultsESID,
+          dstEntityKeyId: staffEKID,
+          dstEntitySetId: staffESID
+        },
+        {
+          data: assessedByData,
+          srcEntityIndex: 0,
+          srcEntitySetId: dmfRiskFactorsESID,
+          dstEntityKeyId: staffEKID,
+          dstEntitySetId: staffESID
+        },
+        {
+          data: assessedByData,
+          srcEntityIndex: 0,
+          srcEntitySetId: psaNotesESID,
+          dstEntityKeyId: staffEKID,
+          dstEntitySetId: staffESID
+        }
+      ],
+      [appearsInESID]: [
+        // person appears in manual case
+        {
+          data: appearsInData,
+          srcEntityKeyId: personEKID,
+          srcEntitySetId: peopleESID,
+          dstEntityIndex: 0,
+          dstEntitySetId: caseESID
+        }
+      ]
+    };
+
+    if (includesPretrialModule) {
+      associations[calculatedForESID] = associations[calculatedForESID].concat([
+        // PSA Scores calculated for _____
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: psaScoresESID,
+          dstEntityIndex: 0,
+          dstEntitySetId: dmfRiskFactorsESID
+        },
+        // DMF Risk Factors calculated for _____
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: dmfRiskFactorsESID,
+          dstEntityKeyId: personEKID,
+          dstEntitySetId: peopleESID
+        },
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: dmfRiskFactorsESID,
+          dstEntityIndex: 0,
+          dstEntitySetId: caseESID
+        },
+        // DMF calculated for _____
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: dmfResultsESID,
+          dstEntityKeyId: personEKID,
+          dstEntitySetId: peopleESID
+        },
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: dmfResultsESID,
+          dstEntityIndex: 0,
+          dstEntitySetId: dmfRiskFactorsESID
+        },
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: dmfResultsESID,
+          dstEntityIndex: 0,
+          dstEntitySetId: caseESID
+        },
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: dmfResultsESID,
+          dstEntityIndex: 0,
+          dstEntitySetId: psaScoresESID
+        }
+      ]);
+    }
+
+    if (arrestCaseEKID) {
+      associations[calculatedForESID] = associations[calculatedForESID].concat(
+        [{
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: psaScoresESID,
+          dstEntityKeyId: arrestCaseEKID,
+          dstEntitySetId: arrestCasesESID
+        },
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: psaRiskFactorsESID,
+          dstEntityKeyId: arrestCaseEKID,
+          dstEntitySetId: arrestCasesESID
+        },
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: psaNotesESID,
+          dstEntityKeyId: arrestCaseEKID,
+          dstEntitySetId: arrestCasesESID
+        },
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: dmfRiskFactorsESID,
+          dstEntityKeyId: arrestCaseEKID,
+          dstEntitySetId: arrestCasesESID
+        },
+        {
+          data: calculatedForData,
+          srcEntityIndex: 0,
+          srcEntitySetId: dmfResultsESID,
+          dstEntityKeyId: arrestCaseEKID,
+          dstEntitySetId: arrestCasesESID
+        }]
+      );
+    }
+
+    if (chargeEntities.length) {
+      entities[chargeESID] = [];
+      associations[chargedWithESID] = [];
+      chargeEntities.forEach((charge, index) => {
+        const chargeSubmitEntity = getPropteryIdToValueMap(charge, edm);
+        const chargeToCaseAssociation = {
+          data: appearsInData,
+          srcEntityIndex: index,
+          srcEntitySetId: chargeESID,
+          dstEntityIndex: 0,
+          dstEntitySetId: caseESID
+        };
+        const personToChargeAssociation = {
+          data: chargedWithData,
+          srcEntityKeyId: personEKID,
+          srcEntitySetId: peopleESID,
+          dstEntityIndex: index,
+          dstEntitySetId: chargeESID
+        };
+        entities[chargeESID].push(chargeSubmitEntity);
+        associations[appearsInESID].push(chargeToCaseAssociation);
+        associations[chargedWithESID].push(personToChargeAssociation);
+      });
+    }
+
+    /*
+     * Submit data and collect response
+     */
+    const response = yield call(
+      createEntityAndAssociationDataWorker,
+      createEntityAndAssociationData({ associations, entities })
+    );
+    if (response.error) throw response.error;
+
+    /*
+     * Collect PSA and Neighbors
+     */
+    const entityKeyIds = fromJS(response.data.entityKeyIds);
+
+    const psaScoresEKID = entityKeyIds.getIn([psaScoresESID, 0], '');
+
+    const { psaScoresEntity, psaNeighborsByAppTypeFqn } = yield call(getPSAScoresAndNeighbors, psaScoresEKID);
+
+    yield put(submitPSA.success(action.id, {
+      psaScoresEntity,
+      psaNeighborsByAppTypeFqn,
+      psaScoresEKID,
+      personEKID
+    }));
+  }
+
+  catch (error) {
+    console.error(error);
+    yield put(submitPSA.failure(action.id, error));
+  }
+  finally {
+    yield put(submitPSA.finally(action.id));
+  }
+}
+
+function* submitPSAWatcher() :Generator<*, *, *> {
+  yield takeEvery(SUBMIT_PSA, submitPSAWorker);
+}
 
 export {
   loadDataModelWatcher,
-  loadNeighborsWatcher
+  loadNeighborsWatcher,
+  submitPSAWatcher
 };
