e01d4491f103cd47081a44c41130befc690d976c
Todd Bergman <todd@openlattice.com>
2019-07-16 11:42:56 -0700

    fixed getUTCDateRangeSearchString

--- src/utils/consts/DateTimeConsts.js
+++ src/utils/consts/DateTimeConsts.js
@@ -10,12 +10,15 @@ import { getSearchTermNotExact } from '../DataUtils';
 export const DATE_FORMAT = 'MM/dd/yyyy';
 export const TIME_FORMAT = 'h:mm a';
 
-export const getUTCDateRangeSearchString = (PTID :UUID, startDate :DateTime, endDate :DateTime) => {
+export const getUTCDateRangeSearchString = (PTID :UUID, startDate :DateTime, endDate :?DateTime) => {
   let start = startDate.toUTC().toISO();
-  let end = endDate.toUTC().toISO();
+  let end;
   if (!endDate) {
     start = startDate.startOf('day').toUTC().toISO();
     end = startDate.endOf('day').toUTC().toISO();
+  }
+  else {
+    end = endDate.toUTC().toISO();
   }
   const dateRangeString = `[${start} TO ${end}]`;
   return getSearchTermNotExact(PTID, dateRangeString);
