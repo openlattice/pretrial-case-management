7084fc917dafd00b11d160d65813d7f531c56f66
Todd Bergman <todd@openlattice.com>
2019-07-19 14:27:22 -0400

    Added new ContactInformationConsts

---
+++ src/utils/consts/redux/ContactInformationConsts.js
@@ -0,0 +1,16 @@
+/*
+ * @flow
+ */
+
+export const CONTACT_INFO_ACTIONS = {
+  CLEAR_SUBMITTED_CONTACT: 'clearSubmittedContact',
+  SUBMIT_CONTACT: 'submitContact',
+  UPDATE_CONTACT: 'updateContact',
+  UPDATE_CONTACTS_BULK: 'updateContactsBulk'
+};
+
+export const CONTACT_INFO_DATA = {
+  SUBMITTED_CONTACT_INFO: 'submittedContact',
+  UPDATING_CONTACT_INFO: 'updatingContactInfo',
+  UPDATED_CONTACTS: 'updatedContacts'
+};
