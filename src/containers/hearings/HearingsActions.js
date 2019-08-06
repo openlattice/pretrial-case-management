e572fb9cea4776bbfcfb102251648f32d22894a6
Todd Bergman <todd@openlattice.com>
2019-08-05 18:04:36 -0700

    These aren't Request Sequences

--- src/containers/hearings/HearingsActions.js
+++ src/containers/hearings/HearingsActions.js
@@ -5,26 +5,36 @@
 import { newRequestSequence } from 'redux-reqseq';
 import type { RequestSequence } from 'redux-reqseq';
 
-const CLEAR_HEARING_SETTINGS :string = 'CLEAR_HEARING_SETTINGS';
-const clearHearingSettings :RequestSequence = newRequestSequence(CLEAR_HEARING_SETTINGS);
+const CLEAR_HEARING_SETTINGS :'CLEAR_HEARING_SETTINGS' = 'CLEAR_HEARING_SETTINGS';
+const clearHearingSettings = () => ({
+  type: CLEAR_HEARING_SETTINGS
+});
 
-const CLEAR_SUBMITTED_HEARING :string = 'CLEAR_SUBMITTED_HEARING';
-const clearSubmittedHearing :RequestSequence = newRequestSequence(CLEAR_SUBMITTED_HEARING);
+const CLEAR_SUBMITTED_HEARING :'CLEAR_SUBMITTED_HEARING' = 'CLEAR_SUBMITTED_HEARING';
+const clearSubmittedHearing = () => ({
+  type: CLEAR_SUBMITTED_HEARING
+});
 
-const CLOSE_HEARING_SETTINGS_MODAL :string = 'CLOSE_HEARING_SETTINGS_MODAL';
-const closeHearingSettingsModal :RequestSequence = newRequestSequence(CLOSE_HEARING_SETTINGS_MODAL);
+const CLOSE_HEARING_SETTINGS_MODAL :'CLOSE_HEARING_SETTINGS_MODAL' = 'CLOSE_HEARING_SETTINGS_MODAL';
+const closeHearingSettingsModal = () => ({
+  type: CLOSE_HEARING_SETTINGS_MODAL
+});
+
+const OPEN_HEARING_SETTINGS_MODAL :'OPEN_HEARING_SETTINGS_MODAL' = 'OPEN_HEARING_SETTINGS_MODAL';
+const openHearingSettingsModal = () => ({
+  type: OPEN_HEARING_SETTINGS_MODAL
+});
+
+const SET_HEARING_SETTINGS :'SET_HEARING_SETTINGS' = 'SET_HEARING_SETTINGS';
+const setHearingSettings = () => ({
+  type: SET_HEARING_SETTINGS
+});
 
 const LOAD_HEARING_NEIGHBORS :string = 'LOAD_HEARING_NEIGHBORS';
 const loadHearingNeighbors :RequestSequence = newRequestSequence(LOAD_HEARING_NEIGHBORS);
 
 const REFRESH_HEARING_AND_NEIGHBORS :string = 'REFRESH_HEARING_AND_NEIGHBORS';
 const refreshHearingAndNeighbors :RequestSequence = newRequestSequence(REFRESH_HEARING_AND_NEIGHBORS);
-
-const OPEN_HEARING_SETTINGS_MODAL :string = 'OPEN_HEARING_SETTINGS_MODAL';
-const openHearingSettingsModal :RequestSequence = newRequestSequence(OPEN_HEARING_SETTINGS_MODAL);
-
-const SET_HEARING_SETTINGS :string = 'SET_HEARING_SETTINGS';
-const setHearingSettings :RequestSequence = newRequestSequence(SET_HEARING_SETTINGS);
 
 const SUBMIT_EXISTING_HEARING :string = 'SUBMIT_EXISTING_HEARING';
 const submitExistingHearing :RequestSequence = newRequestSequence(SUBMIT_EXISTING_HEARING);
