713133d094c909fdacec6bc14968f2f3846558cd
Todd Bergman <todd@openlattice.com>
2019-08-06 09:41:15 -0700

    Created Release Conditions Consts for redux

---
+++ src/utils/consts/redux/ReleaseConditionsConsts.js
@@ -0,0 +1,17 @@
+/*
+ * @flow
+ */
+
+export const RELEASE_COND_ACTIONS = {
+  LOAD_RELEASE_CONDITIONS: 'loadReleaseConditions',
+  SUBMIT_RELEASE_CONDITIONS: 'submitReleaseConditions',
+  UPDATE_OUTCOMES_AND_RELEASE_CONDITIONS: 'updateOutcomesAndReleaseCondtions'
+};
+
+export const RELEASE_COND_DATA = {
+  SELECTED_HEARING: 'contactInfo',
+  HAS_OUTCOME: 'personNeighbors',
+  HEARING_NEIGHBORS: 'subscription',
+  PERSON_NEIGHBORS: 'subscriptionsById',
+  PSA_NEIGHBORS: 'subscriptionsById'
+};
