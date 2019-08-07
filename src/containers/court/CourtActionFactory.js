41286ff1557c10a47e5b11111c7d15d6f1ea3c90
Todd Bergman <todd@openlattice.com>
2019-08-07 16:41:37 -0700

    Removed loadHearingsForDate from CourtActionFactory

--- src/containers/court/CourtActionFactory.js
+++ src/containers/court/CourtActionFactory.js
@@ -5,30 +5,29 @@
 import { newRequestSequence } from 'redux-reqseq';
 import type { RequestSequence } from 'redux-reqseq';
 
+
+const SET_COURT_DATE :'SET_COURT_DATE' = 'SET_COURT_DATE';
+const setCourtDate = value => ({
+  type: SET_COURT_DATE,
+  value
+});
+
 const CHANGE_HEARING_FILTERS :string = 'CHANGE_HEARING_FILTERS';
 const changeHearingFilters :RequestSequence = newRequestSequence(CHANGE_HEARING_FILTERS);
 
 const FILTER_PEOPLE_IDS_WITH_OPEN_PSAS :string = 'FILTER_PEOPLE_IDS_WITH_OPEN_PSAS';
 const filterPeopleIdsWithOpenPSAs :RequestSequence = newRequestSequence(FILTER_PEOPLE_IDS_WITH_OPEN_PSAS);
 
-const LOAD_HEARINGS_FOR_DATE :string = 'LOAD_HEARINGS_FOR_DATE';
-const loadHearingsForDate :RequestSequence = newRequestSequence(LOAD_HEARINGS_FOR_DATE);
-
 const LOAD_JUDGES :string = 'LOAD_JUDGES';
 const loadJudges :RequestSequence = newRequestSequence(LOAD_JUDGES);
-
-const SET_COURT_DATE :string = 'SET_COURT_DATE';
-const setCourtDate :RequestSequence = newRequestSequence(SET_COURT_DATE);
 
 export {
   CHANGE_HEARING_FILTERS,
   FILTER_PEOPLE_IDS_WITH_OPEN_PSAS,
-  LOAD_HEARINGS_FOR_DATE,
   LOAD_JUDGES,
   SET_COURT_DATE,
   changeHearingFilters,
   filterPeopleIdsWithOpenPSAs,
-  loadHearingsForDate,
   loadJudges,
   setCourtDate
 };
