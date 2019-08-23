7e88763138d083b9510b04c620d2d36e874841eb
Todd Bergman <todd@openlattice.com>
2019-08-23 11:27:27 -0700

    Needs to go for now

--- src/utils/consts/DataModelConsts.js
+++ src/utils/consts/DataModelConsts.js
@@ -67,7 +67,8 @@ export const APP_TYPES = {
   OUTCOMES: 'app.psaoutcomes',
   OVERSAW: 'app.oversaw',
   PEOPLE: 'app.people',
-  PRESIDES_OVER: 'app.presidesover',
+  // TODO: Bring this back if we need to write to it
+  // PRESIDES_OVER: 'presidesover.app',
   PRETRIAL_CASES: 'app.courtpretrialcases',
   PSA_RISK_FACTORS: 'app.psariskfactors',
   PSA_SCORES: 'app.psascores',
