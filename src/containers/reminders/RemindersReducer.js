9b84cd524dba009cb1196577fd764c1d1de2bf44
Todd Bergman <todd@openlattice.com>
2019-09-03 14:42:27 -0700

    Refactored for Req States

--- src/containers/reminders/RemindersReducer.js
+++ src/containers/reminders/RemindersReducer.js
@@ -3,9 +3,11 @@
  */
 import { DateTime } from 'luxon';
 import { Map, Set, fromJS } from 'immutable';
+import { RequestStates } from 'redux-reqseq';
 
 import { DATE_FORMAT } from '../../utils/consts/DateTimeConsts';
 import { submitManualReminder } from '../manualreminders/ManualRemindersActionFactory';
+import { SWITCH_ORGANIZATION } from '../app/AppActionFactory';
 import {
   bulkDownloadRemindersPDF,
   loadOptOutNeighbors,
@@ -13,144 +15,234 @@ import {
   loadRemindersActionList,
   loadReminderNeighborsById,
   loadRemindersforDate,
-  REMOVE_FROM_REMIDNERS_ACTION_LIST,
   SET_DATE_FOR_REMIDNERS_ACTION_LIST
 } from './RemindersActionFactory';
-import { SWITCH_ORGANIZATION } from '../app/AppActionFactory';
-import { REMINDERS } from '../../utils/consts/FrontEndStateConsts';
+
+import { REDUX } from '../../utils/consts/redux/SharedConsts';
+import { actionValueIsInvalid } from '../../utils/consts/redux/ReduxUtils';
+import { REMINDERS_ACTIONS, REMINDERS_DATA } from '../../utils/consts/redux/RemindersConsts';
+
+const {
+  FAILURE,
+  PENDING,
+  STANDBY,
+  SUCCESS
+} = RequestStates;
 
 const INITIAL_STATE :Map<*, *> = fromJS({
-  [REMINDERS.REMINDERS_ACTION_LIST_DATE]: DateTime.local(),
-  [REMINDERS.LOADING_REMINDERS_ACTION_LIST]: false,
-  [REMINDERS.REMINDERS_ACTION_LIST]: Map(),
-  [REMINDERS.REMINDER_IDS]: Set(),
-  [REMINDERS.FUTURE_REMINDERS]: Map(),
-  [REMINDERS.PAST_REMINDERS]: Map(),
-  [REMINDERS.SUCCESSFUL_REMINDER_IDS]: Set(),
-  [REMINDERS.FAILED_REMINDER_IDS]: Set(),
-  [REMINDERS.LOADING_REMINDERS]: false,
-  [REMINDERS.LOADED]: false,
-  [REMINDERS.REMINDER_NEIGHBORS]: Map(),
-  [REMINDERS.REMINDERS_WITH_OPEN_PSA_IDS]: Map(),
-  [REMINDERS.LOADING_REMINDER_NEIGHBORS]: false,
-  [REMINDERS.OPT_OUTS]: Map(),
-  [REMINDERS.OPT_OUT_NEIGHBORS]: Map(),
-  [REMINDERS.OPT_OUT_PEOPLE_IDS]: Set(),
-  [REMINDERS.OPT_OUTS_WITH_REASON]: Set(),
-  [REMINDERS.LOADING_OPT_OUTS]: false,
-  [REMINDERS.LOADING_OPT_OUT_NEIGHBORS]: false,
-  [REMINDERS.LOADING_REMINDER_PDF]: false,
+  [REDUX.ACTIONS]: {
+    [REMINDERS_ACTIONS.BULK_DOWNLOAD_REMINDERS_PDF]: {
+      [REDUX.REQUEST_STATE]: STANDBY
+    },
+    [REMINDERS_ACTIONS.LOAD_OPT_OUT_NEIGHBORS]: {
+      [REDUX.REQUEST_STATE]: STANDBY
+    },
+    [REMINDERS_ACTIONS.LOAD_OPT_OUTS_FOR_DATE]: {
+      [REDUX.REQUEST_STATE]: STANDBY
+    },
+    [REMINDERS_ACTIONS.LOAD_REMINDERS_ACTION_LIST]: {
+      [REDUX.REQUEST_STATE]: STANDBY
+    },
+    [REMINDERS_ACTIONS.LOAD_REMINDERS_FOR_DATE]: {
+      [REDUX.REQUEST_STATE]: STANDBY
+    },
+    [REMINDERS_ACTIONS.LOAD_REMINDER_NEIGHBORS]: {
+      [REDUX.REQUEST_STATE]: STANDBY
+    }
+  },
+  [REDUX.ERRORS]: {
+    [REMINDERS_ACTIONS.BULK_DOWNLOAD_REMINDERS_PDF]: Map(),
+    [REMINDERS_ACTIONS.LOAD_OPT_OUT_NEIGHBORS]: Map(),
+    [REMINDERS_ACTIONS.LOAD_OPT_OUTS_FOR_DATE]: Map(),
+    [REMINDERS_ACTIONS.LOAD_REMINDERS_ACTION_LIST]: Map(),
+    [REMINDERS_ACTIONS.LOAD_REMINDERS_FOR_DATE]: Map(),
+    [REMINDERS_ACTIONS.LOAD_REMINDER_NEIGHBORS]: Map()
+  },
+  [REMINDERS_DATA.REMINDERS_ACTION_LIST_DATE]: DateTime.local(),
+  [REMINDERS_DATA.REMINDERS_ACTION_LIST]: Map(),
+  [REMINDERS_DATA.REMINDERS_BY_COUNTY]: Map(),
+  [REMINDERS_DATA.REMINDERS_BY_ID]: Map(),
+  [REMINDERS_DATA.SUCCESSFUL_REMINDER_IDS]: Set(),
+  [REMINDERS_DATA.FAILED_REMINDER_IDS]: Set(),
+  [REMINDERS_DATA.REMINDER_NEIGHBORS]: Map(),
+  [REMINDERS_DATA.REMINDERS_WITH_OPEN_PSA_IDS]: Map(),
+  [REMINDERS_DATA.OPT_OUTS]: Map(),
+  [REMINDERS_DATA.OPT_OUT_NEIGHBORS]: Map(),
+  [REMINDERS_DATA.OPT_OUT_PEOPLE_IDS]: Set()
 });
+
 export default function remindersReducer(state :Map<*, *> = INITIAL_STATE, action :Object) {
   switch (action.type) {
 
     case bulkDownloadRemindersPDF.case(action.type): {
       return bulkDownloadRemindersPDF.reducer(state, action, {
-        REQUEST: () => state.set(REMINDERS.LOADING_REMINDER_PDF, true),
-        FINALLY: () => state.set(REMINDERS.LOADING_REMINDER_PDF, false)
+        REQUEST: () => state
+          .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.BULK_DOWNLOAD_REMINDERS_PDF, action.id], fromJS(action))
+          .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.BULK_DOWNLOAD_REMINDERS_PDF, REDUX.REQUEST_STATE], PENDING),
+        SUCESSS: () => state
+          .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.BULK_DOWNLOAD_REMINDERS_PDF, REDUX.REQUEST_STATE], SUCCESS),
+        FAILURE: () => {
+          if (actionValueIsInvalid(action.value)) {
+            return state;
+          }
+          const { error } = action.value;
+          return state
+            .setIn([REDUX.ERRORS, REMINDERS_ACTIONS.BULK_DOWNLOAD_REMINDERS_PDF], error)
+            .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.BULK_DOWNLOAD_REMINDERS_PDF, REDUX.REQUEST_STATE], FAILURE);
+        },
+        FINALLY: () => state
+          .deleteIn([REDUX.ACTIONS, REMINDERS_ACTIONS.BULK_DOWNLOAD_REMINDERS_PDF, action.id])
       });
     }
 
     case loadOptOutNeighbors.case(action.type): {
       return loadOptOutNeighbors.reducer(state, action, {
-        REQUEST: () => state.set(REMINDERS.LOADING_OPT_OUT_NEIGHBORS, true),
+        REQUEST: () => state
+          .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_OPT_OUT_NEIGHBORS, action.id], fromJS(action))
+          .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_OPT_OUT_NEIGHBORS, REDUX.REQUEST_STATE], PENDING),
         SUCCESS: () => {
           const { optOutNeighborsById, optOutPeopleIds } = action.value;
           return state
-            .set(REMINDERS.OPT_OUT_NEIGHBORS, optOutNeighborsById)
-            .set(REMINDERS.OPT_OUT_PEOPLE_IDS, optOutPeopleIds);
+            .set(REMINDERS_DATA.OPT_OUT_NEIGHBORS, optOutNeighborsById)
+            .set(REMINDERS_DATA.OPT_OUT_PEOPLE_IDS, optOutPeopleIds)
+            .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_OPT_OUT_NEIGHBORS, REDUX.REQUEST_STATE], SUCCESS);
         },
-        FINALLY: () => state.set(REMINDERS.LOADING_OPT_OUT_NEIGHBORS, false)
+        FAILURE: () => {
+          if (actionValueIsInvalid(action.value)) {
+            return state;
+          }
+          const { error } = action.value;
+          return state
+            .setIn([REDUX.ERRORS, REMINDERS_ACTIONS.LOAD_OPT_OUT_NEIGHBORS], error)
+            .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_OPT_OUT_NEIGHBORS, REDUX.REQUEST_STATE], FAILURE);
+        },
+        FINALLY: () => state
+          .deleteIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_OPT_OUT_NEIGHBORS, action.id])
       });
     }
 
     case loadOptOutsForDate.case(action.type): {
       return loadOptOutsForDate.reducer(state, action, {
-        REQUEST: () => state.set(REMINDERS.LOADING_OPT_OUTS, true),
+        REQUEST: () => state
+          .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_OPT_OUTS_FOR_DATE, action.id], fromJS(action))
+          .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_OPT_OUTS_FOR_DATE, REDUX.REQUEST_STATE], PENDING),
         SUCCESS: () => {
-          const {
-            optOutMap,
-            optOutsWithReasons
-          } = action.value;
+          const { optOutMap } = action.value;
           return state
-            .set(REMINDERS.OPT_OUTS, optOutMap)
-            .set(REMINDERS.OPT_OUTS_WITH_REASON, optOutsWithReasons);
+            .set(REMINDERS_DATA.OPT_OUTS, optOutMap)
+            .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_OPT_OUTS_FOR_DATE, REDUX.REQUEST_STATE], SUCCESS);
         },
-        FINALLY: () => state.set(REMINDERS.LOADING_OPT_OUTS, false)
+        FAILURE: () => {
+          if (actionValueIsInvalid(action.value)) {
+            return state;
+          }
+          const { error } = action.value;
+          return state
+            .setIn([REDUX.ERRORS, REMINDERS_ACTIONS.LOAD_OPT_OUTS_FOR_DATE], error)
+            .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_OPT_OUTS_FOR_DATE, REDUX.REQUEST_STATE], FAILURE);
+        },
+        FINALLY: () => state
+          .deleteIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_OPT_OUTS_FOR_DATE, action.id])
+      });
+    }
+
+    case loadRemindersActionList.case(action.type): {
+      return loadRemindersActionList.reducer(state, action, {
+        REQUEST: () => state
+          .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_REMINDERS_ACTION_LIST, action.id], fromJS(action))
+          .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_REMINDERS_ACTION_LIST, REDUX.REQUEST_STATE], PENDING),
+        SUCCESS: () => {
+          const { remindersActionList } = action.value;
+          return state
+            .set(REMINDERS_DATA.REMINDERS_ACTION_LIST, remindersActionList)
+            .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_REMINDERS_ACTION_LIST, REDUX.REQUEST_STATE], SUCCESS);
+        },
+        FAILURE: () => {
+          if (actionValueIsInvalid(action.value)) {
+            return state;
+          }
+          const { error } = action.value;
+          return state
+            .setIn([REDUX.ERRORS, REMINDERS_ACTIONS.LOAD_REMINDERS_ACTION_LIST], error)
+            .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_REMINDERS_ACTION_LIST, REDUX.REQUEST_STATE], FAILURE);
+        },
+        FINALLY: () => state
+          .deleteIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_REMINDERS_ACTION_LIST, action.id])
       });
     }
 
     case loadRemindersforDate.case(action.type): {
       return loadRemindersforDate.reducer(state, action, {
         REQUEST: () => state
-          .set(REMINDERS.LOADING_REMINDERS, true)
-          .set(REMINDERS.LOADED, false),
+          .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_REMINDERS_FOR_DATE, action.id], fromJS(action))
+          .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_REMINDERS_FOR_DATE, REDUX.REQUEST_STATE], PENDING),
         SUCCESS: () => {
           const {
-            reminderIds,
-            futureReminders,
-            pastReminders,
+            remindersById,
             successfulRemindersIds,
             failedRemindersIds,
           } = action.value;
           return state
-            .set(REMINDERS.REMINDER_IDS, reminderIds)
-            .set(REMINDERS.FUTURE_REMINDERS, futureReminders)
-            .set(REMINDERS.PAST_REMINDERS, pastReminders)
-            .set(REMINDERS.SUCCESSFUL_REMINDER_IDS, successfulRemindersIds)
-            .set(REMINDERS.FAILED_REMINDER_IDS, failedRemindersIds);
+            .set(REMINDERS_DATA.REMINDERS_BY_ID, remindersById)
+            .set(REMINDERS_DATA.SUCCESSFUL_REMINDER_IDS, successfulRemindersIds)
+            .set(REMINDERS_DATA.FAILED_REMINDER_IDS, failedRemindersIds)
+            .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_REMINDERS_FOR_DATE, REDUX.REQUEST_STATE], SUCCESS);
+        },
+        FAILURE: () => {
+          if (actionValueIsInvalid(action.value)) {
+            return state;
+          }
+          const { error } = action.value;
+          return state
+            .setIn([REDUX.ERRORS, REMINDERS_ACTIONS.LOAD_REMINDERS_FOR_DATE], error)
+            .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_REMINDERS_FOR_DATE, REDUX.REQUEST_STATE], FAILURE);
         },
         FINALLY: () => state
-          .set(REMINDERS.LOADING_REMINDERS, false)
-          .set(REMINDERS.LOADED, true),
+          .deleteIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_REMINDERS_FOR_DATE, action.id])
       });
     }
 
     case loadReminderNeighborsById.case(action.type): {
       return loadReminderNeighborsById.reducer(state, action, {
-        REQUEST: () => state.set(REMINDERS.LOADING_REMINDER_NEIGHBORS, true),
+        REQUEST: () => state
+          .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_REMINDER_NEIGHBORS, action.id], fromJS(action))
+          .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_REMINDER_NEIGHBORS, REDUX.REQUEST_STATE], PENDING),
         SUCCESS: () => {
-          const { reminderNeighborsById } = action.value;
+          const { reminderNeighborsById, reminderIdsByCounty } = action.value;
           return state
-            .set(REMINDERS.REMINDER_NEIGHBORS, reminderNeighborsById);
+            .set(REMINDERS_DATA.REMINDER_NEIGHBORS, reminderNeighborsById)
+            .set(REMINDERS_DATA.REMINDERS_BY_COUNTY, reminderIdsByCounty)
+            .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_REMINDER_NEIGHBORS, REDUX.REQUEST_STATE], SUCCESS);
         },
-        FINALLY: () => state.set(REMINDERS.LOADING_REMINDER_NEIGHBORS, false)
+        FAILURE: () => {
+          if (actionValueIsInvalid(action.value)) {
+            return state;
+          }
+          const { error } = action.value;
+          return state
+            .setIn([REDUX.ERRORS, REMINDERS_ACTIONS.LOAD_REMINDER_NEIGHBORS], error)
+            .setIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_REMINDER_NEIGHBORS, REDUX.REQUEST_STATE], FAILURE);
+        },
+        FINALLY: () => state
+          .deleteIn([REDUX.ACTIONS, REMINDERS_ACTIONS.LOAD_REMINDER_NEIGHBORS, action.id])
       });
     }
 
-    case loadRemindersActionList.case(action.type): {
-      return loadRemindersActionList.reducer(state, action, {
-        REQUEST: () => state.set(REMINDERS.LOADING_REMINDERS_ACTION_LIST, true),
-        SUCCESS: () => {
-          const { remindersActionList } = action.value;
-          return state
-            .set(REMINDERS.REMINDERS_ACTION_LIST, remindersActionList);
-        },
-        FINALLY: () => state.set(REMINDERS.LOADING_REMINDERS_ACTION_LIST, false)
-      });
+    case SET_DATE_FOR_REMIDNERS_ACTION_LIST: {
+      const { date } = action.value;
+      const formattedDate = DateTime.fromFormat(date, DATE_FORMAT);
+      return state.set(REMINDERS_DATA.REMINDERS_ACTION_LIST_DATE, formattedDate);
     }
 
     case submitManualReminder.case(action.type): {
       return submitManualReminder.reducer(state, action, {
         SUCCESS: () => {
           const { personEKID } = action.value;
-          const remindersActionList = state.get(REMINDERS.REMINDERS_ACTION_LIST, Map()).delete(personEKID);
+          const remindersActionList = state.get(REMINDERS_DATA.REMINDERS_ACTION_LIST, Map()).delete(personEKID);
           return state
-            .set(REMINDERS.REMINDERS_ACTION_LIST, remindersActionList);
+            .set(REMINDERS_DATA.REMINDERS_ACTION_LIST, remindersActionList);
         }
       });
-    }
-
-    case REMOVE_FROM_REMIDNERS_ACTION_LIST: {
-      const { personEntityKeyId } = action.value;
-      const remindersActionList = state.get(REMINDERS.REMINDERS_ACTION_LIST, Map()).delete(personEntityKeyId);
-      return state.set(REMINDERS.REMINDERS_ACTION_LIST, remindersActionList);
-    }
-
-    case SET_DATE_FOR_REMIDNERS_ACTION_LIST: {
-      const { date } = action.value;
-      const formattedDate = DateTime.fromFormat(date, DATE_FORMAT);
-      return state.set(REMINDERS.REMINDERS_ACTION_LIST_DATE, formattedDate);
     }
 
     case SWITCH_ORGANIZATION:
