666bf0182cc2f1048d148e095aee52a22ff023a0
Todd Bergman <todd@openlattice.com>
2019-09-20 10:44:34 -0700

    Created SettingsSagas

---
+++ src/containers/settings/SettingsSagas.js
@@ -0,0 +1,109 @@
+/*
+ * @flow
+ */
+
+import { fromJS, Map } from 'immutable';
+import { DataApiActions, DataApiSagas } from 'lattice-sagas';
+import {
+  call,
+  put,
+  takeEvery,
+  select
+} from '@redux-saga/core/effects';
+import type { SequenceAction } from 'redux-reqseq';
+
+import { getEntitySetIdFromApp } from '../../utils/AppUtils';
+
+import { getPropertyIdToValueMap } from '../../edm/edmUtils';
+import { APP_TYPES, PROPERTY_TYPES } from '../../utils/consts/DataModelConsts';
+import { createIdObject } from '../../utils/DataUtils';
+
+import { STATE } from '../../utils/consts/redux/SharedConsts';
+import { APP_DATA } from '../../utils/consts/redux/AppConsts';
+
+import { submitSettings, SUBMIT_SETTINGS } from './SettingsActions';
+
+const { getEntityData, updateEntityData } = DataApiActions;
+const { getEntityDataWorker, updateEntityDataWorker } = DataApiSagas;
+
+const {
+  APP_SETTINGS,
+} = APP_TYPES;
+
+const { APP_DETAILS } = PROPERTY_TYPES;
+
+
+/*
+ * Selectors
+ */
+const getApp = state => state.get(STATE.APP, Map());
+const getEDM = state => state.get(STATE.EDM, Map());
+const getOrgId = state => state.getIn([STATE.APP, APP_DATA.SELECTED_ORG_ID], '');
+
+function* submitSettingsWorker(action :SequenceAction) :Generator<*, *, *> {
+  try {
+    yield put(submitSettings.request(action.id));
+    const { settingsEKID, settings } = action.value;
+
+    const updatedSettings = { [APP_DETAILS]: [JSON.stringify(settings)] };
+
+    const app = yield select(getApp);
+    const edm = yield select(getEDM);
+    const orgId = yield select(getOrgId);
+
+    const updatedSettingsObject = getPropertyIdToValueMap(updatedSettings, edm);
+    console.log(updatedSettingsObject);
+
+    /*
+     * Get Entity Set Ids
+     */
+    const settingsESID = getEntitySetIdFromApp(app, APP_SETTINGS);
+
+
+    /*
+     * Update Hearing Data
+     */
+
+    const updateResponse = yield call(
+      updateEntityDataWorker,
+      updateEntityData({
+        entitySetId: settingsESID,
+        entities: { [settingsEKID]: updatedSettingsObject },
+        updateType: 'PartialReplace'
+      })
+    );
+    if (updateResponse.error) throw updateResponse.error;
+
+    /*
+     * Get updated hearing
+     */
+    const settingsIdObject = createIdObject(settingsEKID, settingsESID);
+    const settingsResponse = yield call(
+      getEntityDataWorker,
+      getEntityData(settingsIdObject)
+    );
+    if (settingsResponse.error) throw settingsResponse.error;
+    const submittedSettings = fromJS(settingsResponse.data);
+
+    yield put(submitSettings.success(action.id, {
+      orgId,
+      submittedSettings
+    }));
+  }
+
+  catch (error) {
+    console.error(error);
+    yield put(submitSettings.failure(action.id, error));
+  }
+  finally {
+    yield put(submitSettings.finally(action.id));
+  }
+}
+
+function* submitSettingsWatcher() :Generator<*, *, *> {
+  yield takeEvery(SUBMIT_SETTINGS, submitSettingsWorker);
+}
+
+export {
+  submitSettingsWatcher
+};
