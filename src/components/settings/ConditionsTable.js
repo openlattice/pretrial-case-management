b75626f6eb844a037c4628a41638952a51e0bf12
Todd Bergman <todd@openlattice.com>
2019-09-24 11:41:27 -0700

    moved condition table to compoennts/settings folder

--- src/components/settings/ConditionsTable.js
+++ src/components/settings/ConditionsTable.js
@@ -6,7 +6,8 @@ import React from 'react';
 import { Map } from 'immutable';
 import { CardSegment, Table } from 'lattice-ui-kit';
 
-import ConditionsRow from './ConditionsRow';
+import ConditionsRow from '../../containers/settings/ConditionsRow';
+import { HeaderSection } from './SettingsStyledComponents';
 
 type Props = {
   editing :boolean,
@@ -20,7 +21,7 @@ class ReleaseConditionsTable extends React.Component<Props, State> {
     const { editing, levels } = this.props;
     const headers :Object[] = [{ key: 'description', label: 'Description' }];
     for (let level = 1; level <= Object.keys(levels).length; level += 1) {
-      if (levels[level].active) headers.push({ key: `Level ${level}`, label: `Level ${level}` });
+      if (levels[level].active) headers.push({ key: `Level ${level}`, label: `Level ${level}`, cellStyle: { width: '12%' } });
     }
     if (editing) headers.push({ key: 'removerow', label: '' });
     return headers;
@@ -43,20 +44,13 @@ class ReleaseConditionsTable extends React.Component<Props, State> {
       )
     };
 
-    if (editing) {
-      conditions.push(
-        {}
-      );
-    }
-
     return (
       <CardSegment vertical>
+        <HeaderSection>Release Conditions</HeaderSection>
         <Table
             components={components}
             headers={headers}
-            data={conditions}
-            rowsPerPageOptions={[6, 8, 10, 12]}
-            paginated />
+            data={conditions} />
       </CardSegment>
     );
   }
