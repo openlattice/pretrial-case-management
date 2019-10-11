5acc1e7be55a07887b5ac5fe8d01afcd32478206
Todd Bergman <todd@openlattice.com>
2019-10-10 18:46:20 -0700

    Created BookingHold for RCM

---
+++ src/components/rcm/BookingHold.js
@@ -0,0 +1,96 @@
+/*
+ * @flow
+ */
+
+import React from 'react';
+import { Map } from 'immutable';
+import { connect } from 'react-redux';
+
+import RCMCell from './RCMCell';
+import rightArrow from '../../assets/svg/rcm-arrow.svg';
+import { PROPERTY_TYPES } from '../../utils/consts/DataModelConsts';
+import { getEntityProperties } from '../../utils/DataUtils';
+import { RCM_FIELDS } from '../../utils/consts/RCMResultsConsts';
+import {
+  StepHeader,
+  StepWrapper,
+  RCMIncreaseText
+} from './RCMStyledTags';
+import {
+  getRCMDecision,
+  shouldCheckForSecondaryHold,
+  updateRCMSecondaryHold
+} from '../../utils/RCMUtils';
+
+import { STATE } from '../../utils/consts/redux/SharedConsts';
+import { SETTINGS_DATA } from '../../utils/consts/redux/SettingsConsts';
+
+const { NCA_SCALE, FTA_SCALE, NVCA_FLAG } = PROPERTY_TYPES;
+
+type Props = {
+  settings :Map<*, *>,
+  scores :Map<*, *>,
+  riskFactors :Map<*, *>,
+  shouldRender :boolean
+};
+
+class BookingHold extends React.Component<Props, *> {
+
+  renderText = () => {
+    const { riskFactors } = this.props;
+    const secondaryHoldVal :boolean = riskFactors.get(RCM_FIELDS.SECONDARY_HOLD_CHARGES) === `${true}`;
+    return secondaryHoldVal
+      ? <RCMIncreaseText>Charges qualify for a secondary hold option</RCMIncreaseText>
+      : <RCMIncreaseText>Charges do not qualify for a secondary hold option</RCMIncreaseText>;
+  }
+
+  render() {
+    const {
+      shouldRender,
+      scores,
+      riskFactors,
+      settings,
+    } = this.props;
+    const {
+      [NCA_SCALE]: ncaScore,
+      [FTA_SCALE]: ftaScore
+    } = getEntityProperties(scores, [NCA_SCALE, FTA_SCALE, NVCA_FLAG]);
+    const rcmResult = getRCMDecision(ncaScore, ftaScore, settings);
+    const level :number = rcmResult.rcm[PROPERTY_TYPES.CONDITIONS_LEVEL];
+    if (!shouldRender || !shouldCheckForSecondaryHold(level, settings)) return null;
+
+    const { rcm: updatedRCM, bookingConditions: updatedConditions } = updateRCMSecondaryHold(rcmResult);
+
+    const secondaryHoldVal :boolean = riskFactors.get(RCM_FIELDS.SECONDARY_HOLD_CHARGES) === `${true}`;
+    const rcmTransformation = secondaryHoldVal
+      ? (
+        <StepWrapper>
+          <RCMCell rcm={rcmResult.rcm} conditions={rcmResult.bookingConditions} large />
+          <img src={rightArrow} alt="" />
+          <RCMCell rcm={updatedRCM} conditions={updatedConditions} large />
+        </StepWrapper>
+      ) : (
+        <StepWrapper>
+          <RCMCell rcm={rcmResult.rcm} conditions={rcmResult.bookingConditions} large />
+        </StepWrapper>
+      );
+
+    return (
+      <div>
+        <hr />
+        <StepHeader>Bookings Exception</StepHeader>
+        <StepWrapper>{ this.renderText() }</StepWrapper>
+        { rcmTransformation }
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  const settings = state.getIn([STATE.SETTINGS, SETTINGS_DATA.APP_SETTINGS], Map());
+  return {
+    settings
+  };
+}
+
+export default connect(mapStateToProps, null)(BookingHold);
