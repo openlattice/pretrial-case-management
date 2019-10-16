98d45d42f95c8eed2bd24e4f66c0b2ae91de4793
Todd Bergman <todd@openlattice.com>
2019-10-16 14:23:01 -0700

    update key for PSA Date and switch to functional component

--- src/components/requiresaction/RequiresActionTable.js
+++ src/components/requiresaction/RequiresActionTable.js
@@ -9,7 +9,6 @@ import { Table } from 'lattice-ui-kit';
 import RequiresActionPersonRow from '../../containers/people/RequiresActionPersonRow';
 
 type Props = {
-  editing :boolean,
   selectedPersonId :string,
   handleSelect :() => void;
   people :Map<*, *>
@@ -19,39 +18,32 @@ const HEADERS = [
   { key: 'lastName', label: 'Last Name' },
   { key: 'firstName', label: 'First Name' },
   { key: 'dob', label: 'Date of Birth' },
-  { key: 'mostRecentPSA', label: 'PSA Date (latest)' },
-  { key: 'oldestPSA', label: 'PSA Date (oldest)' },
+  { key: 'oldPSADate', label: 'PSA Date (oldest)' },
 ];
 
-class ReleaseConditionsTable extends React.Component<Props, State> {
+const ReleaseConditionsTable = ({
+  handleSelect,
+  people,
+  selectedPersonId,
+} :Props) => {
 
-  render() {
-    const {
-      editing,
-      handleSelect,
-      people,
-      selectedPersonId,
-    } = this.props;
+  const components :Object = {
+    Row: ({ data } :any) => (
+      <RequiresActionPersonRow
+          selectedPersonId={selectedPersonId}
+          handleSelect={handleSelect}
+          data={data} />
+    )
+  };
 
-    const components :Object = {
-      Row: ({ data } :any) => (
-        <RequiresActionPersonRow
-            selectedPersonId={selectedPersonId}
-            editing={editing}
-            handleSelect={handleSelect}
-            data={data} />
-      )
-    };
-
-    return (
-      <Table
-          components={components}
-          headers={HEADERS}
-          data={people}
-          paginated
-          rowsPerPageOptions={[10, 15, 20]} />
-    );
-  }
-}
+  return (
+    <Table
+        components={components}
+        headers={HEADERS}
+        data={people}
+        paginated
+        rowsPerPageOptions={[10, 15, 20]} />
+  );
+};
 
 export default ReleaseConditionsTable;
