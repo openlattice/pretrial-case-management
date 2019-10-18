f151bb8f6e5465e16203aec7148a0409712019ad
Todd Bergman <todd@openlattice.com>
2019-10-18 12:06:45 -0700

    Refactored for using select for drop downs

--- src/containers/settings/LevelColorsSection.js
+++ src/containers/settings/LevelColorsSection.js
@@ -4,34 +4,35 @@
 
 import React from 'react';
 import styled from 'styled-components';
-import { Map } from 'immutable';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import { CardSegment } from 'lattice-ui-kit';
 
-import { COLOR_LABELS, COLOR_MAP } from '../../utils/consts/RCMResultsConsts';
-import ColorSwatches from '../../components/settings/ColorSwatches';
-import {
-  SETTINGS,
-  RCM,
-  RCM_DATA
-} from '../../utils/consts/AppSettingConsts';
-
-
-import { updateSetting, deleteRCMCondition } from './SettingsActions';
+import ColorSwatches from './ColorSwatches';
+import { RCM_DATA } from '../../utils/consts/AppSettingConsts';
 
 const BookingHoldSectionWrapper = styled.div`
   width: 100%;
   display: grid;
   grid-template-columns: repeat(${props => props.columns}, 1fr);
+  grid-gap: 10px;
 `;
 
 const CellContent = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   width: 100%;
   font-weight: 600;
   font-size: 14px;
+`;
+
+const ColorBlock = styled.div`
+  height: 40px;
+  z-index: 10;
+  width: 40px;
+  border-radius: 3px;
+  margin: 0 5px 1px;
+  background: ${props => props.color};
+  opacity: ${props => (props.editing ? 'none' : '50%')};
 `;
 
 type Props = {
@@ -46,77 +47,61 @@ type Props = {
 
 class LevelColorsSection extends React.Component<Props, *> {
 
-  updateColorForLevel = (idx, color) => {
-    const { levels, actions } = this.props;
-    if (Object.values(levels).length < 6) {
-      actions.updateSetting(
-        { path: [SETTINGS.RCM, RCM.LEVELS, `${idx}`, RCM_DATA.COLOR], value: color }
-      );
-    }
-  }
-
-  getAvailableColors = () => {
-    const { levels } = this.props;
-    const usedColors = Object.values(levels).map(level => level[RCM_DATA.COLOR]);
-    return Object.keys(COLOR_MAP).filter(color => !usedColors.includes(color));
-  }
-  getAvailableColorObjectList = () => {
-    const { levels } = this.props;
-    const usedColors = Object.values(levels).map(level => level[RCM_DATA.COLOR]);
-    const colorMap = Map().withMutations((mutableMap) => {
-      Object.keys(COLOR_MAP).forEach((color) => {
-        const value = COLOR_MAP[color];
-        const label = COLOR_LABELS[color];
-        const isDisabled = !usedColors.includes(color);
-        const colorObject = {
-          color,
-          value,
-          label,
-          isDisabled
-        };
-        mutableMap.set(color, colorObject);
-      });
-    });
-    return colorMap;
-  }
-
   getColumns = () => {
+    const { levels } = this.props;
+    const columns = Object.keys(levels)
+      .map((idx) => {
+        const selectedColor = levels[idx][RCM_DATA.COLOR];
+        return (
+          <CellContent key={`RT4Level ${idx}`} align="center">
+            <ColorBlock color={selectedColor} />
+            <div>{`Level ${idx}`}</div>
+          </CellContent>
+        );
+      });
+    return columns;
+  }
+
+  getEditColumns = () => {
     const { levels, editing } = this.props;
+    console.log(editing);
     const columns = Object.keys(levels)
       .map(idx => (
         <CellContent key={`RT4Level ${idx}`} align="center">
           <ColorSwatches
               index={idx}
               editing={editing}
-              levels={levels}
-              updateColorForLevel={this.updateColorForLevel}
-              colorObjects={this.getAvailableColorObjectList()}
-              availableColors={this.getAvailableColors()} />
-          <div>{`Level ${idx}`}</div>
+              levels={levels} />
         </CellContent>
       ));
     return columns;
   }
 
   render() {
-    const columns = this.getColumns();
+    const { editing } = this.props;
+    const colorBlocks = this.getColumns();
+    const colorOptions = this.getEditColumns();
 
     return (
-      <BookingHoldSectionWrapper columns={columns.length + 1}>
-        <CellContent>
-          Colors
-        </CellContent>
-        { columns }
-      </BookingHoldSectionWrapper>
+      <CardSegment vertical>
+        <BookingHoldSectionWrapper columns={colorBlocks.length + 1}>
+          <CellContent>
+            Colors
+          </CellContent>
+          { colorBlocks }
+        </BookingHoldSectionWrapper>
+        {
+          editing
+            ? (
+              <BookingHoldSectionWrapper columns={colorOptions.length + 1}>
+                <CellContent />
+                { colorOptions }
+              </BookingHoldSectionWrapper>
+            ) : null
+        }
+      </CardSegment>
     );
   }
 }
 
-const mapDispatchToProps = (dispatch :Dispatch<any>) => ({
-  actions: bindActionCreators({
-    updateSetting,
-    deleteRCMCondition
-  }, dispatch)
-});
-
-export default connect(null, mapDispatchToProps)(LevelColorsSection);
+export default LevelColorsSection;
