3e0073538287fa8532e61fa3e9be63ceb1645e21
Todd Bergman <todd@openlattice.com>
2019-10-21 13:20:35 -0700

    removed domain related code

--- src/containers/download/DownloadSagas.js
+++ src/containers/download/DownloadSagas.js
@@ -138,8 +138,7 @@ function* downloadPSAsWorker(action :SequenceAction) :Generator<*, *, *> {
     const {
       startDate,
       endDate,
-      filters,
-      domain
+      filters
     } = action.value;
 
     const caseToChargeTypes = {
@@ -235,18 +234,11 @@ function* downloadPSAsWorker(action :SequenceAction) :Generator<*, *, *> {
                 && psaCreationDate.isSameOrBefore(end);
       let usableNeighbors = Immutable.List();
       const neighborList = neighborsById.data[id];
-      let domainMatch = true;
       neighborList.forEach((neighborObj) => {
         const neighbor = getFilteredNeighbor(neighborObj);
         const entitySetId = neighbor.neighborEntitySet.id;
         const entityKeyId = neighbor[PSA_NEIGHBOR.DETAILS][PROPERTY_TYPES.ENTITY_KEY_ID][0];
         const appTypeFqn = entitySetIdsToAppType.get(entitySetId, '');
-        if (domain && neighbor.neighborEntitySet && entitySetId === staffEntitySetId) {
-          const filer = neighbor.neighborDetails[PROPERTY_TYPES.PERSON_ID][0];
-          if (!filer.toLowerCase().endsWith(domain)) {
-            domainMatch = false;
-          }
-        }
 
         if (Object.keys(caseToChargeTypes).includes(appTypeFqn)) {
           caseIdsToScoreIds = caseIdsToScoreIds.setIn([appTypeFqn, entityKeyId], id);
@@ -264,7 +256,7 @@ function* downloadPSAsWorker(action :SequenceAction) :Generator<*, *, *> {
           usableNeighbors = usableNeighbors.push(Immutable.fromJS(neighbor));
         }
       });
-      if (domainMatch && usableNeighbors.size > 0) {
+      if (usableNeighbors.size > 0) {
         usableNeighborsById = usableNeighborsById.set(id, usableNeighbors);
       }
     });
