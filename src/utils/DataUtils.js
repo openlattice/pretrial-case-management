6f99b25c22bb7ee6a55bc4d637a28f2339d382ce
Todd Bergman <todd@openlattice.com>
2019-10-22 17:53:06 -0700

    sortCourtrooms

--- src/utils/DataUtils.js
+++ src/utils/DataUtils.js
@@ -14,6 +14,10 @@ const { OPENLATTICE_ID_FQN } = Constants;
 
 const LAST_WRITE_FQN = 'openlattice.@lastWrite';
 
+export const sortCourtrooms = (str1, str2) => {
+  return str1 > str2 ? 1 : -1;
+};
+
 export const stripIdField = (entity) => {
   if (isImmutable(entity)) {
     return entity.delete(OPENLATTICE_ID_FQN).delete('id');
