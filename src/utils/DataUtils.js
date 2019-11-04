cf3bd52b8bfef8bf249d163ea97d074a005809fa
Todd Bergman <todd@openlattice.com>
2019-11-04 12:40:42 -0800

    get whether drilled in or not

--- src/utils/DataUtils.js
+++ src/utils/DataUtils.js
@@ -17,7 +17,7 @@ const LAST_WRITE_FQN = 'openlattice.@lastWrite';
 
 export const stripIdField = (entity) => {
   if (isImmutable(entity)) {
-    return entity.delete(OPENLATTICE_ID_FQN).delete('id');
+    return entity.get(PSA_NEIGHBOR.DETAILS, entity).delete(OPENLATTICE_ID_FQN).delete('id');
   }
 
   const newEntity = Object.assign({}, entity);
