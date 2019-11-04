6f19797c601812795e511ff7cc3d5cb47e36312b
Todd Bergman <todd@openlattice.com>
2019-11-04 12:34:55 -0800

    Added getPeopleNeighbors and removed getPeople

--- src/containers/people/PeopleActions.js
+++ src/containers/people/PeopleActions.js
@@ -4,11 +4,14 @@
 
 import { newRequestSequence } from 'redux-reqseq';
 
-const CLEAR_PERSON = 'CLEAR_PERSON';
-const clearPerson = newRequestSequence(CLEAR_PERSON);
+const CLEAR_PERSON :'CLEAR_PERSON' = 'CLEAR_PERSON';
+const clearPerson = value => ({
+  type: CLEAR_PERSON,
+  value
+});
 
-const GET_PEOPLE = 'GET_PEOPLE';
-const getPeople = newRequestSequence(GET_PEOPLE);
+const GET_PEOPLE_NEIGHBORS = 'GET_PEOPLE_NEIGHBORS';
+const getPeopleNeighbors = newRequestSequence(GET_PEOPLE_NEIGHBORS);
 
 const GET_PERSON_DATA = 'GET_PERSON_DATA';
 const getPersonData = newRequestSequence(GET_PERSON_DATA);
@@ -25,13 +28,13 @@ const getStaffEKIDs = newRequestSequence(GET_STAFF_EKIDS);
 
 export {
   CLEAR_PERSON,
-  GET_PEOPLE,
+  GET_PEOPLE_NEIGHBORS,
   GET_PERSON_DATA,
   GET_PERSON_NEIGHBORS,
   GET_STAFF_EKIDS,
   LOAD_REQUIRES_ACTION_PEOPLE,
   clearPerson,
-  getPeople,
+  getPeopleNeighbors,
   getPersonData,
   getPersonNeighbors,
   getStaffEKIDs,
