4f3f46b8c530637467c6bb6edcf18113b4f124bb
Todd Bergman <todd@openlattice.com>
2019-11-12 18:58:24 -0800

    added field to layout

--- src/utils/Layout.js
+++ src/utils/Layout.js
@@ -659,3 +659,23 @@ export const ContentLabel = styled.div`
   text-transform: uppercase;
   color: ${OL.GREY02};
 `;
+
+export const Field = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const Header = styled.div`
+  width: 100%;
+  font-size: 11px;
+  font-weight: 600;
+  text-transform: uppercase;
+  color: ${OL.GREY02};
+`;
+
+export const Data = styled.div`
+  width: 100%;
+  font-size: 14px;
+  color: OL.GREY15;
+`;
