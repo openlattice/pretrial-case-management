15d86213d5ed082ec5f1e54ea1e61bb8acfdef46
Todd Bergman <todd@openlattice.com>
2019-11-19 15:36:59 -0800

    Added consts for PersonReducer

--- src/utils/consts/redux/PersonConsts.js
+++ src/utils/consts/redux/PersonConsts.js
@@ -3,10 +3,16 @@
  */
 
 export const PERSON_ACTIONS = {
-  NEW_PERSON_SUBMIT: 'newPersonSubmit'
+  LOAD_PERSON_DETAILS: 'loadPersonDetails',
+  NEW_PERSON_SUBMIT: 'newPersonSubmit',
+  UPDATE_CASES: 'updateCases'
 };
 
 export const PERSON_DATA = {
+  NUM_CASES_TO_LOAD: 'numCasesToLoad',
+  NUM_CASES_LOADED: 'numCasesLoaded',
+  PERSON_DETAILS: 'personDetails',
+  SELECTED_PERSON_ID: 'selectedPersonId',
   SUBMITTED_PERSON: 'submittedPerson',
   SUBMITTED_PERSON_NEIGHBORS: 'submittedPersonNeighbors'
 };
