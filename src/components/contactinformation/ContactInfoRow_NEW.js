d28bf3b8b61f0c0aa2ea386dc50d8b10048f0a41
Giulia <giuliaecampana@gmail.com>
2019-12-10 17:30:15 -0800

    Create custom row for table that includes buttons for the tags

---
+++ src/components/contactinformation/ContactInfoRow_NEW.js
@@ -0,0 +1,100 @@
+/*
+ * @flow
+ */
+import React from 'react';
+import styled from 'styled-components';
+import { Button, StyleUtils } from 'lattice-ui-kit';
+
+import { OL } from '../../utils/consts/Colors';
+
+const { getStickyPosition } = StyleUtils;
+
+export const TableCell = styled.td`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 14px;
+  padding: 10px 30px;
+  text-align: left;
+  vertical-align: middle;
+  word-wrap: break-word;
+  color: ${OL.GREY15};
+  ${props => props.cellStyle};
+
+  :nth-of-type(3) {
+    padding-top: 24px;
+  }
+`;
+
+const StyledTableRow = styled.tr`
+  background-color: ${OL.WHITE};
+  border-bottom: none;
+  color: ${OL.GREY15};
+  font-size: 14px;
+
+  :last-of-type {
+    border-bottom: none;
+  }
+
+  td,
+  th {
+    ${getStickyPosition}
+  }
+
+  ${TableCell}:first-child {
+    padding-left: 65px;
+  }
+
+  ${TableCell}:last-child {
+    padding-right: 30px;
+  }
+`;
+
+const ButtonsWrapper = styled.div`
+  display: grid;
+  grid-gap: 0 8px;
+  grid-template-columns: repeat(2, 1fr);
+  width: 100%;
+`;
+
+type Props = {
+  className ?:string;
+  components :Object;
+  data :Object;
+  headers :Object[];
+};
+
+const ContactInfoRow = ({
+  className,
+  data,
+  headers
+} :Props) => {
+
+  const { id } = data;
+
+  // const cells = headers
+  //   .map(header => (
+  //     <components.Cell
+  //         key={`${id}_cell_${header.key}`}>
+  //       {data[header.key]}
+  //     </components.Cell>
+  //   ));
+
+  return (
+    <StyledTableRow className={className}>
+      <TableCell key={`${id}_cell_${headers[0].key}`}>
+        { data[headers[0].key] }
+      </TableCell>
+      <TableCell key={`${id}_cell_${headers[0].key}`}>
+        <ButtonsWrapper>
+          <Button size="sm">Mobile</Button>
+          <Button size="sm">Preferred</Button>
+        </ButtonsWrapper>
+      </TableCell>
+    </StyledTableRow>
+  );
+};
+
+ContactInfoRow.defaultProps = {
+  className: undefined
+};
+
+export default ContactInfoRow;
