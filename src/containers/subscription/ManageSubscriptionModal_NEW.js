a0094878e24c7ad5eff35e9a52895dda0e245bb8
Giulia <giuliaecampana@gmail.com>
2019-12-10 17:29:04 -0800

    Create new modal using LUK modal

---
+++ src/containers/subscription/ManageSubscriptionModal_NEW.js
@@ -0,0 +1,324 @@
+/*
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import styled, { css } from 'styled-components';
+import {
+  Button,
+  CardSegment,
+  Input,
+  Modal
+} from 'lattice-ui-kit';
+import { List, Map, fromJS } from 'immutable';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faTimesCircle } from '@fortawesome/pro-solid-svg-icons';
+import { faPlus, faTimes } from '@fortawesome/pro-light-svg-icons';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import type { RequestState } from 'redux-reqseq';
+
+import BasicButton from '../../components/buttons/BasicButton';
+import InfoButton from '../../components/buttons/InfoButton';
+import LoadingSpinner from '../../components/LoadingSpinner';
+import SubscriptionInfo from '../../components/subscription/SubscriptionInfo';
+import ContactInfoTable from '../../components/contactinformation/ContactInfoTable_NEW';
+import NewContactForm from '../contactinformation/NewContactForm';
+import { formatPeopleInfo } from '../../utils/PeopleUtils';
+import { getEntityKeyId, getEntityProperties } from '../../utils/DataUtils';
+import { PROPERTY_TYPES } from '../../utils/consts/DataModelConsts';
+import { OL } from '../../utils/consts/Colors';
+import {
+  CloseModalX,
+  NoResults,
+  PaddedStyledColumnRow,
+  TitleWrapper,
+  Wrapper,
+} from '../../utils/Layout';
+import {
+  EDM,
+  REVIEW,
+  PSA_NEIGHBOR
+} from '../../utils/consts/FrontEndStateConsts';
+
+import { STATE } from '../../utils/consts/redux/SharedConsts';
+import { getReqState, requestIsPending } from '../../utils/consts/redux/ReduxUtils';
+import { APP_DATA } from '../../utils/consts/redux/AppConsts';
+import { CONTACT_INFO_ACTIONS } from '../../utils/consts/redux/ContactInformationConsts';
+import { SUBSCRIPTION_ACTIONS, SUBSCRIPTION_DATA } from '../../utils/consts/redux/SubscriptionConsts';
+
+import { updateContactsBulk } from '../contactinformation/ContactInfoActions';
+import {
+  clearSubscriptionModal,
+  loadSubcriptionModal,
+  subscribe,
+  unsubscribe
+} from './SubscriptionActions';
+
+const message :string = 'All numbers tagged mobile and preferred will receive court reminders.';
+const widthValues = css`
+  max-width: 672px;
+  min-width: 572px;
+`;
+
+const ModalHeaderSection = styled.div`
+  align-items: center;
+  color: ${OL.WHITE};
+  display: flex;
+  flex-direction: row;
+  flex: 0 0 auto;
+  justify-content: space-between;
+  /* max-width: 672px; */
+  min-height: 40px;
+  /* min-width: 572px; */
+  ${widthValues};
+  padding: 30px;
+  position: relative;
+`;
+
+const ModalBodyWrapper = styled(CardSegment)`
+  margin: 0 -30px;
+  ${widthValues};
+`;
+
+const ModalTitle = styled.h1`
+  color: ${OL.GREY01};
+  font-size: 22px;
+  font-weight: 600;
+`;
+
+const CloseButton = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-size: 12px;
+  height: 32px;
+  margin: 0 0 0 auto;
+  padding: 0;
+  text-align: center;
+  width: 32px;
+`;
+
+const SubscriptionWrapper = styled.div`
+  border-bottom: 1px solid ${OL.GREY11};
+  display: flex;
+  margin: 0 -30px;
+  ${widthValues};
+  padding: 0 0 30px 30px;
+`;
+
+const TextWrapper = styled.div`
+  margin-left: 20px;
+`;
+
+const AddNewContactSection = styled(ModalBodyWrapper)`
+  align-items: center;
+  border-bottom: none;
+`;
+
+const AddNewContactElementsWrapper = styled.div`
+  display: grid;
+  grid-gap: 8px;
+  grid-template-columns: 21px 1fr 157px;
+  width: 100%;
+`;
+
+type Props = {
+  actions :{
+    clearSubscriptionModal :() => void,
+    updateContactsBulk :(values :{
+      entities :Map,
+      personEKID :string
+    }) => void,
+    subscribe :(values :{
+      personEKID :string,
+      subscriptionEKID :Map<*, *>
+    }) => void,
+    unsubscribe :(values :{
+      personEKID :string,
+      subscriptionEKID :Map<*, *>
+    }) => void,
+  };
+  contactInfo :List;
+  fqnToIdMap :Map;
+  isOpen :boolean;
+  loadingSubscriptionInfo :boolean;
+  onClose :() => void;
+  person :Map;
+  readOnlyPermissions :boolean;
+  submitContactReqState :RequestState;
+  subscribeReqState :RequestState;
+  subscription :Map;
+  unsubscribeReqState :RequestState;
+  updateContactsBulkReqState :RequestState;
+};
+
+type State = {
+  modifyingContactInformation :boolean;
+  updates :Object;
+};
+
+class ManageSubscriptionModal extends Component<Props, State> {
+
+  constructor(props :Props) {
+    super(props);
+    this.state = {
+      modifyingContactInformation: false,
+      updates: {}
+    };
+  }
+
+  editingContactInformation = () => {
+    const { submitContactReqState, updateContactsBulkReqState } = this.props;
+    const submittingContactInfo = requestIsPending(submitContactReqState);
+    const updatingContactInfo = requestIsPending(updateContactsBulkReqState);
+    return submittingContactInfo || updatingContactInfo;
+  }
+
+  getName = () => {
+    const { person } = this.props;
+    const { firstName, middleName, lastName } = formatPeopleInfo(person);
+    const midName = middleName ? ` ${middleName}` : '';
+    return `${firstName}${midName} ${lastName}`;
+  }
+
+  renderStatusIcon = () => {
+    const { subscription } = this.props;
+    const isSubscribed = subscription.getIn([PROPERTY_TYPES.IS_ACTIVE, 0], false);
+    const statusIcon = isSubscribed
+      ? <FontAwesomeIcon color={OL.GREEN02} icon={faCheckCircle} />
+      : <FontAwesomeIcon color={OL.GREY01} icon={faTimesCircle} />;
+    return statusIcon;
+  }
+
+  getIsSubscribedText = () => {
+    const { subscription } = this.props;
+    const isSubscribed = subscription.getIn([PROPERTY_TYPES.IS_ACTIVE, 0], false);
+    const isSubscribedText = isSubscribed
+      ? 'is subscribed to Court Reminders.'
+      : 'is not subscribed to Court Reminders.';
+    return isSubscribedText;
+  }
+
+  handleCheckboxUpdates = (e) => {
+    const { fqnToIdMap } = this.props;
+    const { updates } = this.state;
+    const { value, name, checked } = e.target;
+    const currentEntity = updates[value] || {};
+    currentEntity[fqnToIdMap.get(name)] = [checked];
+    updates[value] = currentEntity;
+    this.setState({
+      updates
+    });
+  }
+
+  renderContactInformation = () => {
+    const {
+      contactInfo,
+      loadingSubscriptionInfo,
+      person,
+      readOnlyPermissions
+    } = this.props;
+    const { modifyingContactInformation } = this.state;
+    const editingContactInformation = this.editingContactInformation();
+    const personEKID = getEntityKeyId(person);
+    return (
+      <ContactInfoTable
+          contactInfo={contactInfo}
+          disabled={editingContactInformation}
+          editing={modifyingContactInformation}
+          handleCheckboxUpdates={this.handleCheckboxUpdates}
+          hasPermission={readOnlyPermissions}
+          loading={loadingSubscriptionInfo}
+          noResults={!contactInfo.size} />
+    );
+  }
+
+  renderModalHeader = (onClickClose :Function) => {
+    const modalTitle :string = `Court Reminders: ${this.getName()}`;
+    return (
+      <ModalHeaderSection>
+        <ModalTitle>{ modalTitle }</ModalTitle>
+        <CloseButton onClick={onClickClose}>
+          <FontAwesomeIcon color={OL.GREY03} icon={faTimes} size="lg" />
+        </CloseButton>
+      </ModalHeaderSection>
+    );
+  }
+
+  render() {
+    const {
+      isOpen,
+      onClose,
+      subscription,
+    } = this.props;
+    const isSubscribed = subscription.getIn([PROPERTY_TYPES.IS_ACTIVE, 0], false);
+    const actionButtonText :string = isSubscribed ? 'Unsubscribe' : 'Subscribe';
+    return (
+      <Modal
+          isVisible={isOpen}
+          onClickPrimary={() => {}}
+          onClose={onClose}
+          shouldStretchButtons
+          textPrimary={actionButtonText}
+          viewportScrolling
+          withHeader={() => this.renderModalHeader(onClose)}>
+        <SubscriptionWrapper>
+          <div>{ this.renderStatusIcon() }</div>
+          <TextWrapper>
+            {`${this.getName()} ${this.getIsSubscribedText()}`}
+          </TextWrapper>
+        </SubscriptionWrapper>
+        <ModalBodyWrapper padding="0px" noBleed={false} vertical>
+          { this.renderContactInformation() }
+        </ModalBodyWrapper>
+        <AddNewContactSection padding="25px 30px 40px 30px" noBleed={false}>
+          <AddNewContactElementsWrapper>
+            <FontAwesomeIcon color={OL.GREY03} icon={faPlus} size="2x" />
+            <Input onChange={() => {}} />
+            <Button size="sm">Add New Contact</Button>
+          </AddNewContactElementsWrapper>
+        </AddNewContactSection>
+        <ModalBodyWrapper padding="sm">
+          { message }
+        </ModalBodyWrapper>
+      </Modal>
+    );
+  }
+}
+
+const mapStateToProps = (state :Map) => {
+  const app = state.get(STATE.APP);
+  const contactInfo = state.get(STATE.CONTACT_INFO);
+  const edm = state.get(STATE.EDM);
+  const review = state.get(STATE.REVIEW);
+  const subscription = state.get(STATE.SUBSCRIPTIONS);
+  return {
+    [APP_DATA.SELECTED_ORG_ID]: app.get(APP_DATA.SELECTED_ORG_ID),
+    [APP_DATA.SELECTED_ORG_SETTINGS]: app.get(APP_DATA.SELECTED_ORG_SETTINGS),
+    [EDM.FQN_TO_ID]: edm.get(EDM.FQN_TO_ID),
+    [REVIEW.READ_ONLY]: review.get(REVIEW.READ_ONLY),
+    [SUBSCRIPTION_DATA.CONTACT_INFO]: subscription.get(SUBSCRIPTION_DATA.CONTACT_INFO),
+    [SUBSCRIPTION_DATA.PERSON_NEIGHBORS]: subscription.get(SUBSCRIPTION_DATA.PERSON_NEIGHBORS),
+    [SUBSCRIPTION_DATA.SUBSCRIPTION]: subscription.get(SUBSCRIPTION_DATA.SUBSCRIPTION),
+    app,
+    loadSubscriptionModalReqState: getReqState(subscription, SUBSCRIPTION_ACTIONS.LOAD_SUBSCRIPTION_MODAL),
+    submitContactReqState: getReqState(contactInfo, CONTACT_INFO_ACTIONS.SUBMIT_CONTACT),
+    subscribeReqState: getReqState(subscription, SUBSCRIPTION_ACTIONS.SUBSCRIBE),
+    unsubscribeReqState: getReqState(subscription, SUBSCRIPTION_ACTIONS.UNSUBSCRIBE),
+    updateContactsBulkReqState: getReqState(contactInfo, CONTACT_INFO_ACTIONS.UPDATE_CONTACTS_BULK),
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators({
+    clearSubscriptionModal,
+    loadSubcriptionModal,
+    updateContactsBulk,
+    subscribe,
+    unsubscribe,
+  }, dispatch)
+});
+
+// $FlowFixMe
+export default connect(mapStateToProps, mapDispatchToProps)(ManageSubscriptionModal);
