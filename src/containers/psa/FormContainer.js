da0630c153b168b12ccb0ae4632960e81a2af7b3
Todd Bergman <todd@openlattice.com>
2019-12-20 16:03:32 -0800

    pass map as default for subscription and only next page if nextPage

--- src/containers/psa/FormContainer.js
+++ src/containers/psa/FormContainer.js
@@ -209,7 +209,7 @@ const ContextItem = styled(StyledSectionWrapper)`
 const HeaderRow = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: ${props => (props.left ? 'flex-start' : 'space-between')};
+  justify-content: ${(props) => (props.left ? 'flex-start' : 'space-between')};
   align-items: center;
   width: 100%;
   margin-bottom: 20px;
@@ -564,7 +564,7 @@ class Form extends React.Component<Props, State> {
     });
   }
 
-  getFqn = propertyType => `${propertyType.getIn(['type', 'namespace'])}.${propertyType.getIn(['type', 'name'])}`
+  getFqn = (propertyType) => `${propertyType.getIn(['type', 'namespace'])}.${propertyType.getIn(['type', 'name'])}`
 
   shouldLoadCases = () => {
     const { selectedOrganizationSettings } = this.props;
@@ -582,7 +582,7 @@ class Form extends React.Component<Props, State> {
     const { selectedOrganizationSettings } = this.props;
     const skipLoad = !selectedOrganizationSettings.get(SETTINGS.ARRESTS_INTEGRATED, true);
     const nextPage = getNextPath(window.location, numPages, skipLoad);
-    this.handlePageChange(nextPage);
+    if (nextPage) this.handlePageChange(nextPage);
   }
 
   prevPage = () => {
@@ -609,7 +609,7 @@ class Form extends React.Component<Props, State> {
     this.submitEntities(
       scores.set(PROPERTY_TYPES.STATUS, List.of(PSA_STATUSES.OPEN)), riskFactors, dmf, dmfRiskFactors
     );
-    this.nextPage()
+    this.nextPage();
   }
 
   clear = () => {
@@ -684,7 +684,7 @@ class Form extends React.Component<Props, State> {
               classNamePrefix="lattice-select"
               options={Object.values(STATUS_OPTIONS_FOR_PENDING_PSAS)}
               onChange={
-                e => (this.setState({ status: e.label }))
+                (e) => (this.setState({ status: e.label }))
               } />
         </FilterWrapper>
       </PSARowListSubHeader>
@@ -705,7 +705,7 @@ class Form extends React.Component<Props, State> {
       ? openPSAs.map(getNeighborDetails)
       : allPSAs.map(getNeighborDetails);
     if (!PSAScores.size) return null;
-    const scoreSeq = PSAScores.map(scores => ([getEntityKeyId(scores), scores]));
+    const scoreSeq = PSAScores.map((scores) => ([getEntityKeyId(scores), scores]));
     return (
       <CenteredListWrapper>
         {this.renderPendingPSAsHeader()}
@@ -762,7 +762,7 @@ class Form extends React.Component<Props, State> {
     );
   }
 
-  formatCharge = charge => (
+  formatCharge = (charge) => (
     `${
       charge.getIn([PROPERTY_TYPES.REFERENCE_CHARGE_STATUTE, 0], '')
     } ${
@@ -796,8 +796,8 @@ class Form extends React.Component<Props, State> {
     });
 
     const sortedChargeList = chargeOptions.valueSeq()
-      .sortBy(charge => getFirstNeighborValue(charge.value, PROPERTY_TYPES.REFERENCE_CHARGE_DESCRIPTION))
-      .sortBy(charge => getFirstNeighborValue(charge.value, PROPERTY_TYPES.REFERENCE_CHARGE_STATUTE));
+      .sortBy((charge) => getFirstNeighborValue(charge.value, PROPERTY_TYPES.REFERENCE_CHARGE_DESCRIPTION))
+      .sortBy((charge) => getFirstNeighborValue(charge.value, PROPERTY_TYPES.REFERENCE_CHARGE_STATUTE));
 
     return {
       chargeOptions: chargeOptions.sortBy((statute, _) => statute),
@@ -850,7 +850,7 @@ class Form extends React.Component<Props, State> {
       selectedOrganizationSettings,
       personNeighbors
     } = this.props;
-    const subscription = personNeighbors.get(SUBSCRIPTION);
+    const subscription = personNeighbors.get(SUBSCRIPTION, Map());
     const courtRemindersEnabled = selectedOrganizationSettings.get(SETTINGS.COURT_REMINDERS, false);
     return courtRemindersEnabled
       ? (
