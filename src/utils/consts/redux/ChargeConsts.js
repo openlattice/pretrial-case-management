89f43bb8db7960c09cebf690b96411f4b8e9ff15
Todd Bergman <todd@openlattice.com>
2020-02-04 10:26:27 -0800

    Charge Consts

---
+++ src/utils/consts/redux/ChargeConsts.js
@@ -0,0 +1,25 @@
+/* @flow */
+/* eslint-disable import/prefer-default-export */
+
+export const CHARGE_DATA = {
+  /* Arrest */
+  ARREST_CHARGES_BY_ID: 'arrestChargesById',
+  ARREST_CHARGES_BY_FLAG: 'arrestChargesByFlag',
+  ARREST_MAX_LEVEL_INCREASE: 'arrestMaxLevelIncreaseCharges',
+  ARREST_PERMISSIONS: 'arrestChargePermissions',
+  ARREST_SINGLE_LEVEL_INCREASE: 'arrestSingleLevelIncreaseCharges',
+  ARREST_VIOLENT: 'violentArrestCharges',
+  BRE: 'bookingReleaseExceptionCharges',
+  BHE: 'bookingHoldExceptionCharges',
+
+  /* Arresting Agencies */
+  ARRESTING_AGENCIES: 'arrestingAgencies',
+
+  /* Court */
+  COURT_CHARGES_BY_ID: 'courtChargesById',
+  COURT_CHARGES_BY_FLAG: 'courtChargesByFlag',
+  COURT_MAX_LEVEL_INCREASE: 'courtMaxLevelIncreaseCharges',
+  COURT_PERMISSIONS: 'courtChargePermissions',
+  COURT_SINGLE_LEVEL_INCREASE: 'courtSingleLevelIncreaseCharges',
+  COURT_VIOLENT: 'violentCourtCharges',
+};
