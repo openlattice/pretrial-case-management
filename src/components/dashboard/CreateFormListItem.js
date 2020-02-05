fe1dd065eef6875c7a694a764d33f534bf4b66cf
Todd Bergman <todd@openlattice.com>
2020-02-05 12:06:35 -0800

    New List time styles for CEPP and all of PCM

--- src/components/dashboard/CreateFormListItem.js
+++ src/components/dashboard/CreateFormListItem.js
@@ -6,31 +6,39 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-import FormListItemIcon from './FormListItemIcon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import { OL } from '../../utils/consts/Colors';
 
 const ItemWrapper = styled(Link)`
   align-items: center;
   background: ${OL.WHITE};
-  color: inherit;
+  color: ${OL.GREY03};
   display: flex;
-  height: 55px;
-  padding: 8px;
-  margin: 10px 0;
+  flex-direction: column;
+  height: 180px;
+  margin: 15px auto;
   width: 100%;
   border: 1px solid ${OL.GREY11};
   border-radius: 5px;
 
   &:hover {
-    background: ${OL.GREY25};
+    color: ${OL.GREY25};
     color: inherit;
     text-decoration: none;
+  }
+
+  svg {
+    padding: 40px 0 30px;
+    height: 65%;
+    width: 100%;
   }
 `;
 
 const StyledName = styled.div`
-  color: ${OL.GREY26};
-  font-size: 18px;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 22px;
 `;
 
 type Props = {
@@ -44,7 +52,7 @@ const CreateFormListItem = (props :Props) => {
   const { icon, name, path } = props;
   return (
     <ItemWrapper to={path}>
-      <FormListItemIcon icon={icon} />
+      <FontAwesomeIcon icon={icon} size="3x" />
       <StyledName>{name}</StyledName>
     </ItemWrapper>
   );
