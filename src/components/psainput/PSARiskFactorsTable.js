0b3557e55dd30df65c559ca36962c916465dada1
Todd Bergman <todd@openlattice.com>
2020-02-05 10:16:56 -0800

    update format to formatRiskFactor

--- src/components/psainput/PSARiskFactorsTable.js
+++ src/components/psainput/PSARiskFactorsTable.js
@@ -12,11 +12,15 @@ import RiskFactorsTable from '../riskfactors/RiskFactorsTable';
 import { PROPERTY_TYPES } from '../../utils/consts/DataModelConsts';
 import { ResultHeader } from '../../utils/Layout';
 
-const format = (valList) => {
-  if (!valList || !valList.length) return '';
-  const val = valList[0];
-  if (val.length) return val;
-  return val ? 'Yes' : 'No';
+const formatRiskFactor = (riskFactorList) => {
+  // if riskFactor is a string return the riskFactor
+  if (!riskFactorList || !riskFactorList.length || riskFactorList.includes(undefined)) return '';
+  const riskFactor = riskFactorList[0];
+  // if riskFactor is a string return the riskFactor
+  if (typeof riskFactor === 'string') return riskFactor;
+  // if riskFactor is a boolean return 'Yes' for true, and 'No' for false.
+  if (typeof riskFactor === 'boolean') return riskFactor ? 'Yes' : 'No';
+  return '';
 };
 
 const ResultHeaderForCard = styled(ResultHeader)`
@@ -34,59 +38,59 @@ const PSARiskFactorsTable = ({ riskFactors } :Props) => {
     {
       number: 1,
       riskFactor: 'Age at Current Arrest',
-      response: format(riskFactors[PROPERTY_TYPES.AGE_AT_CURRENT_ARREST])
+      response: formatRiskFactor(riskFactors[PROPERTY_TYPES.AGE_AT_CURRENT_ARREST])
     },
     {
       number: 2,
       riskFactor: 'Current Violent Offense',
-      response: format(riskFactors[PROPERTY_TYPES.CURRENT_VIOLENT_OFFENSE])
+      response: formatRiskFactor(riskFactors[PROPERTY_TYPES.CURRENT_VIOLENT_OFFENSE])
     },
     {
       number: '2a',
       riskFactor: 'Current Violent Offense & 20 Years Old or Younger',
       italicText: '(calculated from 1 and 2)',
-      response: format(riskFactors[PROPERTY_TYPES.CURRENT_VIOLENT_OFFENSE_AND_YOUNG])
+      response: formatRiskFactor(riskFactors[PROPERTY_TYPES.CURRENT_VIOLENT_OFFENSE_AND_YOUNG])
     },
     {
       number: 3,
       riskFactor: 'Pending Charge at the Time of the Offense',
-      response: format(riskFactors[PROPERTY_TYPES.PENDING_CHARGE])
+      response: formatRiskFactor(riskFactors[PROPERTY_TYPES.PENDING_CHARGE])
     },
     {
       number: 4,
       riskFactor: 'Prior Misdemeanor Conviction',
-      response: format(riskFactors[PROPERTY_TYPES.PRIOR_MISDEMEANOR])
+      response: formatRiskFactor(riskFactors[PROPERTY_TYPES.PRIOR_MISDEMEANOR])
     },
     {
       number: 5,
       riskFactor: 'Prior Felony Conviction',
-      response: format(riskFactors[PROPERTY_TYPES.PRIOR_FELONY])
+      response: formatRiskFactor(riskFactors[PROPERTY_TYPES.PRIOR_FELONY])
     },
     {
       number: '5a',
       riskFactor: 'Prior Conviction',
       italicText: '(calculated from 4 and 5)',
-      response: format(riskFactors[PROPERTY_TYPES.PRIOR_CONVICTION])
+      response: formatRiskFactor(riskFactors[PROPERTY_TYPES.PRIOR_CONVICTION])
     },
     {
       number: 6,
       riskFactor: 'Prior Violent Conviction',
-      response: format(riskFactors[PROPERTY_TYPES.PRIOR_VIOLENT_CONVICTION])
+      response: formatRiskFactor(riskFactors[PROPERTY_TYPES.PRIOR_VIOLENT_CONVICTION])
     },
     {
       number: 7,
       riskFactor: 'Prior Pretrial Failure to Appear in Past 2 Years',
-      response: format(riskFactors[PROPERTY_TYPES.PRIOR_FAILURE_TO_APPEAR_RECENT])
+      response: formatRiskFactor(riskFactors[PROPERTY_TYPES.PRIOR_FAILURE_TO_APPEAR_RECENT])
     },
     {
       number: 8,
       riskFactor: 'Prior Pretrial Failure to Appear Older than 2 Years',
-      response: format(riskFactors[PROPERTY_TYPES.PRIOR_FAILURE_TO_APPEAR_OLD])
+      response: formatRiskFactor(riskFactors[PROPERTY_TYPES.PRIOR_FAILURE_TO_APPEAR_OLD])
     },
     {
       number: 9,
       riskFactor: 'Prior Sentence to Incarceration',
-      response: format(riskFactors[PROPERTY_TYPES.PRIOR_SENTENCE_TO_INCARCERATION])
+      response: formatRiskFactor(riskFactors[PROPERTY_TYPES.PRIOR_SENTENCE_TO_INCARCERATION])
     }
   ]);
 
