d870022f694ac644ae97ae678097c628cb84918f
Todd Bergman <todd@openlattice.com>
2020-02-19 20:30:46 -0800

    moved setting defaults to SettingsConsts

--- src/utils/consts/redux/SettingsConsts.js
+++ src/utils/consts/redux/SettingsConsts.js
@@ -2,6 +2,20 @@
  * @flow
  */
 
+import {
+  defaultMatrix,
+  defaultConditions,
+  defaultLevels,
+  THEMES
+} from '../RCMResultsConsts';
+import {
+  CASE_CONTEXTS,
+  CONTEXTS,
+  MODULE,
+  RCM,
+  SETTINGS,
+} from '../AppSettingConsts';
+
 export const SETTINGS_ACTIONS = {
   SUBMIT_SETTINGS: 'submitSettings'
 };
@@ -9,3 +23,36 @@ export const SETTINGS_ACTIONS = {
 export const SETTINGS_DATA = {
   APP_SETTINGS: 'appSettings'
 };
+
+
+export const defaultRCM = {
+  [RCM.CONDITIONS]: defaultConditions,
+  [RCM.LEVELS]: defaultLevels,
+  [RCM.MATRIX]: defaultMatrix,
+  [RCM.THEME]: THEMES.CLASSIC
+};
+
+
+export const DEFAULT_SETTINGS = {
+  [SETTINGS.ARRESTS_INTEGRATED]: false,
+  [SETTINGS.COURT_CASES_INTEGRATED]: false,
+  [SETTINGS.COURT_REMINDERS]: false,
+  [SETTINGS.ENROLL_VOICE]: false,
+  [SETTINGS.STEP_INCREASES]: false,
+  [SETTINGS.SECONDARY_BOOKING_CHARGES]: false,
+  [SETTINGS.LOAD_CASES]: false,
+  [SETTINGS.CASE_CONTEXTS]: {
+    [CONTEXTS.BOOKING]: CASE_CONTEXTS.ARREST,
+    [CONTEXTS.COURT]: CASE_CONTEXTS.COURT
+  },
+  [SETTINGS.CONTEXTS]: {
+    [CONTEXTS.BOOKING]: false,
+    [CONTEXTS.COURT]: true
+  },
+  [SETTINGS.MODULES]: {
+    [MODULE.PSA]: true,
+    [MODULE.PRETRIAL]: false
+  },
+  [SETTINGS.PREFERRED_COUNTY]: '',
+  [SETTINGS.RCM]: defaultRCM
+}
