3f85cb815f3eefc928f0ed255d33a71c26836dbb
Todd Bergman <todd@openlattice.com>
2020-02-19 20:29:51 -0800

    pulled default settings into const

--- src/containers/settings/SettingsReducer.js
+++ src/containers/settings/SettingsReducer.js
@@ -13,22 +13,15 @@ import {
 } from './SettingsActions';
 
 import { SWITCH_ORGANIZATION } from '../app/AppActionFactory';
-import {
-  defaultMatrix,
-  defaultConditions,
-  defaultLevels,
-  THEMES
-} from '../../utils/consts/RCMResultsConsts';
-import {
-  CASE_CONTEXTS,
-  CONTEXTS,
-  MODULE,
-  RCM,
-  SETTINGS,
-} from '../../utils/consts/AppSettingConsts';
+import { RCM, SETTINGS } from '../../utils/consts/AppSettingConsts';
 
 import { REDUX } from '../../utils/consts/redux/SharedConsts';
-import { SETTINGS_ACTIONS, SETTINGS_DATA } from '../../utils/consts/redux/SettingsConsts';
+import {
+  defaultRCM,
+  DEFAULT_SETTINGS,
+  SETTINGS_ACTIONS,
+  SETTINGS_DATA
+} from '../../utils/consts/redux/SettingsConsts';
 
 const {
   FAILURE,
@@ -36,13 +29,6 @@ const {
   STANDBY,
   SUCCESS
 } = RequestStates;
-
-const defaultRCM = {
-  [RCM.CONDITIONS]: defaultConditions,
-  [RCM.LEVELS]: defaultLevels,
-  [RCM.MATRIX]: defaultMatrix,
-  [RCM.THEME]: THEMES.CLASSIC
-};
 
 const INITIAL_STATE :Map<*, *> = fromJS({
   [REDUX.ACTIONS]: {
@@ -53,30 +39,7 @@ const INITIAL_STATE :Map<*, *> = fromJS({
   [REDUX.ERRORS]: {
     [SETTINGS_ACTIONS.SUBMIT_SETTINGS]: Map()
   },
-  [SETTINGS_DATA.APP_SETTINGS]: {
-    [SETTINGS.ARRESTS_INTEGRATED]: false,
-    [SETTINGS.COURT_CASES_INTEGRATED]: false,
-    [SETTINGS.COURT_REMINDERS]: false,
-    [SETTINGS.ENROLL_VOICE]: false,
-    [SETTINGS.STEP_INCREASES]: false,
-    [SETTINGS.SECONDARY_BOOKING_CHARGES]: false,
-    [SETTINGS.LOAD_CASES]: false,
-    [SETTINGS.CASE_CONTEXTS]: {
-      [CONTEXTS.BOOKING]: CASE_CONTEXTS.ARREST,
-      [CONTEXTS.COURT]: CASE_CONTEXTS.COURT
-    },
-    [SETTINGS.CONTEXTS]: {
-      [CONTEXTS.BOOKING]: false,
-      [CONTEXTS.COURT]: true
-    },
-    [SETTINGS.MODULES]: {
-      [MODULE.PSA]: true,
-      [MODULE.PRETRIAL]: false
-    },
-    [SETTINGS.PREFERRED_COUNTY]: '',
-    [SETTINGS.RCM]: defaultRCM
-  }
-
+  [SETTINGS_DATA.APP_SETTINGS]: DEFAULT_SETTINGS
 });
 
 export default function settingsReducer(state :Map<*, *> = INITIAL_STATE, action :Object) {
