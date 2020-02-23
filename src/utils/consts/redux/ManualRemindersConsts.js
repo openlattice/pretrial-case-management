0a5011e9a89a6d75c5a0606361c416698faebb55
Todd Bergman <todd@openlattice.com>
2020-02-22 16:15:40 -0800

    Created ManualRemindersConsts and deleted them from FrontEndStateConsts

---
+++ src/utils/consts/redux/ManualRemindersConsts.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/prefer-default-export */
+
+export const MANUAL_REMINDERS_DATA = {
+  FAILED_REMINDER_IDS: 'failedManualReminderIds',
+  REMINDER_IDS: 'manualReminderIds',
+  REMINDER_NEIGHBORS: 'manualReminderNeighborsById',
+  REMINDERS_BY_ID: 'manualRemindersById',
+  PEOPLE_NEIGHBORS: 'peopleNeighborsForManualReminder',
+  PEOPLE_RECEIVING_REMINDERS: 'peopleReceivingManualReminders',
+  SUBMITTED_MANUAL_REMINDER: 'submittedManualReminder',
+  SUCCESSFUL_REMINDER_IDS: 'successfulManualReminderIds',
+};
