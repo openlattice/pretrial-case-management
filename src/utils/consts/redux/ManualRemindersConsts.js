bd5a7336a31efb9efbdc6581705bb1bfd7d496c6
Todd Bergman <todd@openlattice.com>
2020-02-22 16:33:51 -0800

    alpha

--- src/utils/consts/redux/ManualRemindersConsts.js
+++ src/utils/consts/redux/ManualRemindersConsts.js
@@ -2,11 +2,11 @@
 
 export const MANUAL_REMINDERS_DATA = {
   FAILED_REMINDER_IDS: 'failedManualReminderIds',
+  PEOPLE_NEIGHBORS: 'peopleNeighborsForManualReminder',
+  PEOPLE_RECEIVING_REMINDERS: 'peopleReceivingManualReminders',
   REMINDER_IDS: 'manualReminderIds',
   REMINDER_NEIGHBORS: 'manualReminderNeighborsById',
   REMINDERS_BY_ID: 'manualRemindersById',
-  PEOPLE_NEIGHBORS: 'peopleNeighborsForManualReminder',
-  PEOPLE_RECEIVING_REMINDERS: 'peopleReceivingManualReminders',
   SUBMITTED_MANUAL_REMINDER: 'submittedManualReminder',
   SUCCESSFUL_REMINDER_IDS: 'successfulManualReminderIds',
 };
