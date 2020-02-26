43cbb039d0a7a34577b0d0d822645efc1bee85a5
Todd Bergman <todd@openlattice.com>
2020-02-26 22:22:51 +0900

    Switched intitializeAppliccation to Request Sequence

--- src/containers/settings/SettingsActions.js
+++ src/containers/settings/SettingsActions.js
@@ -4,25 +4,22 @@
 import { newRequestSequence } from 'redux-reqseq';
 import type { RequestSequence } from 'redux-reqseq';
 
-const DELETE_RCM_CONDITION:'DELETE_RCM_CONDITION' = 'DELETE_RCM_CONDITION';
-const deleteRCMCondition = value => ({
+const DELETE_RCM_CONDITION :'DELETE_RCM_CONDITION' = 'DELETE_RCM_CONDITION';
+const deleteRCMCondition = (value :Object) => ({
   type: DELETE_RCM_CONDITION,
   value
 });
 
-const INITIALIZE_SETTINGS:'INITIALIZE_SETTINGS' = 'INITIALIZE_SETTINGS';
-const initializeSettings = value => ({
-  type: INITIALIZE_SETTINGS,
-  value
-});
-
-const UPDATE_SETTING:'UPDATE_SETTING' = 'UPDATE_SETTING';
-const updateSetting = value => ({
+const UPDATE_SETTING :'UPDATE_SETTING' = 'UPDATE_SETTING';
+const updateSetting = (value :Object) => ({
   type: UPDATE_SETTING,
   value
 });
 
-const SUBMIT_SETTINGS :string = 'SUBMIT_SETTINGS';
+const INITIALIZE_SETTINGS :'INITIALIZE_SETTINGS' = 'INITIALIZE_SETTINGS';
+const initializeSettings :RequestSequence = newRequestSequence(INITIALIZE_SETTINGS);
+
+const SUBMIT_SETTINGS :'SUBMIT_SETTINGS' = 'SUBMIT_SETTINGS';
 const submitSettings :RequestSequence = newRequestSequence(SUBMIT_SETTINGS);
 
 export {
