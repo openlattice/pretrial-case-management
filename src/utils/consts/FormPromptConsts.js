9c1210984c0aea780ba4070f6a6f28479d4fee6c
Todd Bergman <todd@openlattice.com>
2020-03-13 17:35:53 -0700

    updates

--- src/utils/consts/FormPromptConsts.js
+++ src/utils/consts/FormPromptConsts.js
@@ -3,15 +3,15 @@
  */
 /* eslint max-len: 0 */ // --> OFF
 
-export const CURRENT_AGE_PROMPT = 'What was the person’s age on the date at the time of the arrest?';
+export const CURRENT_AGE_PROMPT = 'What was the person’s age on the date of the arrest?';
 export const CURRENT_VIOLENT_OFFENSE_PROMPT = 'Is any current charge considered violent?';
-export const PENDING_CHARGE_PROMPT = 'Did the person have at least one other a pending charge at the time of the arrest? ';
-export const PRIOR_MISDEMEANOR_PROMPT = 'Does the defendant person have any prior misdemeanor convictions?';
-export const PRIOR_FELONY_PROMPT = 'Does the defendant person have any prior felony convictions?';
-export const PRIOR_VIOLENT_CONVICTION_PROMPT = 'How many prior violent convictions does the defendant person have?';
-export const PRIOR_FAILURE_TO_APPEAR_RECENT_PROMPT = 'How many prior pretrial failures to appear does the defendant person have in the past 2 years?';
-export const PRIOR_FAILURE_TO_APPEAR_OLD_PROMPT = 'Does the defendant person have any prior pretrial failures to appear that are older than 2 years?';
-export const PRIOR_SENTENCE_TO_INCARCERATION_PROMPT = 'Does the defendant person have any prior sentences to incarceration?';
+export const PENDING_CHARGE_PROMPT = 'Did the person have a pending charge at the time of the arrest? ';
+export const PRIOR_MISDEMEANOR_PROMPT = 'Does the person have any prior misdemeanor convictions?';
+export const PRIOR_FELONY_PROMPT = 'Does the person have any prior felony convictions?';
+export const PRIOR_VIOLENT_CONVICTION_PROMPT = 'How many prior violent convictions does the person have?';
+export const PRIOR_FAILURE_TO_APPEAR_RECENT_PROMPT = 'How many prior pretrial failures to appear does the person have in the past 2 years?';
+export const PRIOR_FAILURE_TO_APPEAR_OLD_PROMPT = 'Does the person have any prior pretrial failures to appear that are older than 2 years?';
+export const PRIOR_SENTENCE_TO_INCARCERATION_PROMPT = 'Does the person have any prior sentences to incarceration?';
 export const EXTRADITED_PROMPT = 'Was the defendant extradited for any current charge?';
 export const SECONDARY_RELEASE_CHARGES_PROMPT = 'Does the booking hold exception list apply?';
 export const SECONDARY_HOLD_CHARGES_PROMPT = 'Does the booking release exception list apply?';
