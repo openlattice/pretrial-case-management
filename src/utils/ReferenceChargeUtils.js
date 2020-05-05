e939fcf19c8aa7fa47044f15eb6a385d24042e18
Todd Bergman <todd@openlattice.com>
2020-05-05 13:31:25 -0700

    remove console.logs

--- src/utils/ReferenceChargeUtils.js
+++ src/utils/ReferenceChargeUtils.js
@@ -13,8 +13,6 @@ export const getValidRow = (row :Object, edm :Map) => {
 
   const hasRequiredFields = row.statute && row.description;
 
-  console.log(row);
-
   const isValid = Object.keys(row).reduce((acc, column) => {
     if (!acc) {
       return acc;
@@ -41,36 +39,35 @@ export const getValidRow = (row :Object, edm :Map) => {
 
     const propertyType = PROPERTY_TYPE_MAPPINGS[column];
     const ptid = getPropertyTypeId(edm, propertyType);
-    console.log(propertyType);
-    console.log(ptid);
     returnRow[ptid] = [row[column]];
 
     return true;
   }, true);
 
-  console.log(returnRow);
 
   return isValid && hasRequiredFields ? returnRow : null;
 };
 
 export async function parseCsvToJson({ file, edm } :Object) {
-  console.log(edm.toJS());
   return new Promise((resolve, reject) => {
     // first row in CSV should be header
     let rowNumber = 2;
     const csvToJSON = [];
     const badRows = [];
+    let headers = [];
 
     Papa.parse(file, {
       complete: () => resolve({
         data: csvToJSON,
-        error: badRows.length ? badRows : undefined
+        error: badRows.length ? badRows : undefined,
+        headers
       }),
       error: reject,
       header: true,
       dynamicTyping: DYNAMIC_TYPED_COLUMNS,
       step: (results) => {
-        const { data } = results;
+        const { data, meta } = results;
+        headers = meta.fields;
         const row = getValidRow(data, edm);
         if (row) {
           csvToJSON.push(row);
