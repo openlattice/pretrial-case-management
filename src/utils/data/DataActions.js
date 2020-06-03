8df0c79f30df68318e2a8af2369ba141df10d426
Todd Bergman <todd@openlattice.com>
2020-06-03 11:12:54 -0700

    fix type

--- src/utils/data/DataActions.js
+++ src/utils/data/DataActions.js
@@ -5,10 +5,10 @@
 import { newRequestSequence } from 'redux-reqseq';
 import type { RequestSequence } from 'redux-reqseq';
 
-const CREATE_ASSOCIATIONS :string = 'CREATE_ASSOCIATIONS';
+const CREATE_ASSOCIATIONS :'CREATE_ASSOCIATIONS' = 'CREATE_ASSOCIATIONS';
 const createAssociations :RequestSequence = newRequestSequence(CREATE_ASSOCIATIONS);
 
-const DELETE_ENTITY :string = 'DELETE_ENTITY';
+const DELETE_ENTITY :'DELETE_ENTITY' = 'DELETE_ENTITY';
 const deleteEntity :RequestSequence = newRequestSequence(DELETE_ENTITY);
 
 export {
