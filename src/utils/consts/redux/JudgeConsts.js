493399363f37c627d34b05d48bc0aacf2be1bb8f
Todd Bergman <todd@openlattice.com>
2020-08-26 16:03:25 -0400

    added judgesUpdating

--- src/utils/consts/redux/JudgeConsts.js
+++ src/utils/consts/redux/JudgeConsts.js
@@ -5,5 +5,6 @@
 export default {
   ALL_JUDGES: 'allJudges',
   JUDGES_BY_ID: 'judgesById',
-  JUDGES_BY_COUNTY: 'judgesByCounty'
+  JUDGES_BY_COUNTY: 'judgesByCounty',
+  JUDGES_UPDATING: 'judgesUpdating'
 };
