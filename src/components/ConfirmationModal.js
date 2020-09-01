80353603b00f682258b73aa9d5d313c79234988b
Todd Bergman <todd@openlattice.com>
2020-09-01 13:51:09 -0400

    Switch confirmation modal to LUK modal

--- src/components/ConfirmationModal.js
+++ src/components/ConfirmationModal.js
@@ -4,15 +4,14 @@
 
 import React from 'react';
 import styled from 'styled-components';
-import { Button } from 'lattice-ui-kit';
+import { Button, Modal } from 'lattice-ui-kit';
 // $FlowFixMe
-import Modal, { ModalTransition } from '@atlaskit/modal-dialog';
 
 import { Wrapper } from '../utils/Layout';
 
 const ModalBody = styled.div`
-  width: 100%;
-  height: 100%;
+  width: 300px;
+  height: 200px;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
@@ -20,16 +19,11 @@ const ModalBody = styled.div`
   padding: 30px 0;
 `;
 
-const MessageBody = styled.div`
-  text-align: center;
-`;
-
 const ButtonContainer = styled.div`
   width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 20px;
 `;
 
 type Props = {
@@ -41,9 +35,6 @@ type Props = {
   open :boolean;
   onClose :() => void
 }
-
-const MODAL_WIDTH = '300px';
-const MODAL_HEIGHT = '200px';
 
 const ConfirmationModal = ({
   confirmationAction,
@@ -57,7 +48,7 @@ const ConfirmationModal = ({
   const confirmationText = customText || `Are you sure you want to ${confirmationType} this ${objectType}?`;
 
   const buttons = customText
-    ? <Button color="secondary" disabled={disabled} onClick={onClose}>OK</Button>
+    ? <Button color="secondary" disabled={disabled} onClick={onClose}>Ok</Button>
     : (
       <>
         <Button color="secondary" disabled={disabled} onClick={confirmationAction}>Yes</Button>
@@ -66,28 +57,16 @@ const ConfirmationModal = ({
     );
   return (
     <Wrapper>
-      <ModalTransition>
-        {
-          open
-          && (
-            <Modal
-                scrollBehavior="outside"
-                onClose={onClose}
-                width={MODAL_WIDTH}
-                height={MODAL_HEIGHT}
-                max-height={MODAL_HEIGHT}
-                shouldCloseOnOverlayClick
-                stackIndex={30}>
-              <ModalBody>
-                <MessageBody>{confirmationText}</MessageBody>
-                <ButtonContainer>
-                  { buttons }
-                </ButtonContainer>
-              </ModalBody>
-            </Modal>
-          )
-        }
-      </ModalTransition>
+      <Modal
+          isVisible={open}
+          onClose={onClose}
+          onClicckSecondary={onClose}
+          onClickPrimary={confirmationAction}
+          shouldStretchButtons
+          textPrimary="Yes"
+          textSecondary="No"
+          textTitle={confirmationText}
+          viewportScrolling />
     </Wrapper>
   );
 };
