aff19820254e63e8f70f8ef4bafd2d0e9bbcedf5
Todd Bergman <todd@openlattice.com>
2020-09-10 12:36:30 -0700

    No tooltip

--- src/components/hearings/HearingRow.js
+++ src/components/hearings/HearingRow.js
@@ -5,20 +5,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Map } from 'immutable';
-import { Button, Tooltip } from 'lattice-ui-kit';
+import { Button } from 'lattice-ui-kit';
 
 import { OL } from '../../utils/consts/Colors';
-
-const CaseId = styled.div`
-  color: ${OL.GREY15};
-  display: block;
-  font-size: 14px;
-  overflow: hidden;
-  position: relative;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  width: 100%;
-`;
 
 const Cell = styled.td`
   align-items: center;
@@ -30,15 +19,6 @@ const Cell = styled.td`
 
 const Row = styled.tr`
   border-bottom: 1px solid ${OL.GREY11};
-
-  &:hover {
-    cursor: ${(props :Object) => (props.disabled ? 'default' : 'pointer')};
-    background: ${(props :Object) => (props.disabled ? OL.WHITE : OL.GREY14)};
-  }
-
-  &:active {
-    background-color: ${OL.GREY08};
-  }
 
   &:last-child {
     border-bottom: none;
@@ -62,16 +42,11 @@ const HearingRow = ({
   data,
   openConfirmationModal
 } :Props) => (
-  <Row
-      disabled>
+  <Row key={data.id}>
     <Cell>{ data.dateTime }</Cell>
     <Cell>{ data.courtroom }</Cell>
     <Cell>{ data.type }</Cell>
-    <Cell>
-      <Tooltip arrow placement="top" title={data.caseId}>
-        <div>{data.caseId}</div>
-      </Tooltip>
-    </Cell>
+    <Cell>{ data.caseId }</Cell>
     <Cell>
       <Button size="small" onClick={() => openConfirmationModal(data.hearing)} disabled={data.disabled}>
         Cancel
