4d60e2d91d5c18962bbff561cfe40180239c5e25
Todd Bergman <todd@openlattice.com>
2020-09-10 12:37:15 -0700

    Refactor

--- src/components/ConfirmationModal.js
+++ src/components/ConfirmationModal.js
@@ -3,28 +3,9 @@
  */
 
 import React from 'react';
-import styled from 'styled-components';
-import { Button, Modal } from 'lattice-ui-kit';
-// $FlowFixMe
+import { Modal, ModalFooter } from 'lattice-ui-kit';
 
 import { Wrapper } from '../utils/Layout';
-
-const ModalBody = styled.div`
-  width: 300px;
-  height: 200px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  padding: 30px 0;
-`;
-
-const ButtonContainer = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-gap: 20px;
-`;
 
 type Props = {
   customText ?:string;
@@ -44,32 +25,26 @@ const ConfirmationModal = ({
   open,
   onClose,
   objectType
-} :Props) => {
-  const confirmationText = customText || `Are you sure you want to ${confirmationType} this ${objectType}?`;
-
-  const buttons = customText
-    ? <Button color="secondary" disabled={disabled} onClick={onClose}>Ok</Button>
-    : (
-      <>
-        <Button color="secondary" disabled={disabled} onClick={confirmationAction}>Yes</Button>
-        <Button disabled={disabled} onClick={onClose}>No</Button>
-      </>
-    );
-  return (
-    <Wrapper>
-      <Modal
-          isVisible={open}
-          onClose={onClose}
-          onClicckSecondary={onClose}
-          onClickPrimary={confirmationAction}
-          shouldStretchButtons
-          textPrimary="Yes"
-          textSecondary="No"
-          textTitle={confirmationText}
-          viewportScrolling />
-    </Wrapper>
-  );
-};
+} :Props) => (
+  <Wrapper>
+    <Modal
+        isVisible={open}
+        onClose={onClose}
+        withFooter={(
+          <ModalFooter
+              isPendingPrimary={disabled}
+              isDisabledSecondary={disabled}
+              onClickPrimary={confirmationAction}
+              onClicckSecondary={onClose}
+              shouldStretchButtons
+              textPrimary="Yes"
+              textSecondary="No" />
+        )}
+        viewportScrolling>
+      { customText || `Are you sure you want to ${confirmationType} this ${objectType}?` }
+    </Modal>
+  </Wrapper>
+);
 
 ConfirmationModal.defaultProps = {
   customText: '',
