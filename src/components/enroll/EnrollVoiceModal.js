adc596b67ca060b2186abc91e5cec366c60c499c
Todd Bergman <todd@openlattice.com>
2020-09-10 12:42:48 -0700

    switch to LUK Modal

--- src/components/enroll/EnrollVoiceModal.js
+++ src/components/enroll/EnrollVoiceModal.js
@@ -4,18 +4,18 @@
 
 import React from 'react';
 import styled from 'styled-components';
-import Modal, { ModalTransition } from '@atlaskit/modal-dialog';
+import { Modal } from 'lattice-ui-kit';
 
 import EnrollVoice from '../../containers/enroll/EnrollVoice';
 import {
-  CloseModalX,
   PaddedStyledColumnRow,
   TitleWrapper,
   Wrapper
 } from '../../utils/Layout';
 
 const ModalBody = styled.div`
-  width: 100%;
+  width: 750px;
+  height: max-content;
   padding: 0 30px;
 `;
 
@@ -29,12 +29,9 @@ type Props = {
   personId :string,
   personEntityKeyId :string,
   personName :string,
-  open :() => void,
+  open :boolean,
   onClose :() => void
 }
-
-const MODAL_WIDTH = '750px';
-const MODAL_HEIGHT = 'max-content';
 
 const EnrollVoiceModal = ({
   personId,
@@ -44,40 +41,25 @@ const EnrollVoiceModal = ({
   open
 } :Props) => (
   <Wrapper>
-    <ModalTransition>
-      {
-        open
-        && (
-          <Modal
-              scrollBehavior="outside"
+    <Modal
+        isVisible={open}
+        viewportScrolling
+        onClose={onClose}>
+      <ModalBody>
+        <ColumnRow>
+          <TitleWrapper noPadding>
+            <h2>Enroll Voice Profile</h2>
+          </TitleWrapper>
+        </ColumnRow>
+        <ColumnRow>
+          <EnrollVoice
               onClose={onClose}
-              width={MODAL_WIDTH}
-              height={MODAL_HEIGHT}
-              max-height={MODAL_HEIGHT}
-              shouldCloseOnOverlayClick
-              stackIndex={20}>
-            <ModalBody>
-              <ColumnRow>
-                <TitleWrapper noPadding>
-                  <h2>Enroll Voice Profile</h2>
-                  <div>
-                    <CloseModalX onClick={onClose} />
-                  </div>
-                </TitleWrapper>
-              </ColumnRow>
-              <ColumnRow>
-                <EnrollVoice
-                    open={open}
-                    onClose={onClose}
-                    personId={personId}
-                    personEntityKeyId={personEntityKeyId}
-                    personName={personName} />
-              </ColumnRow>
-            </ModalBody>
-          </Modal>
-        )
-      }
-    </ModalTransition>
+              personId={personId}
+              personEntityKeyId={personEntityKeyId}
+              personName={personName} />
+        </ColumnRow>
+      </ModalBody>
+    </Modal>
   </Wrapper>
 );
 
