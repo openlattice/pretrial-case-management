7e1d27f17b5ad34945637e927ac25ea67ebc130d
Todd Bergman <todd@openlattice.com>
2020-10-22 16:39:30 -0700

    refactored so simplify

--- src/components/casehistory/ChargeHistoryStats.js
+++ src/components/casehistory/ChargeHistoryStats.js
@@ -2,13 +2,18 @@
  * @flow
  */
 import React from 'react';
-import Immutable from 'immutable';
+import { List, Map } from 'immutable';
 import { Tag } from 'lattice-ui-kit';
+import { DateTime } from 'luxon';
 
+import { PSA_NEIGHBOR } from '../../utils/consts/FrontEndStateConsts';
+import { APP_TYPES, PROPERTY_TYPES } from '../../utils/consts/DataModelConsts';
 import { getSummaryStats } from '../../utils/HistoricalChargeUtils';
+import { getChargeIdToSentenceDate } from '../../utils/SentenceUtils';
+import { getEntityProperties } from '../../utils/DataUtils';
+import { getPendingCharges } from '../../utils/AutofillUtils';
 import {
   Title,
-  PendingChargeStatus,
   StatsContainer,
   StatsWrapper,
   StatsSubWrapper,
@@ -19,91 +24,126 @@ import {
   StatsSectionHeader
 } from '../../utils/Layout';
 
+const {
+  CHARGES,
+  MANUAL_PRETRIAL_CASES,
+  PRETRIAL_CASES,
+  SENTENCES,
+} = APP_TYPES;
+
+const {
+  ARREST_DATE_TIME,
+  FILE_DATE,
+  CASE_ID
+} = PROPERTY_TYPES;
+
 type Props = {
-  chargeHistory :Immutable.Map<*, *>,
-  pendingCharges :Immutable.List<*, *>,
-  padding :boolean
+  padding :boolean;
+  personNeighbors :Map;
+  psaNeighbors :Map;
 };
 
-class ChargeHistoryStats extends React.Component<Props, *> {
+const ChargeHistoryStats = ({
+  personNeighbors,
+  psaNeighbors,
+  padding
+} :Props) => {
+  const arrest = psaNeighbors.getIn([MANUAL_PRETRIAL_CASES, PSA_NEIGHBOR.DETAILS], Map());
+  const {
+    [ARREST_DATE_TIME]: arrestDateTime,
+    [FILE_DATE]: arrestFileDate,
+    [CASE_ID]: caseId
+  } = getEntityProperties(
+    arrest,
+    [CASE_ID, ARREST_DATE_TIME, FILE_DATE]
+  );
+  const arrestDate = arrestDateTime || arrestFileDate || DateTime.local().toISO();
 
-  renderPendingChargeStatus = () => {
-    const { pendingCharges } = this.props;
-    const statusText = pendingCharges.size
-      ? `${pendingCharges.size} Pending Charge${pendingCharges.size > 1 ? 's' : ''}`
-      : 'No Pending Charges';
-    const mode = pendingCharges.size ? 'danger' : 'neutral';
-    return (
-      <Tag mode={mode}>
-        {statusText}
-      </Tag>
-    );
-  }
+  const allCharges = personNeighbors.get(CHARGES, List());
+  const allCases = personNeighbors.get(PRETRIAL_CASES, List());
+  const allSentences = personNeighbors.get(SENTENCES, List());
 
-  render() {
-    const { chargeHistory, padding, pendingCharges } = this.props;
-    const {
-      numMisdemeanorCharges,
-      numMisdemeanorConvictions,
-      numFelonyCharges,
-      numFelonyConvictions,
-      numViolentCharges,
-      numViolentConvictions
-    } = getSummaryStats(chargeHistory);
+  const chargeIdsToSentenceDates = getChargeIdToSentenceDate(allSentences);
+  const pendingCharges = getPendingCharges(
+    caseId,
+    arrestDate,
+    allCases,
+    allCharges,
+    allSentences
+  );
 
-    const SUMMARY_STATS_ARR = [
-      {
-        label: '# of misdemeanor charges',
-        value: numMisdemeanorCharges
-      },
-      {
-        label: '# of felony charges',
-        value: numFelonyCharges
-      },
-      {
-        label: '# of violent charges',
-        value: numViolentCharges
-      },
-      {
-        label: '# of misdemeanor convictions',
-        value: numMisdemeanorConvictions
-      },
-      {
-        label: '# of felony convictions',
-        value: numFelonyConvictions
-      },
-      {
-        label: '# of violent convictions',
-        value: numViolentConvictions
-      }
-    ];
+  const statusText = pendingCharges.size
+    ? `${pendingCharges.size} Pending Charge${pendingCharges.size > 1 ? 's' : ''}`
+    : 'No Pending Charges';
+  const mode = pendingCharges.size ? 'danger' : 'neutral';
 
-    const SummaryStats = SUMMARY_STATS_ARR.map((stat) => (
-      <StatsItem key={stat.label}>
-        <StatLabel>{stat.label}</StatLabel>
-        <StatValue>{stat.value}</StatValue>
-      </StatsItem>
-    ));
+  const {
+    numMisdemeanorCharges,
+    numMisdemeanorConvictions,
+    numFelonyCharges,
+    numFelonyConvictions,
+    numViolentCharges,
+    numViolentConvictions
+  } = getSummaryStats(allCharges, chargeIdsToSentenceDates);
 
-    return (
-      <StatsWrapper padding={padding}>
-        <StatsSectionHeader>
-          <Title withSubtitle>
-            <span>Summary Statistics</span>
-            All current and past cases
-          </Title>
-          {pendingCharges.size ? this.renderPendingChargeStatus() : null}
-        </StatsSectionHeader>
-        <StatsContainer>
-          <StatsSubWrapper>
-            <StatsGroup>
-              {SummaryStats}
-            </StatsGroup>
-          </StatsSubWrapper>
-        </StatsContainer>
-      </StatsWrapper>
-    );
-  }
-}
+  const SUMMARY_STATS_ARR = [
+    {
+      label: '# of misdemeanor charges',
+      value: numMisdemeanorCharges
+    },
+    {
+      label: '# of felony charges',
+      value: numFelonyCharges
+    },
+    {
+      label: '# of violent charges',
+      value: numViolentCharges
+    },
+    {
+      label: '# of misdemeanor convictions',
+      value: numMisdemeanorConvictions
+    },
+    {
+      label: '# of felony convictions',
+      value: numFelonyConvictions
+    },
+    {
+      label: '# of violent convictions',
+      value: numViolentConvictions
+    }
+  ];
+
+  const SummaryStats = SUMMARY_STATS_ARR.map((stat) => (
+    <StatsItem key={stat.label}>
+      <StatLabel>{stat.label}</StatLabel>
+      <StatValue>{stat.value}</StatValue>
+    </StatsItem>
+  ));
+
+  return (
+    <StatsWrapper padding={padding}>
+      <StatsSectionHeader>
+        <Title withSubtitle>
+          <span>Summary Statistics</span>
+          All current and past cases
+        </Title>
+        {
+          !!pendingCharges.size && (
+            <Tag mode={mode}>
+              {statusText}
+            </Tag>
+          )
+        }
+      </StatsSectionHeader>
+      <StatsContainer>
+        <StatsSubWrapper>
+          <StatsGroup>
+            {SummaryStats}
+          </StatsGroup>
+        </StatsSubWrapper>
+      </StatsContainer>
+    </StatsWrapper>
+  );
+};
 
 export default ChargeHistoryStats;
