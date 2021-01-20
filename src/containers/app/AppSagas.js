d376f5e6abfa5fc34044db9d804ea13a6817171c
Todd Bergman <todd@openlattice.com>
2021-01-20 15:00:32 -0800

    apptype id stuff

--- src/containers/app/AppSagas.js
+++ src/containers/app/AppSagas.js
@@ -3,14 +3,14 @@
  */
 
 import { push } from 'connected-react-router';
-import { Constants, SearchApi, Types } from 'lattice';
+import { Constants, SearchApi } from 'lattice';
 import { AuthActions, AccountUtils } from 'lattice-auth';
 import { OrderedMap, fromJS } from 'immutable';
 import {
   AppApiActions,
+  SearchApiActions,
   AppApiSagas,
-  EntityDataModelApiActions,
-  EntityDataModelApiSagas,
+  SearchApiSagas
 } from 'lattice-sagas';
 import type { SequenceAction } from 'redux-reqseq';
 
@@ -35,13 +35,13 @@ import {
   loadApp
 } from './AppActionFactory';
 
+const { searchEntitySetData, searchEntityNeighborsWithFilter } = SearchApiActions;
+const { searchEntitySetDataWorker, searchEntityNeighborsWithFilterWorker } = SearchApiSagas;
+
 const LOG :Logger = new Logger('AppSagas');
 
-const { SecurableTypes } = Types;
-const { getEntityDataModelProjection } = EntityDataModelApiActions;
-const { getEntityDataModelProjectionWorker } = EntityDataModelApiSagas;
-const { getApp, getAppConfigs, getAppTypes } = AppApiActions;
-const { getAppWorker, getAppConfigsWorker, getAppTypesWorker } = AppApiSagas;
+const { getApp, getAppConfigs } = AppApiActions;
+const { getAppWorker, getAppConfigsWorker } = AppApiSagas;
 
 const { OPENLATTICE_ID_FQN } = Constants;
 
@@ -65,32 +65,15 @@ function* loadAppWorker(action :SequenceAction) :Generator<*, *, *> {
      */
 
     const app = response.data;
-    response = yield all([
-      call(getAppConfigsWorker, getAppConfigs(app.id)),
-      call(getAppTypesWorker, getAppTypes(app.appTypeIds)),
-    ]);
-    if (response[0].error) throw response[0].error;
-    if (response[1].error) throw response[1].error;
+    response = yield call(getAppConfigsWorker, getAppConfigs(app.id));
+    if (response.error) throw response.error;
 
     /*
      * 3. load EntityTypes and PropertyTypes
      */
 
-    const appConfigs :Object[] = response[0].data;
-    const appTypesMap :Object = response[1].data;
-    const appTypes :Object[] = (Object.values(appTypesMap) :any);
-    const projection :Object[] = appTypes.map((appType :Object) => ({
-      id: appType.entityTypeId,
-      include: [SecurableTypes.EntitySet, SecurableTypes.EntityType, SecurableTypes.PropertyTypeInEntitySet],
-      type: SecurableTypes.EntityType,
-    }));
-    response = yield call(getEntityDataModelProjectionWorker, getEntityDataModelProjection(projection));
-    if (response.error) {
-      LOG.error(response.error);
-      throw response.error;
-    }
+    const appConfigs :Object[] = response.data;
 
-    const edm :Object = response.data;
     appConfigs.forEach((appConfig :Object) => {
 
       const { organization } :Object = appConfig;
@@ -100,21 +83,25 @@ function* loadAppWorker(action :SequenceAction) :Generator<*, *, *> {
         appSettingsByOrgId = appSettingsByOrgId.set(orgId, appSettingsConfig.entitySetId);
       }
     });
-    const appSettingCalls = appSettingsByOrgId.valueSeq().map((entitySetId) => (
-      call(SearchApi.searchEntitySetData, entitySetId, {
+    const appSettingCalls = appSettingsByOrgId.valueSeq().map((entitySetId) => {
+      const searchConstraints = {
+        entitySetIds: [entitySetId],
         start: 0,
         maxHits: 10000,
-        searchTerm: '*'
-      })
-    ));
+        constraints: [{ constraints: [{ fuzzy: false, type: 'simple', searchTerm: '*' }]}]
+      };
+      return (
+        call(searchEntitySetDataWorker, searchEntitySetData(searchConstraints))
+      );
+    });
 
     const orgIds = appSettingsByOrgId.keySeq().toJS();
     const appSettingResults = yield all(appSettingCalls.toJS());
 
     let i = 0;
     appSettingResults
-      .map((result) => (
-        result.hits.length ? result : ({ numHits: 1, hits: [{ 'ol.appdetails': [DEFAULT_SETTINGS] }] }
+      .map(({ data }) => (
+        data.hits.length ? data : ({ numHits: 1, hits: [{ 'ol.appdetails': [DEFAULT_SETTINGS] }] }
         )))
       .map(({ hits }) => hits).forEach((setting) => {
         const entitySetId = orgIds[i];
@@ -129,9 +116,7 @@ function* loadAppWorker(action :SequenceAction) :Generator<*, *, *> {
     yield put(loadApp.success(action.id, {
       app,
       appConfigs,
-      appSettingsByOrgId,
-      appTypes,
-      edm
+      appSettingsByOrgId
     }));
 
   }
