583eb051e8af5492b339b16426b08c1e37f44698
Todd Bergman <todd@openlattice.com>
2021-01-20 15:08:49 -0800

    RequiresActionReducer

---
+++ src/containers/requiresaction/RequriesActionReducer.js
@@ -0,0 +1,77 @@
+/*
+ * @flow
+ */
+
+import { Map, fromJS } from 'immutable';
+import { RequestStates } from 'redux-reqseq';
+import type { SequenceAction } from 'redux-reqseq';
+
+import {
+  LOAD_REQUIRES_ACTION,
+  SET_VALUE,
+  loadRequiresAction
+} from './actions';
+
+import {
+  HITS,
+  PAGE,
+  REQUEST_STATE,
+  RS_INITIAL_STATE,
+  TOTAL_HITS,
+} from '../../core/redux/constants';
+import { PSA_STATUSES } from '../../utils/consts/Consts';
+import { PROPERTY_TYPES } from '../../utils/consts/DataModelConsts';
+import REQUIRES_ACTION from '../../utils/consts/redux/RequiresAction';
+
+const { DATE_TIME } = PROPERTY_TYPES;
+
+const INITIAL_STATE = fromJS({
+  [LOAD_REQUIRES_ACTION]: RS_INITIAL_STATE,
+
+  [HITS]: [],
+  [PAGE]: 1,
+  [TOTAL_HITS]: 0,
+  [REQUIRES_ACTION.SORT]: DATE_TIME,
+  [REQUIRES_ACTION.STATUS]: PSA_STATUSES.OPEN,
+  decending: false,
+
+});
+
+export default function reducer(state :Map = INITIAL_STATE, action :SequenceAction) {
+  switch (action.type) {
+
+    case loadRequiresAction.case(action.type): {
+      return loadRequiresAction.reducer(state, action, {
+        REQUEST: () => state
+          .setIn([LOAD_REQUIRES_ACTION, REQUEST_STATE], RequestStates.PENDING)
+          .setIn([LOAD_REQUIRES_ACTION, action.id], action),
+        SUCCESS: () => {
+          const storedAction = state.getIn([LOAD_REQUIRES_ACTION, action.id]);
+          if (storedAction) {
+            const { numHits, hits } = action.value;
+            return state
+              .set(HITS, hits)
+              .set(TOTAL_HITS, numHits)
+              .setIn([LOAD_REQUIRES_ACTION, REQUEST_STATE], RequestStates.SUCCESS);
+          }
+          return state;
+        },
+        FAILURE: () => {
+          if (state.hasIn([LOAD_REQUIRES_ACTION, action.id])) {
+            return state.setIn([LOAD_REQUIRES_ACTION, REQUEST_STATE], RequestStates.FAILURE);
+          }
+          return state;
+        },
+        FINALLY: () => state.deleteIn([LOAD_REQUIRES_ACTION, action.id]),
+      });
+    }
+
+    case SET_VALUE: {
+      const { field, value } = action.value;
+      return state.set(field, value);
+    }
+
+    default:
+      return state;
+  }
+}
