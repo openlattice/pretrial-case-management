534e782f1e60c098ec20f7c70c9c5ed2a2721cb3
Todd Bergman <todd@openlattice.com>
2021-01-21 18:55:45 -0800

    refactor to include additino info and sort imports

--- src/containers/requiresaction/components/SearchResult.js
+++ src/containers/requiresaction/components/SearchResult.js
@@ -4,50 +4,67 @@
 
 import React from 'react';
 
-import { List, Map, get, getIn, fromJS } from 'immutable';
-import { Card, CardSegment, Label, Skeleton, Tag } from 'lattice-ui-kit';
+import styled from 'styled-components';
+import {
+  List,
+  Map,
+  get,
+  getIn
+} from 'immutable';
+import {
+  Card,
+  Label,
+  Skeleton,
+  Tag
+} from 'lattice-ui-kit';
 import { DataUtils, ReduxUtils } from 'lattice-utils';
 import { DateTime } from 'luxon';
 import { useSelector } from 'react-redux';
-import styled, { css } from 'styled-components';
 
-import REVIEW_DATA from '../../../utils/consts/redux/ReviewConsts';
+import ActionButton from '../ActionButton';
+import BooleanFlag from '../../../components/BooleanFlag';
 import PSAMetaData from '../../../components/review/PSAMetaData';
+import REVIEW_DATA from '../../../utils/consts/redux/ReviewConsts';
+import ScoreScale from '../../../components/ScoreScale';
+import { REQUEST_STATE } from '../../../core/redux/constants';
 import { getPendingCharges } from '../../../utils/AutofillUtils';
 import { getEntityProperties } from '../../../utils/DataUtils';
+import { getPersonNameString } from '../../../utils/PeopleUtils';
+import { PSA_STATUSES } from '../../../utils/consts/Consts';
 import { APP_TYPES, PROPERTY_TYPES } from '../../../utils/consts/DataModelConsts';
-import { PSA_ASSOCIATION } from '../../../utils/consts/FrontEndStateConsts';
 import { APP_DATA } from '../../../utils/consts/redux/AppConsts';
-import { LOAD_PSA_DATA } from '../../review/ReviewActions';
 import { PEOPLE_ACTIONS, PEOPLE_DATA } from '../../../utils/consts/redux/PeopleConsts';
 import { REDUX, STATE } from '../../../utils/consts/redux/SharedConsts';
-import ScoreScale from '../../../components/ScoreScale'
-import BooleanFlag from '../../../components/BooleanFlag'
-import { REQUEST_STATE } from '../../../core/redux/constants';
+import { LOAD_PSA_DATA } from '../../review/ReviewActions';
 
 const Data = styled.div`
   display: flex;
   flex-direction: column;
   overflow: hidden;
-  white-space: nowrap;
   text-overflow: ellipsis;
+  white-space: nowrap;
+  width: 100%;
 `;
 
 const BottomContainer = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 10px 20px;
+
+  > div:first-child {
+    width: min-content;
+  }
 `;
 
-const FlagContainer = styled.div`
-  display: flex;
+const BottomSubContainer = styled.div`
   align-items: center;
+  display: flex;
   width: 100%;
 `;
 
 const DataGrid = styled(BottomContainer)`
   display: grid;
-  grid-template-columns: 5fr 5fr 5fr 5fr 4fr 4fr 4fr;
+  grid-template-columns: 4fr 3fr 3fr 3fr 3fr 3fr 1fr;
   justify-content: space-between;
 `;
 
@@ -60,30 +77,37 @@ const { getEntityKeyId } = DataUtils;
 const { isPending, isSuccess } = ReduxUtils;
 
 const {
-  ASSESSED_BY,
   CHARGES,
   FTAS,
   MANUAL_PRETRIAL_CASES,
+  OUTCOMES,
   PEOPLE,
   PRETRIAL_CASES,
   PSA_SCORES,
   SENTENCES,
-  STAFF,
 } = APP_TYPES;
 
 const {
   ARREST_DATE_TIME,
   CASE_ID,
   DATE_TIME,
+  DOB,
   ENTITY_KEY_ID,
-  FIRST_NAME,
   FTA_SCALE,
-  LAST_NAME,
   NCA_SCALE,
   NVCA_FLAG,
-  MIDDLE_NAME,
-  PERSON_ID
+  PERSON_ID,
+  STATUS
 } = PROPERTY_TYPES;
+
+const STATUS_MODE = {
+  [PSA_STATUSES.OPEN]: 'primary',
+  [PSA_STATUSES.SUCCESS]: 'success',
+  [PSA_STATUSES.FAILURE]: 'danger',
+  [PSA_STATUSES.CANCELLED]: undefined,
+  [PSA_STATUSES.DECLINED]: undefined,
+  [PSA_STATUSES.DISMISSED]: undefined,
+};
 
 type Props = {
   result :Map;
@@ -94,33 +118,36 @@ const PSAResult = (props :Props) => {
   const flags = [];
   const esidsToAppType = useSelector((store) => getIn(store, [STATE.APP, ENTITY_SETS_BY_ORG], Map()));
   const getPeopleNeighborsRS = useSelector((store) => store.getIn(
-    [STATE.PEOPLE, REDUX.ACTIONS, GET_PEOPLE_NEIGHBORS, REQUEST_STATE])
-  );
+    [STATE.PEOPLE, REDUX.ACTIONS, GET_PEOPLE_NEIGHBORS, REQUEST_STATE]
+  ));
   const getPSANeighborsRS = useSelector((store) => store.getIn([STATE.REVIEW, LOAD_PSA_DATA, REQUEST_STATE]));
-  const psaEKID = getEntityKeyId(psa);
+  const psaEKID :UUID = getEntityKeyId(psa) || '';
+  const status = getIn(psa, [STATUS, 0], '');
   const nvca = getIn(psa, [NVCA_FLAG, 0], '');
   const nca = getIn(psa, [NCA_SCALE, 0], '');
-  const fta = getIn(psa, [FTA_SCALE, 0], '');
+  const ftaScore = getIn(psa, [FTA_SCALE, 0], '');
   const psaDate = DateTime.fromISO(getIn(psa, [DATE_TIME, 0], ''));
   const psaNeighbors = useSelector((store) => getIn(store, [STATE.REVIEW, PSA_NEIGHBORS_BY_ID, psaEKID], Map()));
-  const staff = get(psaNeighbors, STAFF, Map());
   const manualPretrialCase = get(psaNeighbors, MANUAL_PRETRIAL_CASES, Map());
+  const psaOutcome = get(psaNeighbors, OUTCOMES, Map());
   const { [CASE_ID]: currentCaseNumber } = getEntityProperties(manualPretrialCase, [CASE_ID]);
   const person = get(psaNeighbors, PEOPLE, Map());
   const {
+    [DOB]: dob,
     [ENTITY_KEY_ID]: personEKID,
-    [FIRST_NAME]: firstName,
-    [LAST_NAME]: lastName,
-    [MIDDLE_NAME]: middleName,
     [PERSON_ID]: id,
-  } = getEntityProperties(person, [ENTITY_KEY_ID, FIRST_NAME, LAST_NAME, MIDDLE_NAME, PERSON_ID]);
-  const personNeighbors = useSelector((store) => getIn(store, [STATE.PEOPLE, PEOPLE_NEIGHBORS_BY_ID, personEKID], Map()));
-  const allPSAs = get(personNeighbors, PSA_SCORES, List());
+  } = getEntityProperties(person, [DOB, ENTITY_KEY_ID, PERSON_ID]);
+  const personName = getPersonNameString(person);
+  const personNeighbors = useSelector((store) => getIn(
+    store, [STATE.PEOPLE, PEOPLE_NEIGHBORS_BY_ID, personEKID], Map()
+  ));
+  const allOpenPSAs = get(personNeighbors, PSA_SCORES, List()).filter((scores) => {
+    const psaStatus = getIn(scores, [STATUS, 0], '');
+    return psaStatus === PSA_STATUSES.OPEN;
+  });
   const allCases = get(personNeighbors, PRETRIAL_CASES, List());
   const allCharges = get(personNeighbors, CHARGES, List());
   const allSentences = get(personNeighbors, SENTENCES, List());
-
-  if (allPSAs.size > 1) flags.push(<Tag mode="info">{`${allPSAs.size} Open PSAs`}</Tag>)
 
   const ftas = personNeighbors.get(FTAS, List());
   const hasFTASincePSA = ftas.some((fta) => {
@@ -132,19 +159,7 @@ const PSAResult = (props :Props) => {
     return false;
   });
 
-  if (hasFTASincePSA) flags.push(<Tag mode="danger">Recent FTA</Tag>)
-
   const { [ARREST_DATE_TIME]: arrestDate } = getEntityProperties(manualPretrialCase, [ARREST_DATE_TIME]);
-
-  let filer = '';
-  staff.forEach((neighbor) => {
-    const associationEntitySetId = getIn(neighbor, [PSA_ASSOCIATION.ENTITY_SET, 'id']);
-    const appTypeFqn = get(esidsToAppType, associationEntitySetId);
-    const { [PERSON_ID]: personId } = getEntityProperties(person, [PERSON_ID]);
-    if (appTypeFqn === ASSESSED_BY) {
-      filer = personId;
-    }
-  });
 
   const pendingCharges = getPendingCharges(
     currentCaseNumber,
@@ -154,49 +169,55 @@ const PSAResult = (props :Props) => {
     allSentences,
   );
 
-  if (!pendingCharges.size) flags.push(<Tag mode="secondary">No Pending Charges</Tag>)
-
-  const numberOfPendingCharges = pendingCharges;
+  if (psaOutcome.size > 1) flags.push(<Tag mode="success">Has Outcome</Tag>);
+  if (allOpenPSAs.size > 1) flags.push(<Tag mode="info">{`${allOpenPSAs.size} Open PSAs`}</Tag>);
+  if (hasFTASincePSA) flags.push(<Tag mode="danger">Recent FTA</Tag>);
+  if (!pendingCharges.size) flags.push(<Tag mode="secondary">No Pending Charges</Tag>);
 
   const data = {
-    First: firstName,
-    Middle: middleName,
-    Last: lastName,
+    Name: personName,
+    'Date of Birth': dob,
     ID: id,
     NCA: <ScoreScale score={nca} />,
-    FTA: <ScoreScale score={fta} />,
+    FTA: <ScoreScale score={ftaScore} />,
     NVCA: <BooleanFlag value={nvca} />
   };
 
   return (
-    <>
     <Card>
       <DataGrid key={psaEKID}>
         {
           Object.entries(data).map(([key, value]) => (
             <Data>
               <Label>{key}</Label>
-              {isPending(getPSANeighborsRS) ? <Skeleton/> : value}
+              {isPending(getPSANeighborsRS) ? <Skeleton /> : value}
             </Data>
           ))
         }
-      </DataGrid>
-      <BottomContainer>
-      {
-        isPending(getPeopleNeighborsRS) && <Skeleton width="100%"/>
-      }
-      {
-        (isSuccess(getPeopleNeighborsRS) && flags.length > 0) && (
-          <FlagContainer>{flags}</FlagContainer>
-        )
-      }
-        <PSAMetaData
-            entitySetIdsToAppType={esidsToAppType}
+        <ActionButton
+            personEKID={personEKID}
+            psaEKID={psaEKID}
             psaNeighbors={psaNeighbors}
             scores={psa} />
-    </BottomContainer>
+      </DataGrid>
+      <BottomContainer>
+        <BottomSubContainer><Tag mode={STATUS_MODE[status]}>{status}</Tag></BottomSubContainer>
+        {
+          isPending(getPeopleNeighborsRS) && <Skeleton width="100%" />
+        }
+        {
+          (isSuccess(getPeopleNeighborsRS) && flags.length > 0) && (
+            <BottomSubContainer>{flags}</BottomSubContainer>
+          )
+        }
+        <BottomSubContainer>
+          <PSAMetaData
+              entitySetIdsToAppType={esidsToAppType}
+              psaNeighbors={psaNeighbors}
+              scores={psa} />
+        </BottomSubContainer>
+      </BottomContainer>
     </Card>
-    </>
   );
 };
 
