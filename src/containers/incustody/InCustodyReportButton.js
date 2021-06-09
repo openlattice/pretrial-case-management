8fe854a0669822b2cb3e7dfc8d97f222f4bf46a1
Todd Bergman <todd@openlattice.com>
2021-06-09 15:39:40 -0700

    add a in-custody requestSequence status to disabled

--- src/containers/incustody/InCustodyReportButton.js
+++ src/containers/incustody/InCustodyReportButton.js
@@ -8,6 +8,7 @@ import type { RequestState } from 'redux-reqseq';
 import { Button } from 'lattice-ui-kit';
 import { connect } from 'react-redux';
 import { Map, Set } from 'immutable';
+import { ReduxUtils } from 'lattice-utils';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowAltCircleDown } from '@fortawesome/pro-light-svg-icons';
@@ -19,7 +20,9 @@ import { STATE } from '../../utils/consts/redux/SharedConsts';
 import REVIEW_DATA from '../../utils/consts/redux/ReviewConsts';
 import { IN_CUSTODY_ACTIONS, IN_CUSTODY_DATA } from '../../utils/consts/redux/InCustodyConsts';
 import { PEOPLE_ACTIONS, PEOPLE_DATA } from '../../utils/consts/redux/PeopleConsts';
-import { getReqState, requestIsPending } from '../../utils/consts/redux/ReduxUtils';
+import { getReqState } from '../../utils/consts/redux/ReduxUtils';
+
+const { isPending } = ReduxUtils;
 
 const ButtonWrapper = styled.div`
   width: 100%;
@@ -29,6 +32,7 @@ const ButtonWrapper = styled.div`
 
 type Props = {
   downloadInCustodyReportReqState :RequestState;
+  getInCustodyDataRequestState :RequestState;
   getPeopleNeighborsReqState :RequestState;
   jailStaysById :Map;
   jailStayNeighborsById :Map;
@@ -43,13 +47,15 @@ class InCustodyDownloadButton extends React.Component<Props, *> {
   isDisabled = () => {
     const {
       downloadInCustodyReportReqState,
+      getInCustodyDataRequestState,
       getPeopleNeighborsReqState,
       loadingResults,
       peopleInCustody
     } = this.props;
     return loadingResults
-      || requestIsPending(downloadInCustodyReportReqState)
-      || requestIsPending(getPeopleNeighborsReqState)
+      || isPending(downloadInCustodyReportReqState)
+      || isPending(getInCustodyDataRequestState)
+      || isPending(getPeopleNeighborsReqState)
       || !peopleInCustody.size;
   }
 
@@ -87,12 +93,13 @@ function mapStateToProps(state) {
   const people = state.get(STATE.PEOPLE, Map());
   const loadPSAsByStatusRS = getReqState(review, LOAD_PSAS_BY_STATUS);
   const loadPSADataRS = getReqState(review, LOAD_PSA_DATA);
-  const loadingPSAsByStatus = requestIsPending(loadPSAsByStatusRS);
-  const loadingPSAData = requestIsPending(loadPSADataRS);
+  const loadingPSAsByStatus = isPending(loadPSAsByStatusRS);
+  const loadingPSAData = isPending(loadPSADataRS);
   const loadingResults = loadingPSAsByStatus || loadingPSAData;
   return {
     // In-Custody
     downloadInCustodyReportReqState: getReqState(inCustody, IN_CUSTODY_ACTIONS.DOWNLOAD_IN_CUSTODY_REPORT),
+    getInCustodyDataRequestState: getReqState(inCustody, IN_CUSTODY_ACTIONS.GET_IN_CUSTODY_DATA),
     [IN_CUSTODY_DATA.JAIL_STAYS_BY_ID]: inCustody.get(IN_CUSTODY_DATA.JAIL_STAYS_BY_ID),
     [IN_CUSTODY_DATA.JAIL_STAY_NEIGHBORS_BY_ID]: inCustody.get(IN_CUSTODY_DATA.JAIL_STAY_NEIGHBORS_BY_ID),
     [IN_CUSTODY_DATA.PEOPLE_IN_CUSTODY]: inCustody.get(IN_CUSTODY_DATA.PEOPLE_IN_CUSTODY),
